4cdbd814397ff726bdac3519e4a2641f55be13a0
ripan <reponmondal007@gmail.com>
2023-10-27 15:50:27 +0000

    Oct 27, 2023, 9:20 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      name: "random",
     };
     this.increment = this.increment.bind(this);
   }
@@ -19,6 +19,13 @@ class App extends React.Component {
       <>
         <h1>{this.state.count}</h1>
         <button onClick={this.increment}>increment</button>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="enter name"
+          value={this.state.name}
+        />
       </>
     );
   }
