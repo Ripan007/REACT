17bb8c8f9c40878e375beafde81d94726bfd2bc5
ripan <reponmondal007@gmail.com>
2023-10-30 09:21:11 +0000

    Oct 30, 2023, 2:51 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from "react";
-import { FirstCompWithCounter } from "./FirstCompWithCounter";
-import { SecondCompWithCounter } from "./SecondCompWithCounter";
+function Card() {}
 
-const App = () => {
+function App() {
   return (
-    <>
-      <FirstCompWithCounter />
-      <SecondCompWithCounter />
-    </>
+    <div id="app">
+      <h1>Available Experts</h1>
+      <Card name="Anthony Blake">
+        <p>
+          Blake is a professor of Computer Science at the University of
+          Illinois.
+        </p>
+        <p>
+          <a href="mailto:blake@example.com">Email Anthony</a>
+        </p>
+      </Card>
+
+      <Card name="Maria Miles">
+        <p>
+          Maria is a professor of Computer Science at the University of
+          Illinois.
+        </p>
+        <p>
+          <a href="mailto:blake@example.com">Email Maria</a>
+        </p>
+      </Card>
+    </div>
   );
-};
+}
 
 export default App;
-
-//  redux toolkit query  (rtx query)
-//  fetching , useEffect , extra state management , caching
-
-//  this one is the  most decent theme , then why i am wasting my time for searching for  the theme
-//  this  combination of  fontfamily and  theme is awesome
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const DATAS = [
+  { id: 1, title: "first", description: "first description" },
+  { id: 2, title: "second", description: "second description" },
+  { id: 3, title: "third", description: "third description" },
+];
