ab7e5e422ce11c9dbee5eb18d8217187c02b7d38
ripan <reponmondal007@gmail.com>
2023-10-31 16:50:24 +0000

    Oct 31, 2023, 10:20 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-// import { CONTENT } from "./contentData";
-import CoreConcepts from "./CoreConcepts";
-import Examples from "./Examples";
-
-const reactDescription = ["awesome", "fast", "loop"];
-
-function getRandomIndex(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const react = reactDescription[getRandomIndex(2)];
-  return react;
-}
+import React from "react";
 
 export const App = () => {
-  return (
-    <>
-      <Header />
-
-      <CoreConcepts />
-      <Examples />
-    </>
-  );
+  return <div>App</div>;
 };
