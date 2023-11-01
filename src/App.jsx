24d3766c7b7a6332ffa7d6003aa5a042838e25ec
ripan <reponmondal007@gmail.com>
2023-11-01 14:45:59 +0530

    app file modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,5 @@ import React from "react";
 export const App = () => {
   return <div>App</div>;
 };
+
+//  what i want to achieve for that  i have to be that much dedicated towards  it then only i would able to achieve it
