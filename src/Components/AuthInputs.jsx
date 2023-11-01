84aeddbe580cb06ec4eb2a0a6b794ad5d66e93a3
ripan <reponmondal007@gmail.com>
2023-11-01 18:15:37 +0000

    Nov 1, 2023, 11:45 PM

--- src/Components/AuthInputs.jsx
+++ src/Components/AuthInputs.jsx
@@ -58,7 +58,9 @@ export default function AuthInputs() {
             // style={{
             //   backgroundColor: emailNotValid ? "#fed2d2 " : " #d1d5db ",
             // }}
-            className={emailNotValid ? "invalid" : undefined}
+
+            invalid={emailNotValid}
+            // className={emailNotValid ? "invalid" : undefined}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
