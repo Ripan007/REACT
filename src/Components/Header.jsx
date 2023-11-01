20ad51ff3a14cf930b9fe8c93e99f592f7cd0860
ripan <reponmondal007@gmail.com>
2023-11-01 16:31:25 +0000

    Nov 1, 2023, 10:01 PM

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,10 @@
 export default function Header() {
   return (
     <header>
-      <img alt="A canvas" />
+      <img
+        src="https://cdn.pixabay.com/photo/2023/10/28/18/02/songbird-8348139_1280.png"
+        alt="A canvas"
+      />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
