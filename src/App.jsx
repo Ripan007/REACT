8eedaf2582882289bd9b329e5b0d7c93167b36d6
ripan <reponmondal007@gmail.com>
2023-11-01 13:50:45 +0000

    Nov 1, 2023, 7:20 PM

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,4 @@ export const App = () => {
   return <div>App</div>;
 };
 
-//  what i want to achieve for that  i have to be that much dedicated towards  it then only i would able to achieve it
+//  css and responsive design
