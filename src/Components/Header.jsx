96eb1cc56610bbeea2c3706bf791b54f40ee5cfd
ripan <reponmondal007@gmail.com>
2023-11-02 09:05:04 +0000

    Nov 2, 2023, 2:35 PM

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header>
+    <header className="flex flex-col  items-center mt-8 mb-16">
       <img
         src="https://cdn.pixabay.com/photo/2023/10/28/18/02/songbird-8348139_1280.png"
         alt="A canvas"
