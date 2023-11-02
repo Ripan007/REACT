0d1ba1dfebfaca4acc55242b02bb6647f9e5576a
ripan <reponmondal007@gmail.com>
2023-11-02 09:22:12 +0000

    Nov 2, 2023, 2:52 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import AuthInputs from "./Components/AuthInputs";
 import Header from "./Components/Header";
-import "./index.css";
+// import "./index.css";
 export const App = () => {
   return (
     <>
       <Header />
 
-      <h1 className="text-3xl font-bold underline text-amber-500">
-        Hello world!
-      </h1>
       <main>
         <AuthInputs />
       </main>
