63e7895557c9087e85bf2c761d04e1997c9447a9
ripan <reponmondal007@gmail.com>
2023-11-07 17:06:42 +0000

    Nov 7, 2023, 10:36 PM

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enterValues, setEnterValues] = useState({
+    email: "default",
+    password: "",
+  });
+
+  const emailNotValid =
+    enterValues.email !== "" && !enterValues.email.includes("@");
+  function inputHandleChange(identifier, value) {
+    setEnterValues((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+  }
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(enterValues);
   }
 
   return (
@@ -9,10 +25,19 @@ export default function Login() {
         type="email"
         name="email"
         id=""
+        value={enterValues.email}
         placeholder="email"
         onChange={(e) => inputHandleChange("email", e.target.value)}
       />
-      <input type="password" name="password" id="" placeholder="password" />
+      {emailNotValid && <p>email is not valid</p>}
+      <input
+        type="password"
+        name="password"
+        id=""
+        value={enterValues.password}
+        placeholder="password"
+        onChange={(e) => inputHandleChange("password", e.target.value)}
+      />
       <button type="submit">submit</button>
     </form>
   );
