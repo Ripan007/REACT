eac2b8782202191b3720f25b2ba049cb10768802
ripan <reponmondal007@gmail.com>
2023-11-07 16:25:25 +0000

    Nov 7, 2023, 9:55 PM

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,42 +1,22 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  const [enterData, setEnterData] = useState({
-    email: "default",
-    password: "default",
-  });
-
-  function inputHandleChange(identifier, value) {
-    setEnterData((prevValue) => ({
-      ...prevValue,
-      [identifier]: value,
-    }));
-  }
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(enterData);
+    const emailData = email.current.value;
+    const passwordData = password.current.value;
+    console.log(emailData, passwordData);
+    email.current.value = "";
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        name="email"
-        id=""
-        value={enterData.email}
-        placeholder="email"
-        onChange={(e) => inputHandleChange("email", e.target.value)}
-      />
-      <input
-        type="password"
-        name="password"
-        id=""
-        value={enterData.password}
-        placeholder="password"
-        onChange={(e) => inputHandleChange("password", e.target.value)}
-      />
+      <input type="email" name="email" id="" ref={email} />
+      <input type="password" name="password" id="" ref={password} />
+      <button type="submit">submit</button>
       <button type="reset">reset</button>
-      <button type="login">login</button>
     </form>
   );
 }
