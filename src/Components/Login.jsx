393af11cdce411c309df377320a641af8da83dc3
ripan <reponmondal007@gmail.com>
2023-11-07 14:47:33 +0000

    Nov 7, 2023, 8:17 PM

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enterValue, setEnterValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleInputChange(identifier, value) {
+    setEnterValue((preValue) => ({
+      ...preValue,
+      [identifier]: value,
+    }));
+  }
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(enterValue);
   }
   return (
     <form onSubmit={handleSubmit}>
       <h2>login</h2>
       <div>
         <label htmlFor="">email:</label>
-        <input type="email" name="" id="" />
+        <input
+          type="email"
+          name=""
+          id=""
+          value={enterValue.email}
+          onChange={(e) => handleInputChange("email", e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="">password:</label>
-        <input type="password" name="" id="" />
+        <input
+          type="password"
+          name=""
+          id=""
+          value={enterValue.password}
+          onChange={(e) => handleInputChange("password", e.target.value)}
+        />
       </div>
       <p>
         <button className="bg-green-400 p-1 rounded-sm text-white m-3">
