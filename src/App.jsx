f7b0edbd9f0bd82d000fa2dea49194f2e70466ac
ripan <reponmondal007@gmail.com>
2023-11-07 13:23:52 +0000

    Nov 7, 2023, 6:53 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
 export default function App() {
-  return <h1>app</h1>;
+  return (
+    <>
+      <h1>login</h1>
+      <form>
+        <label htmlFor="">email:</label>
+        <input type="email" name="" id="" />
+        <br />
+        <label htmlFor="">password:</label>
+        <input type="password" name="" id="" />
+        <br />
+        <button className="">reset</button>
+        <button className="">login</button>
+      </form>
+    </>
+  );
 }
