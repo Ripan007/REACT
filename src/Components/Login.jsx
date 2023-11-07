172938b86b7dc747d7f814e55d386992b6fe9023
ripan <reponmondal007@gmail.com>
2023-11-07 16:00:46 +0000

    Nov 7, 2023, 9:30 PM

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,15 +2,19 @@ export default function Login() {
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
+    const identicalObject = fd.getAll("identical");
     const data = Object.fromEntries(fd.entries());
+    data.identical = identicalObject;
     console.log(data);
   }
   return (
     <form onSubmit={handleSubmit}>
       <input type="email" name="email" id="" />
       <input type="password" name="password" id="" />
-      <input type="checkbox" name="acquisition" value="animal" id="animal" />
-      <input type="checkbox" name="acquisition" value="human" id="human" />
+      <input type="text" name="firstName" id="" />
+      <input type="text" name="lastName" id="" />
+      <input type="checkbox" name="identical" value="animal" id="animal" />
+      <input type="checkbox" name="identical" value="human" id="human" />
       <button>reset</button>
       <button>login</button>
     </form>
