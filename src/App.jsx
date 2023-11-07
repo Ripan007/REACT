eaa2fb91433c11e6badf799500109e62c8e3af59
ripan <reponmondal007@gmail.com>
2023-11-07 13:10:20 +0000

    Nov 7, 2023, 6:40 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import Player from "./Components/Player";
-import TimeChallenge from "./Components/TimeChallenge";
+import React from "react";
 
-export default function App() {
-  return (
-    <>
-      <Player />
-      <TimeChallenge title={"easy"} targetTime={1} />
-      <TimeChallenge title={"not easy"} targetTime={5} />
-      <TimeChallenge title={"getting  tough"} targetTime={10} />
-      <TimeChallenge title={"pros only"} targetTime={15} />
-    </>
-  );
-}
+export const App = () => {
+  return <div>App</div>;
+};
