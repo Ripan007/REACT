d11fe1978aa93b6182f42d1835545adb4b8e4129
ripan <reponmondal007@gmail.com>
2023-11-07 15:53:04 +0000

    Nov 7, 2023, 9:23 PM

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,13 +2,17 @@ export default function Login() {
   function handleSubmit(event) {
     event.preventDefault();
     const fd = new FormData(event.target);
+    const acquistionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquistionChannel;
     console.log(data);
   }
   return (
     <form onSubmit={handleSubmit}>
       <input type="email" name="email" id="" />
       <input type="password" name="password" id="" />
+      <input type="checkbox" name="acquisition" value="animal" id="" />
+      <input type="checkbox" name="acquisition" value="human" id="" />
       <button>reset</button>
       <button>login</button>
     </form>
