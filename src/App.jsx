990e246b1281a0d5a70dc0861d2af86795eb37cf
ripan <reponmondal007@gmail.com>
2023-11-07 14:15:45 +0000

    Nov 7, 2023, 7:45 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
 export default function App() {
-  function handleSubmit() {
-    console.log("submitted");
-  }
-  return (
-    <>
-      <h1>login</h1>
-      <form>
-        <label htmlFor="">email:</label>
-        <input type="email" name="" id="" />
-        <br />
-        <label htmlFor="">password:</label>
-        <input type="password" name="" id="" />
-        <br />
-        <button className="">reset</button>
-        <button
-          type="button"
-          className="bg-red-700 text-white p-9"
-          onClick={handleSubmit}
-        >
-          login
-        </button>
-      </form>
-    </>
-  );
+  return <h1>welcome</h1>;
 }
