7273a277754bbf04fe0948e89cd2dd8f615032b7
ripan <reponmondal007@gmail.com>
2023-11-09 12:13:32 +0000

    Nov 9, 2023, 5:43 PM

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,14 @@
+import { useNavigate } from "react-router-dom";
 function Products() {
-  return <h1>product page</h1>;
+  const navigate = useNavigate();
+  function handleNavigate() {
+    navigate("/products");
+  }
+  return (
+    <>
+      <h1>product page</h1>
+      <button onClick={handleNavigate}>navigate</button>
+    </>
+  );
 }
 export default Products;
