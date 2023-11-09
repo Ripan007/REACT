92b0a1ab461d9632687c2e01c2b7e0d1833aed94
ripan <reponmondal007@gmail.com>
2023-11-09 11:33:36 +0000

    Nov 9, 2023, 5:03 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./Components/pages/Home";
-import ProductPage from "./Components/pages/Products";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+import MainNavigation from "./Components/MainNavigation";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductPage /> },
+  {
+    path: "/",
+    element: <MainNavigation />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductPage /> },
+    ],
+  },
 ]);
 function App() {
   return <RouterProvider router={router} />;
