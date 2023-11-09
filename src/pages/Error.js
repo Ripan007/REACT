e93d46f2f7986fa27b7925e53523a4aa3016afd8
ripan <reponmondal007@gmail.com>
2023-11-09 11:44:41 +0000

    Nov 9, 2023, 5:14 PM

---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import MainNavigation from "../Components/MainNavigation";
+
+function ErrorPage() {
+  return (
+    <>
+      <MainNavigation />
+      <div>
+        <p>error occured</p>
+        <p>page not found</p>
+      </div>
+    </>
+  );
+}
+
+export default ErrorPage;
