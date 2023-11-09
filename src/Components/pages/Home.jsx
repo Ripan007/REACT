480b6e3dea8b340f72bc6b49cf59c3ceb781a5c2
ripan <reponmondal007@gmail.com>
2023-11-09 10:57:07 +0000

    Nov 9, 2023, 4:27 PM

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -1,4 +1,6 @@
 function Home() {
-  return <h1 className="text-blue-600 uppercase">home</h1>;
+  return <h1 className="text-blue-600 uppercase">welcome to the home page</h1>;
 }
 export default Home;
+
+//  at this movement if  i try  to learn vim that would be a great mistake , thats why i  would i
