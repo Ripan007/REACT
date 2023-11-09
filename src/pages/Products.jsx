70f9adf1034ae86786d23964231dea0280f7cacd
ripan <reponmondal007@gmail.com>
2023-11-09 12:24:51 +0000

    Nov 9, 2023, 5:54 PM

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,13 @@
 import { useNavigate } from "react-router-dom";
 function Products() {
   const navigate = useNavigate();
-  function handleNavigate() {
+  function NavigateHandler() {
     navigate("/");
   }
   return (
     <>
       <h1>product page</h1>
-      <button className="bg-green-50" onClick={handleNavigate}>
+      <button className="bg-green-100" onClick={NavigateHandler}>
         navigate
       </button>
     </>
