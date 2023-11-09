73f8103654e28ba73fa04e68ff58f1105d26f7e0
ripan <reponmondal007@gmail.com>
2023-11-09 11:47:43 +0000

    Nov 9, 2023, 5:17 PM

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ function ErrorPage() {
   return (
     <>
       <MainNavigation />
-      <div>
+      <div className="bg-red-900 p-6 text-white">
         <p>error occured</p>
         <p>page not found</p>
       </div>
