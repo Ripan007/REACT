d64701d0455d7fe8a6ca6ca5ecd5e9ea595f7f4d
ripan <reponmondal007@gmail.com>
2023-11-09 11:58:49 +0000

    Nov 9, 2023, 5:28 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
-
+import "./App.css";
 const router = createBrowserRouter([
   {
     path: "/",
--- src/Components/MainNavigation.jsx
+++ src/Components/MainNavigation.jsx
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function MainNavigation() {
   return (
     <>
       <h1>main navigation</h1>
-      <header className="bg-slate-600 p-2 text-white uppercase ">
+      <header className="bg-slate-600 p-2 text-white uppercase flex ">
         <nav>
           <ul>
-            <Link className="m-2" to="/">
-              home
-            </Link>
-            <Link to="/products">products</Link>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                to="/"
+              >
+                home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                to="/products"
+              >
+                products
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </header>
