497ee2ee84e50bf2e7a81515a1e73b8e9932972b
ripan <reponmondal007@gmail.com>
2023-11-09 12:40:27 +0000

    Nov 9, 2023, 6:10 PM

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
 function ProductDetailPage() {
+  const params = useParams();
   return (
     <>
       <h1>product detail page</h1>
+      <h2>{params.productId}</h2>
     </>
   );
 }
