2d1029c6a560937d4fa0d50b4275188bdaed7654
ripan <reponmondal007@gmail.com>
2023-11-09 10:56:07 +0000

    Nov 9, 2023, 4:26 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,7 @@ import Home from "./Components/pages/Home";
 
 const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 function App() {
-  return (
-    <>
-      <h1>welcome</h1>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
