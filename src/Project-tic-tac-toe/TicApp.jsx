b58d296f5e1271a05d4adab22c373e21d1dbd74d
ripan <reponmondal007@gmail.com>
2023-11-16 17:36:05 +0000

    Nov 16, 2023, 11:06 PM

--- src/Project-tic-tac-toe/TicApp.jsx
+++ src/Project-tic-tac-toe/TicApp.jsx
@@ -20,18 +20,29 @@ function derivedActivePlayer(gameTurns) {
 export default function TicApp() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = derivedActivePlayer(gameTurns);
-    let gameBoard = initialGameBoard;
+  let gameBoard = initialGameBoard;
   for (const turn of gameTurns) {
     const { square, player } = turn;
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
-
+  let winner;
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const currentPlayer = derivedActivePlayer(prevTurns);
@@ -58,6 +69,7 @@ export default function TicApp() {
             isActive={activePlayer === "o"}
           />
         </ol>
+        {winner && <p>you won , {winner}</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
