9a30f9d0d6c5dc8a0de7f4c21e4eec45c5e8e573
ripan <reponmondal007@gmail.com>
2023-11-16 09:07:14 +0000

    Nov 16, 2023, 2:37 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@
 // import { DynamicValue } from "./React-essentials/components/Header";
 // import { Main } from "./React-essentials/components/Main";
 import "./App.css";
-// import { MainDeep } from "./ReactEssentials-Deep/components/MainDeep";
+import { MainDeep } from "./ReactEssentials-Deep/components/MainDeep";
 // import RenderContent from "./RenderContent";
-import { Practice } from "./practice";
+// import { Practice } from "./practice";
 
 function App() {
   return (
     <>
       {/* <h1>welcome</h1> */}
       {/* <RenderContent /> */}
-      {/* <MainDeep /> */}
-      <Practice />
+      <MainDeep />
+      {/* <Practice /> */}
     </>
   );
 }
