4a320d6a1ef20410b0e9ada40a486292c9735e36
ripan <reponmondal007@gmail.com>
2023-11-16 10:38:49 +0000

    Nov 16, 2023, 4:08 PM

--- src/Project-tic-tac-toe/TicApp.jsx
+++ src/Project-tic-tac-toe/TicApp.jsx
@@ -8,11 +8,13 @@ export default function TicApp() {
             <span className="player-symbol">0</span>
           </li>
           <li>
-            <span className="player-name">player 1</span>
-            <span className="player-symbol">0</span>
+            <span className="player-name">player 2</span>
+            <span className="player-symbol">x</span>
           </li>
         </ol>
+        game board
       </div>
+      game logic
     </main>
   );
 }
