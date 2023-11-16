80befa00ea0892afcf970d88db00d1cda3aee31d
ripan <reponmondal007@gmail.com>
2023-11-16 10:36:50 +0000

    Nov 16, 2023, 4:06 PM

--- src/Project-tic-tac-toe/TicApp.jsx
+++ src/Project-tic-tac-toe/TicApp.jsx
@@ -1,3 +1,18 @@
 export default function TicApp() {
-  return <h1>lets start</h1>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">player 1</span>
+            <span className="player-symbol">0</span>
+          </li>
+          <li>
+            <span className="player-name">player 1</span>
+            <span className="player-symbol">0</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
