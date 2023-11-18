edba0e43217a6137c0cb82230ec7fe149d4dfe79
ripan <reponmondal007@gmail.com>
2023-11-18 14:05:49 +0000

    Nov 18, 2023, 7:35 PM

---
+++ src/investment-calculator/components/Header/UserInput.jsx
@@ -0,0 +1,26 @@
+export default function UserInput() {
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </p>
+        <p>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </p>
+        <p>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </p>
+      </div>
+    </section>
+  );
+}
