85c38bcc0675a6dfccfd4684bec713801dc641f3
ripan <reponmondal007@gmail.com>
2023-11-18 17:53:35 +0000

    Nov 18, 2023, 11:23 PM

--- src/ref-portals/components/Players.jsx
+++ src/ref-portals/components/Players.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [enterPlayerName, setEnterPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+  // const [enterPlayerName, setEnterPlayerName] = useState("");
+  // const [submitted, setSubmitted] = useState(false);
 
-  function handleChange(e) {
-    setEnterPlayerName(e.target.value);
-  }
+  // function handleChange(e) {
+  //   setEnterPlayerName(e.target.value);
+  // }
   function handleClick() {
-    setSubmitted(true);
+    // setSubmitted(true);
+    setEnterPlayerName(playerName.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2>
+      {/* <h2>Welcome {submitted ? enterPlayerName : "unknown entity"}</h2> */}
+      <h2>welcome {enterPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enterPlayerName} />
+        {/* <input type="text" onChange={handleChange} value={enterPlayerName} /> */}
+        <input type="text" ref={playerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
