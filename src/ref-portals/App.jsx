a8f034216166f226c2bacc5ee87c8abb184db736
ripan <reponmondal007@gmail.com>
2023-11-19 07:27:16 +0000

    Nov 19, 2023, 12:57 PM

--- src/ref-portals/App.jsx
+++ src/ref-portals/App.jsx
@@ -1,11 +1,17 @@
 import Player from "./components/Players";
 import "./App.css";
+import TimerChallenge from "./components/TimerChallenge";
 
 export default function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge />
+        <TimerChallenge />
+        <TimerChallenge />
+        <TimerChallenge />
+      </div>
     </>
   );
 }
---
+++ src/ref-portals/components/TimerChallenge.jsx
@@ -0,0 +1,3 @@
+export default function TimerChallenge() {
+  return <h1>timer challenge</h1>;
+}
