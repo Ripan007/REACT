8e3a324c756a0b3e319bcff7290b5682d574d5e9
ripan <reponmondal007@gmail.com>
2023-11-20 09:07:41 +0000

    Nov 20, 2023, 2:37 PM

--- src/Javascript/JS/shoppingCart.js
+++ src/Javascript/JS/shoppingCart.js
@@ -1,5 +1,10 @@
 export const cart = [];
 
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${product}${quantity}`);
+};
+
 export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`{product}${quantity}`);
