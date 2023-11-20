5381f898f4a684a2583f99387848d96a4619c3f8
ripan <reponmondal007@gmail.com>
2023-11-20 08:40:04 +0000

    Nov 20, 2023, 2:10 PM

--- src/Javascript/JS/script.js
+++ src/Javascript/JS/script.js
@@ -1,5 +1,2 @@
-//  three stages
-//  development => build process => production
-//  module => third party module  => npm (node package  manager)
-//  build process => bundling(bundle all module into one) => transpiling/polyfilling(babel)(covert the  modern code into es5 code)
-// production => javascript bundle
+import "./shoppingCart";
+console.log("importing module");
---
+++ src/Javascript/JS/shoppingCart.js
@@ -0,0 +1,1 @@
+console.log("exporting module");
