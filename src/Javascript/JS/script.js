a5c3600e1025989df0f58f0f0d08cc7933534b31
ripan <reponmondal007@gmail.com>
2023-11-20 08:22:59 +0000

    Nov 20, 2023, 1:52 PM

---
+++ src/Javascript/JS/script.js
@@ -0,0 +1,4 @@
+//  three stages
+//  development => build process => production
+//  module => third party module  => npm (node package  manager)
+//  build process => bundling(bundle all module into one) => transpiling/polyfilling
