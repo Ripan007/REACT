230ba4500e06fce404a5b2e386e8627df21ca140
ripan <reponmondal007@gmail.com>
2023-11-20 09:21:07 +0000

    Nov 20, 2023, 2:51 PM

--- src/Javascript/JS/script.js
+++
@@ -1,9 +0,0 @@
-import { cart, addToCart } from "./shoppingCart.js";
-
-addToCart("pizza", 9);
-addToCart("laddo", 39);
-addToCart("jamun", 95);
-addToCart("maruti", 95);
-console.log(cart);
-
-//  patient
