74021dfbdb5a5593a7cfdc39af22fbd335d61328
ripan <reponmondal007@gmail.com>
2023-11-20 08:24:03 +0000

    Nov 20, 2023, 1:54 PM

--- src/Javascript/JS/script.js
+++ src/Javascript/JS/script.js
@@ -2,3 +2,4 @@
 //  development => build process => production
 //  module => third party module  => npm (node package  manager)
 //  build process => bundling(bundle all module into one) => transpiling/polyfilling
+// production => javascript bundle
