2a176c670f296dd49416a3f9c1ca37779c11464a
ripan <reponmondal007@gmail.com>
2023-11-20 09:50:55 +0000

    Nov 20, 2023, 3:20 PM

--- src/Javascript/JS/script.js
+++ src/Javascript/JS/script.js
@@ -1,5 +1,16 @@
 "use strict";
-let hasDriverLicense = false;
-const passTest = true;
-if (passTest) hasDriverLicense = true;
-if (hasDriverLicense) console.log("he can drives");
+// let hasDriverLicense = false;
+// const passTest = true;
+// if (passTest) hasDriverLicense = true;
+// if (hasDriverLicense) console.log("he can drives");
+
+// functions  //
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice contain  ${apples} and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(2, 0);
+console.log(appleJuice);
