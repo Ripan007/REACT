c2ac00b95e03bd8d58691ea8e56b121f52254927
ripan <reponmondal007@gmail.com>
2023-11-20 10:19:54 +0000

    Nov 20, 2023, 3:49 PM

--- src/Javascript/JS/script.js
+++ src/Javascript/JS/script.js
@@ -7,15 +7,34 @@
 // functions  //
 //  reusable chunk of code => maintainable code
 //  dry principle => clean code
-function fruitProcessor(apples, oranges) {
+/*function fruitProcessor(apples, oranges) {
   console.log(apples, oranges);
   const juice = `Juice contain  ${apples} and ${oranges} oranges`;
   return juice;
 }
+const appleJuice = fruitProcessor(2, 0);
+console.log(appleJuice);
+*/
 
 //  calling /running /invoking functions
 //  if we return =>  then  we have  to store  => then invoke
 //  arguments => value
 //  parameter  => reference
-const appleJuice = fruitProcessor(2, 0);
-console.log(appleJuice);
+
+// function declaration //
+
+function calAge1(birthYear) {
+  return 2024 - birthYear;
+}
+
+const age1 = calAge1(1995);
+
+// function expressions
+const calAge2 = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const age2 = calAge2(1994);
+console.log(age1, age2);
+
+// function expressions  //
