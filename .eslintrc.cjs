cf96ab662d67e5929b8192fb9159bc2caf3afbab
ripan <reponmondal007@gmail.com>
2023-11-21 16:13:35 +0000

    Nov 21, 2023, 9:43 PM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", "eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parseOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
