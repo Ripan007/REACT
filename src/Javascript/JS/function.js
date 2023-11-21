73a924b2c51f6885f32295eae7a33642ebd52137
ripan <reponmondal007@gmail.com>
2023-11-21 11:08:29 +0000

    Nov 21, 2023, 4:38 PM

--- src/Javascript/JS/function.js
+++ src/Javascript/JS/function.js
@@ -251,4 +251,7 @@ boardPassenger(180, 3);*/
 (function () {
   const header = document.querySelector('h1');
   header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 })();
