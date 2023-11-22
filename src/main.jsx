225f65b7214ad377d6c9f8d0b63d4f6fcf819f9a
ripan <reponmondal007@gmail.com>
2023-11-22 06:05:34 +0000

    Nov 22, 2023, 11:35 AM

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../src/six/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../src/sixpack-prog/App';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
---
+++ src/sixpack-prog/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1>six pack programmer</h1>;
+}
