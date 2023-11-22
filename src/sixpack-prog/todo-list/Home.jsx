c2bc07b48bb125380b75c6b2fe36d383c479e3ca
ripan <reponmondal007@gmail.com>
2023-11-22 07:18:57 +0000

    Nov 22, 2023, 12:48 PM

--- src/sixpack-prog/todo-list/Home.jsx
+++ src/sixpack-prog/todo-list/Home.jsx
@@ -3,9 +3,12 @@ import Task from './Task';
 
 function Home() {
   const [task, setTask] = useState([]);
+  function submitHandler(e) {
+    e.preventDefault();
+  }
   return (
     <div className="container">
-      <form>
+      <form onSubmit={submitHandler}>
         <input type="text" placeholder="enter text" className="title" />
         <textarea name="" id="" className="description"></textarea>
         <button>add</button>
