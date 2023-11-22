c90ec95b51fe79f7193d7e7e3790794358effc21
ripan <reponmondal007@gmail.com>
2023-11-22 11:25:01 +0000

    Nov 22, 2023, 4:55 PM

--- src/sixpack-prog/netflix-clone/components/Home/Home.jsx
+++ src/sixpack-prog/netflix-clone/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="banner"></div>
+    </section>
+  );
 }
 
 export default Home;
