22d89ceef9ab366e19f34298d65c28f79cb53baf
ripan <reponmondal007@gmail.com>
2023-11-22 06:23:17 +0000

    Nov 22, 2023, 11:53 AM

--- src/sixpack-prog/App.jsx
+++ src/sixpack-prog/App.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState(0);
-
-  function clickHandler() {
-    setInputValue(inputValue + 1);
-  }
-  return (
-    <div>
-      <h1>{inputValue}</h1>
-      <input type="text" value={inputValue} placeholder="enter some value" />
-      <button onClick={clickHandler}> +</button>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
