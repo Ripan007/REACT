df1ae5e063f4455a35785e99eb1b91a585183566
ripan <reponmondal007@gmail.com>
2023-11-22 06:57:41 +0000

    Nov 22, 2023, 12:27 PM

--- src/sixpack-prog/todo-list/Task.jsx
+++ src/sixpack-prog/todo-list/Task.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Task() {
-  return <div>Task</div>;
+  return (
+    <div className="task">
+      <div>
+        <h1>title</h1>
+        <p>description</p>
+      </div>
+      <button>+</button>
+    </div>
+  );
 }
 
 export default Task;
