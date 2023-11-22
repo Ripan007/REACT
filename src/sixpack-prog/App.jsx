050458fd87c1c6353937c65c9c8b8e39190b94be
ripan <reponmondal007@gmail.com>
2023-11-22 08:29:39 +0000

    Nov 22, 2023, 1:59 PM

--- src/sixpack-prog/App.jsx
+++ src/sixpack-prog/App.jsx
@@ -3,10 +3,33 @@ import Todo from './todo-list/Todo';
 
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
-const router = createBrowserRouter([{}, {}]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>home page</h1>
+        <Link to="about">aboutpage link</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: (
+      <div>
+        about page
+        <Link to="/">home page</Link>
+      </div>
+    ),
+  },
+]);
 
 function App() {
-  return <>{/* <Todo /> */}</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
