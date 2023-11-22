74567c530efcc5a7105c0ace00fa3c8a8a361a3f
ripan <reponmondal007@gmail.com>
2023-11-22 06:34:55 +0000

    Nov 22, 2023, 12:04 PM

--- src/sixpack-prog/todo-list/Todo.jsx
+++ src/sixpack-prog/todo-list/Todo.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Todo.css';
+import Header from './Header';
+import Home from './Home';
 
 function Todo() {
-  return <div>Todo</div>;
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
 }
 
 export default Todo;
