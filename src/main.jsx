ce4c1102defadbd29d41ad2546c92c49fbdbf9d4
ripan <reponmondal007@gmail.com>
2023-11-22 18:06:51 +0000

    Nov 22, 2023, 11:36 PM

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/sixpack-prog/App';
+import { createContext } from 'react';
+const Context = createContext();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={'ripan'}>
+      <App />
+    </Context.Provider>
   </React.StrictMode>
 );
