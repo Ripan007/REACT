c197b88afad45a8f400b72e72a0a57f3be98957f
ripan <reponmondal007@gmail.com>
2023-11-22 06:42:37 +0000

    Nov 22, 2023, 12:12 PM

--- src/sixpack-prog/todo-list/Home.jsx
+++ src/sixpack-prog/todo-list/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <form>
+        <input type="text" placeholder="enter text" className="title" />
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          className="description"
+        ></textarea>
+        <button>add</button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
