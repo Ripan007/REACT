4523c77012995344d3a14ddc21376872a415bdd8
ripan <reponmondal007@gmail.com>
2023-11-22 07:25:12 +0000

    Nov 22, 2023, 12:55 PM

--- src/sixpack-prog/todo-list/Home.jsx
+++ src/sixpack-prog/todo-list/Home.jsx
@@ -3,14 +3,21 @@ import Task from './Task';
 
 function Home() {
   const [task, setTask] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
   function submitHandler(e) {
     e.preventDefault();
   }
   return (
     <div className="container">
       <form onSubmit={submitHandler}>
-        <input type="text" placeholder="enter text" className="title" />
-        <textarea name="" id="" className="description"></textarea>
+        <input
+          type="text"
+          placeholder="title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <textarea name="" id="" placeholder="description"></textarea>
         <button>add</button>
       </form>
       {task.map(() => (
