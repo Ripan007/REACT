5e863532b884e0109a5e3808d6101121c929c143
ripan <reponmondal007@gmail.com>
2023-11-22 10:42:31 +0000

    Nov 22, 2023, 4:12 PM

--- src/sixpack-prog/netflix-clone/components/Header/Header.jsx
+++ src/sixpack-prog/netflix-clone/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { netflix } from '../../../../assets/netflix-logo.png';
 
 function Header() {
-  return <nav className="header">Header</nav>;
+  return (
+    <nav className="header">
+      <img src={netflix} alt="" />
+    </nav>
+  );
 }
 
 export default Header;
