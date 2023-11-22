dc5fc9c1e7c8098b03016e3894f08cbfd466d486
ripan <reponmondal007@gmail.com>
2023-11-22 06:04:12 +0000

    Nov 22, 2023, 11:34 AM

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../src/ref-portals/App";
+import App from "../src/six/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
