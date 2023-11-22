204599453d341bed81bd29c3488f2a420aaf6ca2
ripan <reponmondal007@gmail.com>
2023-11-22 10:35:20 +0000

    Nov 22, 2023, 4:05 PM

--- src/sixpack-prog/netflix-clone/components/AppNetflix.jsx
+++ src/sixpack-prog/netflix-clone/components/AppNetflix.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './AppNetflix.scss';
-import { createBrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
+import Header from './Header/Header';
 
 function AppNetflix() {
   return (
-    <createBrowserRouter>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </createBrowserRouter>
+    </BrowserRouter>
   );
 }
 
