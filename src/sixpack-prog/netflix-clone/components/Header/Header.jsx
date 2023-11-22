f7150e60a585cee1414dcbbb72b6f030c204882b
ripan <reponmondal007@gmail.com>
2023-11-22 10:57:35 +0000

    Nov 22, 2023, 4:27 PM

--- src/sixpack-prog/netflix-clone/components/Header/Header.jsx
+++ src/sixpack-prog/netflix-clone/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import netflix from '../../../../../src/assets/netflix.png';
 import '../Home/Home.scss';
+import { Link } from 'react-router-dom';
+import { CiSearch } from 'react-icons/ci';
 
 function Header() {
   return (
     <nav className="header">
       <img src={netflix} alt="" />
+      <div>
+        <Link to="/tvshows">tvshows</Link>
+        <Link to="/tvshows">tvshows</Link>
+        <Link to="/tvshows">tvshows</Link>
+        <Link to="/tvshows">tvshows</Link>
+      </div>
+      <CiSearch />
     </nav>
   );
 }
