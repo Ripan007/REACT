9a303b0fad1c8d5061158e58f977c82c73f83eba
ripan <reponmondal007@gmail.com>
2023-11-23 15:28:26 +0000

    Nov 23, 2023, 8:58 PM

--- src/JS/dom-manipulation/guess-no/script.js
+++ src/JS/dom-manipulation/guess-no/script.js
@@ -5,10 +5,13 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
   console.log(guess, typeof guess);
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '☠️ nothing is selected';
+    // when guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'guess correct';
+    // when guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'guess is high';
@@ -18,6 +21,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'you loose the game';
       document.querySelector('.score').textContent = 0;
     }
+    // when guess is low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'guess is low';
