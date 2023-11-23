aced3fc7bf4afa23a654343a1d41652232318170
ripan <reponmondal007@gmail.com>
2023-11-23 15:01:43 +0000

    Nov 23, 2023, 8:31 PM

--- src/JS/dom-manipulation/guess-no/script.js
+++ src/JS/dom-manipulation/guess-no/script.js
@@ -1,7 +1,10 @@
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
   console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = '☠️ nothing is selected';
+  } else if (guess === secretNumber) {
   }
 });
