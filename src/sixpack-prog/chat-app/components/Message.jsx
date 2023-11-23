7669d6687543f84f346a501a3b2b3c5d996ef1d2
ripan <reponmondal007@gmail.com>
2023-11-23 09:18:49 +0000

    Nov 23, 2023, 2:48 PM

--- src/sixpack-prog/chat-app/components/Message.jsx
+++ src/sixpack-prog/chat-app/components/Message.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 
 function Message({ title, uri }) {
   return (
-    <HStack borderRadius="2" bg="gray.100" py="2" px="4">
+    <HStack
+      alignSelf={'flex-end'}
+      borderRadius="md"
+      bg="gray.100"
+      py="2"
+      px="4"
+    >
       <Text>{title}</Text>
       <Avatar src={uri} />
     </HStack>
