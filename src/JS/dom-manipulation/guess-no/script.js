80bc2762ee4e55f25e1e6fa376f617a0cdfa305b
ripan <reponmondal007@gmail.com>
2023-11-23 14:54:25 +0000

    Nov 23, 2023, 8:24 PM

--- src/JS/dom-manipulation/guess-no/script.js
+++ src/JS/dom-manipulation/guess-no/script.js
@@ -2,6 +2,6 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
   console.log(guess, typeof guess);
   if (!guess) {
-    document.querySelector('.message').textContent = '';
+    document.querySelector('.message').textContent = '☠️ nothing is selected';
   }
 });
