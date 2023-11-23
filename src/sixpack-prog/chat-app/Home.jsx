d8054b04943b255fc926231075a530bd42ad0482
ripan <reponmondal007@gmail.com>
2023-11-23 08:56:07 +0000

    Nov 23, 2023, 2:26 PM

--- src/sixpack-prog/chat-app/Home.jsx
+++ src/sixpack-prog/chat-app/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, VStack } from '@chakra-ui/react';
+import { Box, Button, Container, VStack } from '@chakra-ui/react';
 import React from 'react';
 
 function Home() {
@@ -6,7 +6,9 @@ function Home() {
     <Box bg="red.50">
       <Container height="100vh" bg="white">
         <VStack h="full" bg="telegram.100">
-          <button></button>
+          <Button colorScheme="red" w="full">
+            logout
+          </Button>
         </VStack>
       </Container>
     </Box>
