b6fce8f67556fa4bbe5850f574414a8311372446
ripan <reponmondal007@gmail.com>
2023-11-23 15:18:26 +0000

    Nov 23, 2023, 8:48 PM

--- src/JS/dom-manipulation/guess-no/script.js
+++ src/JS/dom-manipulation/guess-no/script.js
@@ -9,12 +9,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'guess correct';
   } else if (guess > secretNumber) {
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector('.message').textContent = 'guess is high';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'you loose the game';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'guess is low';
