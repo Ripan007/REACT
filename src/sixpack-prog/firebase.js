998626b2f87edd7a0d26ae109a78173170fc57ee
ripan <reponmondal007@gmail.com>
2023-11-23 09:37:15 +0000

    Nov 23, 2023, 3:07 PM

---
+++ src/sixpack-prog/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDj824xz8SQNfHoSYHjNcxN0pqQoV9FUo8',
+  authDomain: 'chatapp-aa68f.firebaseapp.com',
+  projectId: 'chatapp-aa68f',
+  storageBucket: 'chatapp-aa68f.appspot.com',
+  messagingSenderId: '659927222551',
+  appId: '1:659927222551:web:8210b6945db2330cbb0f52',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
