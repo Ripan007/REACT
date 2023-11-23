f66ac9d9472479ff9400c243612df408057d3c93
ripan <reponmondal007@gmail.com>
2023-11-23 08:41:15 +0000

    Nov 23, 2023, 2:11 PM

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/sixpack-prog/App';
-import { createContext } from 'react';
-export const Context = createContext();
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Context.Provider value={'ripan'}>
+    <ChakraProvider>
       <App />
-    </Context.Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
