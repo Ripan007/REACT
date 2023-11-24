a3a4eef3a498ce6212d8b06013a3df49e46993de
ripan <reponmondal007@gmail.com>
2023-11-24 11:28:03 +0000

    Nov 24, 2023, 4:58 PM

--- src/JS/dom-manipulation/pig-game/script.js
+++ src/JS/dom-manipulation/pig-game/script.js
@@ -2,8 +2,8 @@
 // selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const score1El = document.querySelector('#score--1');
-const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -21,5 +21,8 @@ btnRoll.addEventListener('click', function () {
   // 2.display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  // 3.check for rolled 1:if true,switch to next player
+  // 3.check for rolled 1
+  if (dice !== 1) {
+    // add dice to current score
+  }
 });
