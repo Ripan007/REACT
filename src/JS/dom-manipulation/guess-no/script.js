1870d11145865ce74f9dab5cb287a0517bfbfbbf
ripan <reponmondal007@gmail.com>
2023-11-24 09:25:23 +0000

    Nov 24, 2023, 2:55 PM

--- src/JS/dom-manipulation/guess-no/script.js
+++ src/JS/dom-manipulation/guess-no/script.js
@@ -33,6 +33,8 @@ if (!guess) {
       score--;
       document.querySelector('.score').textContent = score;
     } else {
+      displayMessage('you lost the game');
+      document.querySelector('.score').textContent = 0;
     }
   }
 }
