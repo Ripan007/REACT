ff8a99f2c7549c662fc520f928d563032bd8d56d
ripan <reponmondal007@gmail.com>
2023-11-24 15:14:28 +0000

    Nov 24, 2023, 8:44 PM

--- src/JS/dom-manipulation/pig-game/script.js
+++ src/JS/dom-manipulation/pig-game/script.js
@@ -13,6 +13,7 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 //  rolling dice functionality
