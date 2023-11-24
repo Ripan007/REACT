f97bf725c8af13f9d4053fa872ce34080d7a1520
ripan <reponmondal007@gmail.com>
2023-11-24 11:12:03 +0000

    Nov 24, 2023, 4:42 PM

--- src/JS/dom-manipulation/pig-game/script.js
+++ src/JS/dom-manipulation/pig-game/script.js
@@ -3,10 +3,13 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.');
+const btnNew = document.querySelector('btn--new');
+const btnRoll = document.querySelector('btn--roll');
+const btnHold = document.querySelector('btn--hold');
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 //  rolling dice functionality
+btnRoll.addEventListener('click', function () {});
