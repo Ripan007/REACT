558bdebdd785e28272d37ac40e2a598ad11a19d9
ripan <reponmondal007@gmail.com>
2023-11-24 10:59:30 +0000

    Nov 24, 2023, 4:29 PM

--- src/JS/dom-manipulation/pig-game/script.js
+++ src/JS/dom-manipulation/pig-game/script.js
@@ -1,5 +1,7 @@
+'use strict';
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-score0El = 0;
-score1El = 0;
-document.querySelector('dice').classList.add('hidden');
+const diceEl = document.querySelector('.dice');
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
