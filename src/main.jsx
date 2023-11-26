fa904a41a90d59b14ac8c3bdcef2bd946593894b
ripan <reponmondal007@gmail.com>
2023-11-26 15:47:28 +0000

    Nov 26, 2023, 9:17 PM

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../src/react-summary/App';
 // import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/react-summary/App.jsx
+++ src/react-summary/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
 function Header() {
   return (
-    <>
+    <header style={header}>
       <h1>react poster</h1>
-    </>
+      <button>new post</button>
+    </header>
   );
 }
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
