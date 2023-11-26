cee95df7552e7f94573078ce1f5ee53bdc29191b
ripan <reponmondal007@gmail.com>
2023-11-26 14:01:27 +0000

    Nov 26, 2023, 7:31 PM

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react';
 
 function App() {
   const [hover, setHover] = useState(false);
+
+  function handleMouseEnter() {
+    setHover(true);
+  }
+  function handleMouseLeave() {
+    setHover(false);
+  }
   return (
-    <>
-      <h1>welcome , add more line</h1>
-    </>
+    <div
+      style={{
+        width: '100px',
+        height: '100px',
+        textAlign: 'center',
+        backgroundColor: hover ? 'yellow' : 'blue',
+        color: hover ? 'black' : 'white',
+        padding: '1rem',
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <p>coding beauty</p>
+    </div>
   );
 }
 
