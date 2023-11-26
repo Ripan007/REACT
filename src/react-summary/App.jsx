6a070405d00fa730244ca85a602b9e6ea4a7fe2d
ripan <reponmondal007@gmail.com>
2023-11-26 15:58:21 +0000

    Nov 26, 2023, 9:28 PM

--- src/react-summary/App.jsx
+++ src/react-summary/App.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import './App.css';
 import { FaRegMessage } from 'react-icons/fa6';
 
+function modal() {
+  function submitForm() {}
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        <input type="text" name="" id="" />
+      </form>
+    </>
+  );
+}
+
 function Header() {
   function handleClick() {}
   return (
