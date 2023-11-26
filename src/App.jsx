cadc1eed8fc99006ea8debed19efcffbeaf7eb87
ripan <reponmondal007@gmail.com>
2023-11-26 13:50:27 +0000

    Nov 26, 2023, 7:20 PM

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,3 @@ function App() {
 }
 
 export default App;
-
-// CONSISTENCY , EXCITEMENT , PATIENT  , LEARNABILITY  , imagination ,focus => MANTRA
-//  ask why , how and when => apply
-//  this few key points  i have  to remember
-// for learning  => https://react.dev/learn => official react source
