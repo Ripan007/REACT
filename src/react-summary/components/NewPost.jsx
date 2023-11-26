668419453dbc38f468ffe29f2dbd75e277215569
ripan <reponmondal007@gmail.com>
2023-11-26 16:49:01 +0000

    Nov 26, 2023, 10:19 PM

--- src/react-summary/components/NewPost.jsx
+++ src/react-summary/components/NewPost.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function NewPost() {
-  return <form></form>;
+  return (
+    <form>
+      <label htmlFor="">text</label>
+      <textarea name="" id="name" cols="30" required rows="10"></textarea>
+    </form>
+  );
 }
 
 export default NewPost;
