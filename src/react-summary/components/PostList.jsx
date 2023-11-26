4035d4321aad9734d7f1ff6a59c3247f99f4ff95
ripan <reponmondal007@gmail.com>
2023-11-26 16:44:12 +0000

    Nov 26, 2023, 10:14 PM

--- src/react-summary/components/PostList.jsx
+++ src/react-summary/components/PostList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Post from './Post';
-
+import style from './PostList.module.css';
 function PostList() {
   return (
-    <ul>
+    <ul className={style.postlist}>
       <Post name="mohit" body="react is awesome" />
       <Post name="janvi" body="javascript is better than react" />
     </ul>
