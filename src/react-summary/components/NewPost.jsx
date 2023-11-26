c63c23bbbcaa48af76c73b63fb6c895910a88280
ripan <reponmondal007@gmail.com>
2023-11-26 16:49:45 +0000

    Nov 26, 2023, 10:19 PM

--- src/react-summary/components/NewPost.jsx
+++ src/react-summary/components/NewPost.jsx
@@ -4,7 +4,9 @@ function NewPost() {
   return (
     <form>
       <label htmlFor="">text</label>
-      <textarea name="" id="name" cols="30" required rows="10"></textarea>
+      <textarea name="" id="body" cols="30" required rows="10"></textarea>
+      <label htmlFor="">name</label>
+      <input type="text" id="name" required />
     </form>
   );
 }
