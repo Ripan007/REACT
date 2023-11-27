07248c4bfc5295cf1fd9530449830d2b5276d819
ripan <reponmondal007@gmail.com>
2023-11-27 14:23:28 +0000

    Nov 27, 2023, 7:53 PM

--- src/BACKEND/node.js
+++ src/BACKEND/node.js
@@ -1,5 +1,12 @@
 import fs from 'fs';
+import { open } from 'node:fs/promises';
 fs.writeFile('message.txt', 'welcome node', err => {
   if (err) throw err;
   console.log('welcome');
 });
+
+const file = await open('./message.txt');
+for await (const line of file.readLines(){
+    console.log(line);
+
+})
