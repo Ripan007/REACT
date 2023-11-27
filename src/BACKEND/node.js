0f3fa9e9c634738600d57b3cd7702ad17fb59f97
ripan <reponmondal007@gmail.com>
2023-11-27 15:40:58 +0000

    Nov 27, 2023, 9:10 PM

--- src/BACKEND/node.js
+++ src/BACKEND/node.js
@@ -32,11 +32,13 @@ console.log(`i am ${superheroes.random()}`);
 */
 
 import inquirer from 'inquirer';
+import qr from 'qr-image';
 
 inquirer
   .prompt([{ message: 'Type in your URL:', name: 'URL' }])
   .then(answers => {
-    console.log(answers);
+    var qr_svg = qr.image('I love QR!', { type: 'svg' });
+    qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
   })
   .catch(error => {
     if (error.isTtyError) {
