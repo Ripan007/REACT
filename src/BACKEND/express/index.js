7ab6b3f0ab3cf1940208c4d9796f4767d9db4e9c
ripan <reponmondal007@gmail.com>
2023-11-27 17:54:36 +0000

    Nov 27, 2023, 11:24 PM

--- src/BACKEND/express/index.js
+++ src/BACKEND/express/index.js
@@ -4,6 +4,12 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('<h1>home page</h1>');
 });
+app.get('/contact', (req, res) => {
+  res.send('<h1>contact page</h1>');
+});
+app.get('/about', (req, res) => {
+  res.send('<h1>about page</h1>');
+});
 app.listen(port, () => {
   console.log(`port is running at ${port}`);
 });
