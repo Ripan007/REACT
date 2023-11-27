cc4b3e8b80c392f98a7f7cdb42a1681a8b417514
ripan <reponmondal007@gmail.com>
2023-11-27 15:36:36 +0000

    Nov 27, 2023, 9:06 PM

--- src/BACKEND/node.js
+++ src/BACKEND/node.js
@@ -36,7 +36,7 @@ import inquirer from 'inquirer';
 inquirer
   .prompt([{ message: 'Type in your URL:', name: 'URL' }])
   .then(answers => {
-    // Use user feedback for... whatever!!
+    console.log(answers);
   })
   .catch(error => {
     if (error.isTtyError) {
