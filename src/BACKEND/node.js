aa3e022769061264a64e6da6ee99b4405cdaf6df
ripan <reponmondal007@gmail.com>
2023-11-27 15:51:47 +0000

    Nov 27, 2023, 9:21 PM

--- src/BACKEND/node.js
+++ src/BACKEND/node.js
@@ -41,6 +41,10 @@ inquirer
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    fs.writeFile('URL.txt', url, err => {
+      if (err) throw err;
+      console.log('the file has been saved');
+    });
   })
   .catch(error => {
     if (error.isTtyError) {
