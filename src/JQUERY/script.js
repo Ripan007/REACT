360ef979cec67ec8158edbc858516e576a895275
ripan <reponmondal007@gmail.com>
2023-11-27 13:18:03 +0000

    Nov 27, 2023, 6:48 PM

--- src/JQUERY/script.js
+++ src/JQUERY/script.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < document.querySelectorAll('button').length; i++) {
   document.querySelectorAll('button')[i].addEventListener('click', function () {
-    document.querySelector('h1').style.color = 'red';
+    document.querySelector('h1').style.color = 'blue';
   });
 }
