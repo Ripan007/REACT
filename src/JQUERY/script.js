2cc264f3635652cd3ecda0d72a80c937df59ae4b
ripan <reponmondal007@gmail.com>
2023-11-27 13:21:20 +0000

    Nov 27, 2023, 6:51 PM

--- src/JQUERY/script.js
+++ src/JQUERY/script.js
@@ -1,5 +1,9 @@
-for (let i = 0; i < document.querySelectorAll('button').length; i++) {
-  document.querySelectorAll('button')[i].addEventListener('click', function () {
-    document.querySelector('h1').style.color = 'blue';
-  });
-}
+// for (let i = 0; i < document.querySelectorAll('button').length; i++) {
+//   document.querySelectorAll('button')[i].addEventListener('click', function () {
+//     document.querySelector('h1').style.color = 'blue';
+//   });
+// }
+
+$('button').click(function () {
+  $('h1').css('color', 'red');
+});
