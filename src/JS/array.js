ec09795adde1be4861002532bbb350b3018a0759
ripan <reponmondal007@gmail.com>
2023-11-29 03:22:05 +0000

    Nov 29, 2023, 8:52 AM

--- src/JS/array.js
+++ src/JS/array.js
@@ -1,13 +1,21 @@
-const friends = ['mohit', 'rajiv', 'gopi', 'kamla'];
-// accessing the  value
+/*rajiv', 'gopi', 'kamla'];
+ accessing the  value
 console.log(friends[0]);
 
 const years = new Array('2000', '2001');
 console.log(years[0]);
 
-// mutating the value
+ mutating the value
 
 friends[0] = 'chanda';
 console.log(friends);
 
-console.log(friends[friends.length - 2]);
+console.log(friends[friends.length - 2]);*/
+
+const calAge = function (birthyear) {
+  return 2024 - birthyear;
+};
+
+const years = [2000, 2001, 1995, 1994];
+const age1 = calAge[0];
+console.log(age1);
