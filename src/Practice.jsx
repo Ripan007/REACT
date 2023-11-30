bc274116a44afe02317d05c2a4706bb664ef1fb3
ripan <reponmondal007@gmail.com>
2023-11-30 15:15:57 +0000

    Nov 30, 2023, 8:45 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -3,9 +3,14 @@ import { useRef } from 'react';
 
 function Practice() {
   const userInput = useRef();
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('user_input--value:', userInput.current.value);
+  }
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" ref={userInput} />
+      <button type="submit">submit</button>
     </form>
   );
 }
