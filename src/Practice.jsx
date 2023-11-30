82dda37669e83db5288e73c76b43c0430572f7ec
ripan <reponmondal007@gmail.com>
2023-11-30 15:01:48 +0000

    Nov 30, 2023, 8:31 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -9,12 +9,20 @@ function Practice() {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <label>email</label>
+      <label>email:</label>
       <input
         type="email"
         name=""
         id=""
+        value={email}
         onChange={() => setEmail(e.target.value)}
+      />
+      <label>password:</label>
+      <input
+        type="password"
+        name=""
+        id=""
+        onChange={e => setPassword(e.target.value)}
       />
     </form>
   );
