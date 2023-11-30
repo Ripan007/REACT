1fabf48ae6354e3d0087c608a80aec7c2d5fbdf1
ripan <reponmondal007@gmail.com>
2023-11-30 14:18:12 +0000

    Nov 30, 2023, 7:48 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Practice() {
-  cns;
-  return <div>Practice</div>;
+  const [inputValue, setInputValue] = useState('');
+
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log('the press txt', inputValue);
+  }
+  return (
+    <form onSubmit={handleSubmitForm}>
+      <label>
+        name:
+        <input
+          type="text"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+      </label>
+    </form>
+  );
 }
 
 export default Practice;
