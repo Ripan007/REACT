97c0a2e95c57295c84dcca07f8882df786d9668d
ripan <reponmondal007@gmail.com>
2023-11-30 13:14:48 +0000

    Nov 30, 2023, 6:44 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // const element = <h1>hello</h1>;
 
-const element = React.createElement('h1', 'hello,world');
+const element = React.createElement('h1', null, 'hello,world');
 function Practice() {
   return <div>{element}</div>;
 }
