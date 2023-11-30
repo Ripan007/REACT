33cbc8087aa490c9578a3fa717a5f6fe4b967e19
ripan <reponmondal007@gmail.com>
2023-11-30 12:33:32 +0000

    Nov 30, 2023, 6:03 PM

--- src/JS/general.js
+++ src/JS/general.js
@@ -11,3 +11,9 @@ const newArr = arr.toSorted();
 console.log(newArr);
 console.log(arr);
 */
+
+/* toSpliced , toReverse*/
+
+const arr = [1, 2, 3];
+const newArr = arr.toReversed();
+console.log(newArr);
