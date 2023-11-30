4a0e554476d82d4c15ead0e342423910641de097
ripan <reponmondal007@gmail.com>
2023-11-30 12:31:48 +0000

    Nov 30, 2023, 6:01 PM

--- src/JS/general.js
+++ src/JS/general.js
@@ -4,8 +4,10 @@ const newArr = arr.sort();
 console.log(arr);
 console.log(newArr);
 */
+
+/* !mutated
 const arr = [1, 5, 2];
 const newArr = arr.toSorted();
-
 console.log(newArr);
 console.log(arr);
+*/
