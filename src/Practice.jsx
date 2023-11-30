a53d2ebecbd3e724760073c9523b65edc0e263f4
ripan <reponmondal007@gmail.com>
2023-11-30 15:40:01 +0000

    Nov 30, 2023, 9:10 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -5,12 +5,6 @@ function Practice() {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
 
-  const validateEmail = value => {
-    if (!value) {
-      setEmailError('email is required');
-    }
-  }else if(!/\S+@\S+\.\S+/.test(value))
-
   return <div>Practice</div>;
 }
 
