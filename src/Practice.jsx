5bea9cc5b955f2007c15fa4c3d1eb54a2f08f242
ripan <reponmondal007@gmail.com>
2023-11-30 13:46:36 +0000

    Nov 30, 2023, 7:16 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+const userDatas = [
+  { name: 'random', age: 23, gender: 'male' },
+  { name: 'random', age: 23 },
+  { name: 'random', age: 23, gender: 'male' },
+];
+
+function UserProfiles() {
+  return <div></div>;
+}
+
 function Practice() {
   return <div>Practice</div>;
 }
