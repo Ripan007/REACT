cdf29bcbed7e75307edd4890790d96fa9f831654
ripan <reponmondal007@gmail.com>
2023-11-30 12:50:18 +0000

    Nov 30, 2023, 6:20 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Practice() {
-  return <div>Practice</div>;
+  const [count, setCount] = useState(0);
+  const [total, setTotal] = useState(1);
+  console.log(count);
+  function handleClick() {
+    setCount(c => c + 1);
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>next</button>
+    </div>
+  );
 }
 
 export default Practice;
