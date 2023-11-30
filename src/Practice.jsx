364493996043437c1fb894130c941985c644e527
ripan <reponmondal007@gmail.com>
2023-11-30 15:13:26 +0000

    Nov 30, 2023, 8:43 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,40 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
 
 function Practice() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('Email:', email, 'Password:', password);
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        email:{' '}
-        <input
-          type="email"
-          name=""
-          id=""
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </label>
-
-      <label>
-        password:{' '}
-        <input
-          type="password"
-          name=""
-          id=""
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </label>
-
-      <button type="submit">submit</button>
-    </form>
-  );
+  return <div>Practice</div>;
 }
 
 export default Practice;
