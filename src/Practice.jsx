0f9b497fe27ce18820b1ed504bd25e273750e6b8
ripan <reponmondal007@gmail.com>
2023-11-30 13:32:09 +0000

    Nov 30, 2023, 7:02 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-function UserProfile() {
-  return <div></div>;
+function UserProfile({ user }) {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+      <p>{user.age}</p>
+      {user.website && (
+        <p>
+          website <a href={user.website}>{user.website}</a>
+        </p>
+      )}
+    </div>
+  );
 }
 function UserProfiles({ users }) {
   if (users === 0) return <p>no user available</p>;
   return (
     <div>
       {users.map((user, index) => (
-        <UserProfile user={user} />
+        <UserProfile key={index} user={user} />
       ))}
     </div>
   );
@@ -21,7 +32,7 @@ const userData = [
   },
   {
     name: 'John',
-    email: '@john ',
+
     age: 23,
     website: 'https://:www.google.com',
   },
