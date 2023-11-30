410370a35d8a7410abc78175994a06dd647816fe
ripan <reponmondal007@gmail.com>
2023-11-30 07:51:55 +0000

    Nov 30, 2023, 1:21 PM

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../src/react-summary/App';
 // import { ChakraProvider } from '@chakra-ui/react';
+dingle;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
