8c44f2bcfcdc53edbff8edb832545de9169a592f
ripan <reponmondal007@gmail.com>
2023-11-30 14:21:04 +0000

    Nov 30, 2023, 7:51 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useRef } from 'react';
 import { useState } from 'react';
 
 function Practice() {
   const [inputValue, setInputValue] = useState('');
+
+  const inputRef = useRef();
+  console.log(inputRef);
 
   function handleSubmitForm(e) {
     e.preventDefault();
@@ -17,7 +21,7 @@ function Practice() {
           value={inputValue}
           onChange={e => setInputValue(e.target.value)}
         /> */}
-        <input type="text"  ref={}/>
+        <input type="text" ref={inputRef} />
       </label>
       <button type="submit">submit</button>
     </form>
