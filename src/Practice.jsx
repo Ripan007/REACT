edeb0a484a88166ffda9474876b566232eb34181
ripan <reponmondal007@gmail.com>
2023-11-30 13:23:52 +0000

    Nov 30, 2023, 6:53 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
-const userData = [{ name: 'John', email: ' ' }];
+const userData = [
+  {
+    name: 'John',
+    email: '@john ',
+    age: 23,
+    website: 'https://:www.google.com',
+  },
+  {
+    name: 'John',
+    email: '@john ',
+    age: 23,
+    website: 'https://:www.google.com',
+  },
+  {
+    name: 'John',
+    email: '@john ',
+    age: 23,
+    website: 'https://:www.google.com',
+  },
+];
 function Practice() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>user profiles</h1>
+    </div>
+  );
 }
 
 export default Practice;
