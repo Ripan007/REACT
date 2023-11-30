825b9bffac295e06126be36eba2070ed71755845
ripan <reponmondal007@gmail.com>
2023-11-30 13:49:58 +0000

    Nov 30, 2023, 7:19 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -6,11 +6,24 @@ const userData = [
   { name: 'random', age: 23, gender: 'male' },
 ];
 
-function UserProfiles() {
-  return <div></div>;
+function UserProfiles(users) {
+  if (users === 0) {
+    return <p>user not available</p>;
+  }
+  return (
+    <div>
+      {users.map((user, index) => {
+        return <UserProfile user={user} />;
+      })}
+    </div>
+  );
 }
-function UserProfile() {
-  return <div></div>;
+function UserProfile({ user }) {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+    </div>
+  );
 }
 function Practice() {
   return (
