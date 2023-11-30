bbe5c03fe30a1d9d0f387acc5fec063f182a8eb5
ripan <reponmondal007@gmail.com>
2023-11-30 15:00:43 +0000

    Nov 30, 2023, 8:30 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { useRef } from 'react';
 import { useState } from 'react';
 
 function Practice() {
-  const [inputValue, setInputValue] = useState('');
-
-  const inputRef = useRef();
-  console.log(inputRef);
-
-  function handleSubmitForm(e) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log('the press txt', inputValue);
   }
   return (
-    <form onSubmit={handleSubmitForm}>
-      <label>
-        name:
-        {/* <input
-          type="text"
-          value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
-        /> */}
-        <input
-          type="text"
-          ref={inputRef}
-          onChange={e => setInputValue(e.target.value)}
-        />
-      </label>
-      <button type="submit">submit</button>
+    <form onSubmit={handleSubmit}>
+      <label>email</label>
+      <input
+        type="email"
+        name=""
+        id=""
+        onChange={() => setEmail(e.target.value)}
+      />
     </form>
   );
 }
