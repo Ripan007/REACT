9029506a87fcbe4e1ec1b0d73de3a550d2f97e87
ripan <reponmondal007@gmail.com>
2023-11-30 12:29:44 +0000

    Nov 30, 2023, 5:59 PM

---
+++ src/JS/general.js
@@ -0,0 +1,11 @@
+/*original array mutted
+const arr = [1, 4, 3];
+const newArr = arr.sort();
+console.log(arr);
+console.log(newArr);
+*/
+const arr = [1, 5, 2];
+const newArr = arr.toSorted();
+
+console.log(newArr);
+console.log(arr);
