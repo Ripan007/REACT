069e5bbf23c8ffdc5b65eadfd5a0fb76b194f8cf
ripan <reponmondal007@gmail.com>
2023-12-02 13:39:14 +0000

    Dec 2, 2023, 7:09 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 
-function Loading
+function Loading() {
+  return <h1>Loading</h1>;
+}
+
+function List() {
+  return <h1>List</h1>;
+}
 
 function Practice() {
-  const[loading,setLoading] =  useState(false)
-  return <div>Practice</div>;
+  const [loading, setLoading] = useState(true);
+  return <div>{loading ? <Loading /> : <List />}</div>;
 }
 
 export default Practice;
