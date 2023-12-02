4365f829ec4622eccae8baaaabff685725c1f90a
ripan <reponmondal007@gmail.com>
2023-12-02 13:29:46 +0000

    Dec 2, 2023, 6:59 PM

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,41 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
 
 function Practice() {
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-
-  const validateEmail = value => {
-    if (!value) {
-      setEmailError('Email is required.');
-    } else if (!/\S+@\S+\.\S+/.test(value)) {
-      setEmailError('Email is invalid.');
-    } else {
-      setEmailError('');
-    }
-  };
-  const handleSubmit = event => {
-    event.preventDefault();
-    const isValid = validateEmail(email);
-    if (isValid) {
-      // Submit form
-    }
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={e => {
-          setEmail(e.target.value);
-          validateEmail(e.target.value);
-        }}
-      />
-      {emailError && <div className="error">{emailError}</div>}
-
-      <button type="submit">submit</button>
-    </form>
-  );
+  return <div>Practice</div>;
 }
 
 export default Practice;
