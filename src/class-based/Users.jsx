74bf9801eed2bcda7d23c7c670a376c4a64019d9
ripan <reponmondal007@gmail.com>
2023-12-03 01:06:37 +0000

    Dec 3, 2023, 6:36 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const DUMMY_USERS = [
+  { id: 'u1  ', name: 'rajiv' },
+  { id: 'u2  ', name: 'mohit' },
+  { id: 'u3  ', name: 'sanjiv' },
+];
+
 function Users() {
   return <div>Users</div>;
 }
