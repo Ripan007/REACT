794a4f5d81e69629b0e2edc075b780c69318cb9b
ripan <reponmondal007@gmail.com>
2023-12-03 08:03:44 +0000

    Dec 3, 2023, 1:33 PM

--- src/class-based/filter/UserFinder.jsx
+++ src/class-based/filter/UserFinder.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'mohit' },
+  { id: 'u2', name: 'rajiv' },
+  { id: 'u3', name: 'gopal' },
+];
+
 function UserFinder() {
+  const [filteredUsers, setFilteredUsers] = useState();
   return <div>UserFinder</div>;
 }
 
