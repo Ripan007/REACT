a335fe46900848d0e252825a6cd54620719bb6a5
ripan <reponmondal007@gmail.com>
2023-12-03 02:21:38 +0000

    Dec 3, 2023, 7:51 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,32 +1,9 @@
-import React, { useState } from 'react';
-import User from './User';
+import { Component } from 'react';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'romit' },
-  { id: 'u2', name: 'rajiv' },
-  { id: 'u3', name: 'promit' },
-];
-
-function Users() {
-  const [showUsers, setShowUsers] = useState(false);
-
-  const toggleUsers = () => {
-    setShowUsers(currState => !currState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map(user => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-  return (
-    <>
-      <button onClick={toggleUsers}> {showUsers ? 'hide' : 'show'}</button>
-      {showUsers && usersList}
-    </>
-  );
+class Users extends Component {
+  render() {
+    return <h1>users</h1>;
+  }
 }
 
 export default Users;
