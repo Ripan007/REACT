c5a351068b17501def0194b9d426f690f025c9d4
ripan <reponmondal007@gmail.com>
2023-12-03 07:37:30 +0000

    Dec 3, 2023, 1:07 PM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,40 +1,6 @@
-import { Component } from 'react';
-import User from './User';
-
-const USERS_DATA = [
-  { id: 'u1', name: 'john' },
-  { id: 'u2', name: 'mohit' },
-  { id: 'u3', name: 'gopi' },
-];
 class Users extends Component {
   constructor() {
     super();
-    this.state = {
-      showUsers: false,
-    };
-  }
-
-  toggleUsersHandler() {
-    this.setState(currState => {
-      return { showUsers: !currState.showUsers };
-    });
-  }
-  render() {
-    const usersList = (
-      <ul>
-        {USERS_DATA.map(user => (
-          <User key={user.id} name={user.name} />
-        ))}
-      </ul>
-    );
-    return (
-      <>
-        <button onClick={this.toggleUsersHandler.bind(this)}>
-          {this.state.showUsers ? 'hide' : 'show'}
-        </button>
-        {this.state.showUsers && usersList}
-      </>
-    );
   }
 }
 
