c18eea76b29f7f29d43e88924aecc3bcf874bd4c
ripan <reponmondal007@gmail.com>
2023-12-03 08:07:02 +0000

    Dec 3, 2023, 1:37 PM

--- src/class-based/filter/UserFinder.jsx
+++ src/class-based/filter/UserFinder.jsx
@@ -9,7 +9,11 @@ const DUMMY_USERS = [
 function UserFinder() {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchTerm, setSearchTerm] = useState('');
-
+  useEffect(() => {
+    setFilteredUsers(
+      DUMMY_USERS.filter(user => user.name.includes(searchTerm))
+    );
+  }, [searchTerm]);
   return <div>UserFinder</div>;
 }
 
