ac06994e5649a1328c5b9fdcf205bf773e8947f0
ripan <reponmondal007@gmail.com>
2023-12-03 00:49:03 +0000

    Dec 3, 2023, 6:19 AM

--- src/class-based/App.jsx
+++ src/class-based/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-  return <div>welcome back class based compnent</div>;
+class App extends React.Component {
+  render() {
+    return <h1>welcome</h1>;
+  }
 }
 
 export default App;
