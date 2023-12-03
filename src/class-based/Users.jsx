877f6f2afc07d4e16d315bfc8a408cadebae2658
ripan <reponmondal007@gmail.com>
2023-12-03 02:40:05 +0000

    Dec 3, 2023, 8:10 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,15 +1,22 @@
 const USERS_DATA = [
   { id: 'u1', name: 'john' },
-  { id: 'u1', name: 'john' },
-  { id: 'u1', name: 'john' },
+  { id: 'u2', name: 'mohit' },
+  { id: 'u3', name: 'deepak' },
 ];
 
 class Users extends Component {
   constructor() {
     super();
+    this.state = {
+      showUsers: false,
+    };
   }
 
-  toggleUsersHandler() {}
+  toggleUsersHandler() {
+    this.setState(currState => {
+      return { showUsers: !currState.showUsers };
+    });
+  }
 
   render() {
     return <h1></h1>;
