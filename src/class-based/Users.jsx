01961c227be8c16580125b0da34a48818716f26d
ripan <reponmondal007@gmail.com>
2023-12-03 01:08:12 +0000

    Dec 3, 2023, 6:38 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DUMMY_USERS = [
   { id: 'u1  ', name: 'rajiv' },
@@ -7,6 +7,11 @@ const DUMMY_USERS = [
 ];
 
 function Users() {
+  const [showUsers, setShowUsers] = useState(true);
+
+  const toggleUsersHandler = () => {
+    setShowUsers(currUsers);
+  };
   return <div>Users</div>;
 }
 
