cef9ce91cdab8ade881a80abb9f4a9bab930f47b
ripan <reponmondal007@gmail.com>
2023-12-03 01:22:25 +0000

    Dec 3, 2023, 6:52 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'romit' },
+  { id: 'u2', name: 'rajiv' },
+  { id: 'u3', name: 'promit' },
+];
 
 function Users() {
+  const [showUsers, setShowUsers] = useState(true);
   return <div>Users</div>;
 }
 
