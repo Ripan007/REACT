5911a462ae4c4e40834ac95b38eb39b8ed3a50ae
ripan <reponmondal007@gmail.com>
2023-12-03 02:26:15 +0000

    Dec 3, 2023, 7:56 AM

--- src/class-based/Users.jsx
+++ src/class-based/Users.jsx
@@ -1,6 +1,17 @@
 import { Component } from 'react';
 
+const USERS_DATA = [
+  { id: 'u1', name: 'john' },
+  { id: 'u2', name: 'mohit' },
+  { id: 'u3', name: 'gopal' },
+];
 class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUser: false,
+    };
+  }
   render() {
     return <h1>users</h1>;
   }
