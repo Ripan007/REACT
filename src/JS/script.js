ba3f0122ef84a3dfb349bb79ddda4b4f710d0de3
ripan <reponmondal007@gmail.com>
2023-12-05 17:11:31 +0000

    Dec 5, 2023, 10:41 PM

---
+++ src/JS/script.js
@@ -0,0 +1,8 @@
+const content = [['react is  awesome'], ['declarative'], ['mdn docs']];
+
+const btnWhyReact = document.getElementById('btn-why-react');
+const btnCoreFeatures = document.getElementById('btn-core-features');
+const btnRelated = document.getElementById('btn-resources');
+const tabContent = document.getElementById('tab-content');
+
+function displayContent(items) {}
