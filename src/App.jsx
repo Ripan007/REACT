ed0003bf2af5b584b2453f6971ae6cd7e598639e
ripan <reponmondal007@gmail.com>
2023-12-05 18:04:33 +0000

    Dec 5, 2023, 11:34 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import TabContent from './TabContent';
+
+import Main from './ReactEssentials/Main';
 
 function App() {
   return (
     <div>
-      <TabContent />{' '}
+      <Main />
     </div>
   );
 }
