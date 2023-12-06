2f53c455508a5458f5a7cd75ce3caa48f09689f2
ripan <reponmondal007@gmail.com>
2023-12-06 12:33:02 +0000

    Dec 6, 2023, 6:03 PM

--- src/ReactEssentials/Practice/Practice.jsx
+++ src/ReactEssentials/Practice/Practice.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Practice() {
   const [isDeleting, setIsDeleting] = useState(false);
@@ -9,12 +9,20 @@ function Practice() {
   function deleteHandler() {
     setIsDeleting(false);
   }
-  return (
-    <div className="container">
+
+  let isWarning;
+
+  if (isDeleting) {
+    isWarning = (
       <div className="dialog__box">
         <h1>are you sure</h1>
         <button onClick={proceedHandler}>proceed</button>
       </div>
+    );
+  }
+  return (
+    <div className="container">
+      {isWarning}
       <button onClick={deleteHandler}>delete</button>
     </div>
   );
