68cd65cad0b0bf735ab6e544e001abad84cd4088
ripan <reponmondal007@gmail.com>
2023-12-06 09:35:59 +0000

    Dec 6, 2023, 3:05 PM

--- src/ReactEssentials/Components/CoreConcept.jsx
+++ src/ReactEssentials/Components/CoreConcept.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function CoreConcept({ title, description }) {
   return (
     <div>
+      <h1>core concepts</h1>
       <h2>{title}</h2>
       <p>{description}</p>
     </div>
