47f2edcdea05cff5cbd4ee9f7d8f19f2cff3cbfb
ripan <reponmondal007@gmail.com>
2023-12-06 08:50:17 +0000

    Dec 6, 2023, 2:20 PM

--- src/ReactEssentials/Main.jsx
+++ src/ReactEssentials/Main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const reactDescription = ['fundamental', 'crucial', 'core'];
+const reactDescription = ['Fundamental', 'Crucial', 'core'];
+
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 function Main() {
-  return <div>Main</div>;
+  const description = reactDescription[getRandomIndex(2)];
+  return (
+    <div>
+      {' '}
+      <b>{description}</b> : react concept require to build any react app
+    </div>
+  );
 }
 
 export default Main;
