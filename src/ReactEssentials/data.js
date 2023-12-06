5737a73087b3d206af03a7730736d9abe6b309d9
ripan <reponmondal007@gmail.com>
2023-12-06 09:07:14 +0000

    Dec 6, 2023, 2:37 PM

---
+++ src/ReactEssentials/data.js
@@ -0,0 +1,7 @@
+const CORE_CONCEPTS = [
+  { title: 'components', description: 'reusable code' },
+  { title: 'components', description: 'reusable code' },
+  { title: 'components', description: 'reusable code' },
+];
+
+export default CORE_CONCEPTS;
