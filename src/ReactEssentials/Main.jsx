0bc73668df18ec951d99bf1461e1c9bfb309c809
ripan <reponmondal007@gmail.com>
2023-12-06 09:13:56 +0000

    Dec 6, 2023, 2:43 PM

--- src/ReactEssentials/Main.jsx
+++ src/ReactEssentials/Main.jsx
@@ -1,45 +1,7 @@
 import React from 'react';
-import CORE_CONCEPTS from './data';
-
-function Header() {
-  const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-  function getRandomIndex(max) {
-    return Math.floor(Math.random() * (max + 1));
-  }
-
-  const description = reactDescription[getRandomIndex(2)];
-
-  return (
-    <header>
-      <h1>react essentials</h1>
-      <b>{description}</b> : react concept require to build any react app
-    </header>
-  );
-}
-
-function CoreConcept({ title, description }) {
-  return (
-    <main>
-      <h2>react coreConcepts</h2>
-      <p>{title}</p>
-      <p>{description}</p>
-    </main>
-  );
-}
 
 function Main() {
-  return (
-    <div>
-      <Header />
-      <CoreConcept title={'react'} description={CORE_CONCEPTS[0]} />
-      <CoreConcept />
-      <CoreConcept />
-      <CoreConcept />
-    </div>
-  );
+  return <div>Main</div>;
 }
 
 export default Main;
-
-//  state props components
