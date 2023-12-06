9039291b81e15dd07f5a2cf8753895caf64660b7
ripan <reponmondal007@gmail.com>
2023-12-06 09:40:03 +0000

    Dec 6, 2023, 3:10 PM

--- src/ReactEssentials/Components/Header.jsx
+++ src/ReactEssentials/Components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
   const description = reactDescription[displayRandomIndex(2)];
   return (
     <div>
-      <img style={{ width: '100px' }} src={mind} alt="mind--set" />
+      <img style={{ maxWidth: '300px' }} src={mind} alt="mind--set" />
       <h2>react essentials</h2>
       <b>{description}:</b> react topics required to build any react app
     </div>
