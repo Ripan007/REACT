309b9a258651b7721b243536f4b06a9058d1e671
ripan <reponmondal007@gmail.com>
2023-12-06 09:38:56 +0000

    Dec 6, 2023, 3:08 PM

--- src/ReactEssentials/Components/Header.jsx
+++ src/ReactEssentials/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import mind from '../../assets/mind.png';
 const reactDescription = ['Important', 'Crucial', 'Core'];
 
 function displayRandomIndex(max) {
@@ -10,6 +10,7 @@ function Header() {
   const description = reactDescription[displayRandomIndex(2)];
   return (
     <div>
+      <img style={{ width: '100px' }} src={mind} alt="mind--set" />
       <h2>react essentials</h2>
       <b>{description}:</b> react topics required to build any react app
     </div>
