bdf92994f6bcc314a93b6b8fa64d47407855f65f
ripan <reponmondal007@gmail.com>
2023-12-06 09:17:14 +0000

    Dec 6, 2023, 2:47 PM

--- src/ReactEssentials/Components/Header.jsx
+++ src/ReactEssentials/Components/Header.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const reactDescription = ['Important', 'Crucial', 'Core'];
+
+function displayRandomIndex(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return <div>Header</div>;
 }
