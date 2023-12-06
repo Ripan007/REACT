c2c605f97e94c01225be576831ede6198077224c
ripan <reponmondal007@gmail.com>
2023-12-06 11:44:35 +0000

    Dec 6, 2023, 5:14 PM

--- src/ReactEssentials/datas.js
+++ src/ReactEssentials/datas.js
@@ -1,5 +1,21 @@
 const EXAMPLES = {
-  components: {},
+  components: {
+    title: 'components',
+    description: 'reusable block of code',
+  },
+  jsx: {
+    title: 'components',
+    description: 'reusable block of code',
+  },
+
+  props: {
+    title: 'components',
+    description: 'reusable block of code',
+  },
+  state: {
+    title: 'components',
+    description: 'reusable block of code',
+  },
 };
 
 export default EXAMPLES;
