2765cf6a3172c633722c9f6555e8f64a233da450
ripan <reponmondal007@gmail.com>
2023-12-06 12:08:56 +0000

    Dec 6, 2023, 5:38 PM

--- src/ReactEssentials/Practice/Practice.jsx
+++ src/ReactEssentials/Practice/Practice.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Practice() {
-  return <div>Practice</div>;
+  return (
+    <div>
+      <div
+        style={{ background: 'red', padding: '1rem', maxWidth: '20opx' }}
+        className="content"
+      >
+        <p>Lorem, ipsum dolor.</p>
+        <button>proceed</button>
+      </div>
+      <button>delete</button>
+    </div>
+  );
 }
 
 export default Practice;
