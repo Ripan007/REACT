5e6beebb603a06683a7ce0efaf33f399713869d7
ripan <reponmondal007@gmail.com>
2023-12-07 03:25:10 +0000

    Dec 7, 2023, 8:55 AM

--- src/Tic-tac-toe/Main.jsx
+++ src/Tic-tac-toe/Main.jsx
@@ -5,7 +5,14 @@ function Main() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li></li>
+          <li>
+            <span className="players-name">player 1</span>
+            <span className="players-symbol">X</span>
+          </li>
+          <li>
+            <span className="players-name">player 2</span>
+            <span className="players-symbol">O</span>
+          </li>
         </ol>
       </div>
     </main>
