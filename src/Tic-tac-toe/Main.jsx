1a37db524a37deffcc9f36419364771b5f37388c
ripan <reponmondal007@gmail.com>
2023-12-08 08:10:27 +0000

    Dec 8, 2023, 1:40 PM

--- src/Tic-tac-toe/Main.jsx
+++ src/Tic-tac-toe/Main.jsx
@@ -3,6 +3,7 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combinations';
+import { GameOver } from './components/GameOver';
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -69,7 +70,7 @@ function Main() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        {winner && <p>you won {winner} </p>}
+        {winner && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
