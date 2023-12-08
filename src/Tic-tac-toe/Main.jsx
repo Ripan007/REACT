5be0a4916aadbd2d1e53b6b7f59550710aa2eba6
ripan <reponmondal007@gmail.com>
2023-12-08 08:46:52 +0000

    Dec 8, 2023, 2:16 PM

--- src/Tic-tac-toe/Main.jsx
+++ src/Tic-tac-toe/Main.jsx
@@ -31,21 +31,24 @@ function Main() {
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
-  let winner;
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol =
-      gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol =
-      gameBoard[combination[2].row][combination[2].column];
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
+  function deriveWinner(gameBoard, players) {
+    let winner;
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol =
+        gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol =
+        gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol =
+        gameBoard[combination[2].row][combination[2].column];
+      if (
+        firstSquareSymbol &&
+        firstSquareSymbol === secondSquareSymbol &&
+        firstSquareSymbol === thirdSquareSymbol
+      ) {
+        winner = players[firstSquareSymbol];
+      }
     }
+    return winner;
   }
 
   const hasDraw = gameTurns.length === 9 && !winner;
