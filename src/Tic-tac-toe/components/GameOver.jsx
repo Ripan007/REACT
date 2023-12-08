927557ca2e2cfec5c27a9d028b2f02ab9152e0af
ripan <reponmondal007@gmail.com>
2023-12-08 08:05:37 +0000

    Dec 8, 2023, 1:35 PM

--- src/Tic-tac-toe/components/GameOver.jsx
+++ src/Tic-tac-toe/components/GameOver.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export const GameOver = () => {
-  return <div>GameOver</div>;
+export const GameOver = ({ winner }) => {
+  return (
+    <div>
+      <h2>game over</h2>
+      <p>{winner}won</p>
+      <p>
+        <button>rematch</button>
+      </p>
+    </div>
+  );
 };
