d9c9eeb2fcaaca5e5802cf5ca3ea57549af0c853
ripan <reponmondal007@gmail.com>
2023-12-09 10:48:34 +0000

    Dec 9, 2023, 4:18 PM

--- src/Ref--Portal/components/Player.jsx
+++ src/Ref--Portal/components/Player.jsx
@@ -4,11 +4,11 @@ export default function Player() {
   const playerName = useRef();
   const [enterPlayerName, setEnterPlayerName] = useState('');
   function handleClick() {
-    se;
+    setEnterPlayerName(playerName.current.value);
   }
   return (
     <section id="player">
-      <h2>welcome unknown entity</h2>
+      <h2>welcome {enterPlayerName ?? 'unknown entity'} </h2>
       <p>
         <input type="text" ref={playerName} />
         <button onClick={handleClick}>set name</button>
