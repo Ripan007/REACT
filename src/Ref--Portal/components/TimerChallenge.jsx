0c182b6b9ded4ab9b1c05349f54f04cc4657b0cf
ripan <reponmondal007@gmail.com>
2023-12-09 11:07:48 +0000

    Dec 9, 2023, 4:37 PM

---
+++ src/Ref--Portal/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button>start challenge</button>
+      </p>
+      <p className="">time is running / Timer </p>
+    </section>
+  );
+}
