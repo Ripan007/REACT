d25bdd7d6ea0550ea42a1d50eca219de1b0b6450
ripan <reponmondal007@gmail.com>
2023-12-09 03:26:09 +0000

    Dec 9, 2023, 8:56 AM

--- src/InvesCal/Main.jsx
+++ src/InvesCal/Main.jsx
@@ -3,10 +3,25 @@ import Header from './components/Header';
 import UserInput from './components/UserInput';
 
 export default function Main() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 6,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(PrevUserInput => {
+      return {
+        ...PrevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} />
     </>
   );
 }
