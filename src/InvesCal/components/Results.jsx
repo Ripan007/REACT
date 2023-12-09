cbbf77692e1d04bf03e0cd401f6084791d041873
ripan <reponmondal007@gmail.com>
2023-12-09 04:14:08 +0000

    Dec 9, 2023, 9:44 AM

--- src/InvesCal/components/Results.jsx
+++ src/InvesCal/components/Results.jsx
@@ -3,6 +3,10 @@ import { calculateInvestmentResults, formatter } from '../Util/investment';
 
 export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
   console.log(resultsData);
   return (
     <>
