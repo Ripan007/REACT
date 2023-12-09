d5c11d43576551db3b5bdebbe99a9c5e9baef9ae
ripan <reponmondal007@gmail.com>
2023-12-09 17:09:15 +0000

    Dec 9, 2023, 10:39 PM

--- class-based-comp/components/Users.jsx
+++ class-based-comp/components/Users.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'rajiv' },
+  { id: 'u1', name: 'rajiv' },
+  { id: 'u1', name: 'rajiv' },
+];
+
 export default function Users() {
   const [showUsers, setShowUsers] = useState(true);
   return <div>Users</div>;
