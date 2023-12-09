cee21f6a3bf4f7426f61475e788d9e21ec828715
ripan <reponmondal007@gmail.com>
2023-12-09 04:21:33 +0000

    Dec 9, 2023, 9:51 AM

--- src/InvesCal/Main.jsx
+++ src/InvesCal/Main.jsx
@@ -24,6 +24,7 @@ export default function Main() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && <p>please enter a valid input data</p>}
       {inputIsValid && <Results input={userInput} />}
     </>
   );
