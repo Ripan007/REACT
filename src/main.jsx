e1b2debf2b95943b7a3e0f20e8cd179b4e9b6159
ripan <reponmondal007@gmail.com>
2023-12-09 02:02:07 +0000

    Dec 9, 2023, 7:32 AM

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
-import Practice from './Practice/Practice';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* <Practice /> */}
   </React.StrictMode>
 );
