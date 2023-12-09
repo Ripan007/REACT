c4cf0e0df53b1362aeb39c43cf6a4edf249ea3cb
ripan <reponmondal007@gmail.com>
2023-12-09 02:52:48 +0000

    Dec 9, 2023, 8:22 AM

---
+++ src/InvesCal/components/UserInput.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function UserInput() {
+  return (
+    <section>
+      <div>
+        <p>
+          <label></label>
+          <input type="number" />
+        </p>
+        <p>
+          <label></label>
+          <input type="number" />
+        </p>
+      </div>
+      <div>
+        <p>
+          <label></label>
+          <input type="number" />
+        </p>
+        <p>
+          <label></label>
+          <input type="number" />
+        </p>
+      </div>
+    </section>
+  );
+}
