9046a12cd4cfc1602ec07d26b30417c5c463bf17
ripan <reponmondal007@gmail.com>
2023-12-09 11:09:25 +0000

    Dec 9, 2023, 4:39 PM

--- src/Ref--Portal/Main.jsx
+++ src/Ref--Portal/Main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
 
 export default function Main() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge />
+      </div>
     </>
   );
 }
