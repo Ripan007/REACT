e681b1a58635ae0b08ab3ef868968650f2099672
ripan <reponmondal007@gmail.com>
2023-12-09 10:39:41 +0000

    Dec 9, 2023, 4:09 PM

--- src/Ref--Portal/components/Player.jsx
+++ src/Ref--Portal/components/Player.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Player() {
+  const [enterPlayerName, setEnterPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(e) {
+    setEnterPlayerName(e.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>welcome unknown entity</h2>
+      <h2>welcome {submitted ? enterPlayerName : 'unknown entity'} </h2>
       <p>
-        <input type="text" />
-        <button>set name</button>
+        <input type="text" value={enterPlayerName} onChange={handleChange} />
+        <button onClick={handleClick}>set name</button>
       </p>
     </section>
   );
