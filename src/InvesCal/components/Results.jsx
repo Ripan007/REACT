8d60a140ea4fc64f7ac1a9fc6cb80272d030f84c
ripan <reponmondal007@gmail.com>
2023-12-09 04:04:04 +0000

    Dec 9, 2023, 9:34 AM

--- src/InvesCal/components/Results.jsx
+++ src/InvesCal/components/Results.jsx
@@ -5,8 +5,31 @@ export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
   console.log(resultsData);
   return (
-    <div>
-      <p>results</p>
-    </div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>year</th>
+            <th>investmentValue</th>
+            <th>interest(year)</th>
+            <th>total interest</th>
+            <th>invested capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultsData.map(yearData => {
+            return (
+              <tr key={yearData.year}>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
