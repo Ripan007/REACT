312535bf56be894a1e5f23507cf92a3d6b38111f
ripan <reponmondal007@gmail.com>
2023-12-09 03:52:30 +0000

    Dec 9, 2023, 9:22 AM

--- src/InvesCal/components/Results.jsx
+++ src/InvesCal/components/Results.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { calculateInvestmentResults } from '../Util/investment';
 
 export default function Results({ input }) {
-  const resultsData = calculateInvestmentResults();
-  console.log(input);
+  const resultsData = calculateInvestmentResults(input);
+  console.log(resultsData);
   return (
     <div>
       <p>results</p>
