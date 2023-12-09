d56d77f46f131a30c2604e9dfbc63093ca1c253d
ripan <reponmondal007@gmail.com>
2023-12-09 03:15:47 +0000

    Dec 9, 2023, 8:45 AM

--- src/InvesCal/Util/investment.js
+++ src/InvesCal/Util/investment.js
@@ -3,4 +3,8 @@ export function calculateInvestmentResults({
   annualInvestment,
   expectedResult,
   duration,
-}) {}
+}) {
+  const annualData = [];
+  let investmentValue = initialInvestment;
+  for (let i = 0; i < duration.length; i++) {}
+}
