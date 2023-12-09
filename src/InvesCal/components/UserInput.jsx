23499a909a6ecf695f7e1e57fe71531dc093f4a5
ripan <reponmondal007@gmail.com>
2023-12-09 03:08:42 +0000

    Dec 9, 2023, 8:38 AM

--- src/InvesCal/components/UserInput.jsx
+++ src/InvesCal/components/UserInput.jsx
@@ -30,17 +30,32 @@ export default function UserInput() {
         </p>
         <p>
           <label>annual investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            required
+            onChange={e => handleChange('annualInvestment', e.target.value)}
+          />
         </p>
       </div>
       <div>
         <p>
           <label>expected return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            required
+            onChange={e => handleChange('expectedReturn', e.target.value)}
+          />
         </p>
         <p>
           <label>return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            value={userInput.duration}
+            required
+            onChange={e => handleChange('duration', e.target.value)}
+          />
         </p>
       </div>
     </section>
