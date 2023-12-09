2560fad6bf699e30507c59fcd018b88b00adb1cb
ripan <reponmondal007@gmail.com>
2023-12-09 02:54:43 +0000

    Dec 9, 2023, 8:24 AM

--- src/InvesCal/Main.jsx
+++ src/InvesCal/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import UserInput from './components/UserInput';
 
 export default function Main() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
