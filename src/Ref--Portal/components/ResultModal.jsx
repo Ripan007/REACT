fbf0b588dd73a0d5770eb8e803690477be0032b1
ripan <reponmondal007@gmail.com>
2023-12-09 16:29:32 +0000

    Dec 9, 2023, 9:59 PM

--- src/Ref--Portal/components/ResultModal.jsx
+++ src/Ref--Portal/components/ResultModal.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function ResultModal({ result, TargetTime }) {
+export default function ResultModal({ result, targetTime }) {
   return (
     <dialog>
       <h1>you {result}</h1>
+      <p>
+        the target time was <strong>{targetTime}</strong>seconds
+      </p>
+      <p>
+        you stop the timer with <strong>x seconds left</strong>
+      </p>
+      <form method="dialog">
+        <button>close</button>
+      </form>
     </dialog>
   );
 }
