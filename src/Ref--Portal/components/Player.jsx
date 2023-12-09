b6f835dab233ce941a1ed1cb1be1371056bee840
ripan <reponmondal007@gmail.com>
2023-12-09 11:03:45 +0000

    Dec 9, 2023, 4:33 PM

--- src/Ref--Portal/components/Player.jsx
+++ src/Ref--Portal/components/Player.jsx
@@ -6,6 +6,7 @@ export default function Player() {
   function handleClick() {
     setEnterPlayerName(playerName.current.value);
   }
+
   return (
     <section id="player">
       <h2>welcome {enterPlayerName ?? 'unknown entity'} </h2>
