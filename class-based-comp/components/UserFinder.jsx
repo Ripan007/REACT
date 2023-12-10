67b14f635c12ad7ac6587a7ae83c52b8582ef02e
ripan <reponmondal007@gmail.com>
2023-12-10 08:57:30 +0000

    Dec 10, 2023, 2:27 PM

--- class-based-comp/components/UserFinder.jsx
+++ class-based-comp/components/UserFinder.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const DUMMY_DATA = [
+const DUMMY_USERS = [
   { id: 'u1', name: 'anjali' },
   { id: 'u2', name: 'gopi' },
   { id: 'u3', name: 'joti' },
 ];
 
 const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_DATA);
+  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
   const [searchItem, setSearchItem] = useState('');
+
+  useEffect(() => {
+    setFilteredUsers(
+      DUMMY_USERS.filter(user => user.name.includes(searchItem))
+    );
+  });
   return <></>;
 };
 
