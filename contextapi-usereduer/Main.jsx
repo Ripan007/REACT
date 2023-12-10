c7d7ccfcd011c12d09d22be82a7d5c1313f301fc
ripan <reponmondal007@gmail.com>
2023-12-10 15:57:03 +0000

    Dec 10, 2023, 9:27 PM

--- contextapi-usereduer/Main.jsx
+++ contextapi-usereduer/Main.jsx
@@ -3,6 +3,14 @@ import React, { useState } from 'react';
 export default function Main() {
   const [shoppingCart, setShoppingCart] = useState({ items: [] });
 
-  function handleAddItemToCart() {}
+  function handleAddItemToCart() {
+    setShoppingCart(prevShoppingCart => {
+      const updatedItems = [...prevShoppingCart.items];
+    });
+    const existingCartItemIndex = updatedItems.findIndex(
+      cartItem => cartItem === id
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+  }
   return <div>Main</div>;
 }
