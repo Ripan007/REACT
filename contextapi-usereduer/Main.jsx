75fc08242c2350b8acb32f375b14ec886ea92718
ripan <reponmondal007@gmail.com>
2023-12-10 16:03:15 +0000

    Dec 10, 2023, 9:33 PM

--- contextapi-usereduer/Main.jsx
+++ contextapi-usereduer/Main.jsx
@@ -11,6 +11,15 @@ export default function Main() {
       cartItem => cartItem === id
     );
     const existingCartItem = updatedItems[existingCartItemIndex];
+    if (existingCartItem) {
+      const updatedItems = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItems;
+    } else {
+      const product = DUMMY_PRODUCTS.find(product => product.id === id);
+    }
   }
   return <div>Main</div>;
 }
