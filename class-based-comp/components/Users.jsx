8d60219288be6296b81f745dd3fba7e4bbec791e
ripan <reponmondal007@gmail.com>
2023-12-10 07:59:39 +0000

    Dec 10, 2023, 1:29 PM

--- class-based-comp/components/Users.jsx
+++ class-based-comp/components/Users.jsx
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 
+const DUMMY_DATA = [
+  { id: 'u1', name: 'anjali' },
+  { id: 'u2', name: 'gopi' },
+  { id: 'u3', name: 'joti' },
+];
+
 export default function Users() {
   const [showUsers, setShowUsers] = useState(false);
-
+  const usersListData = (
+    <ul>
+      {DUMMY_DATA.map(user => (
+        <></>
+      ))}
+    </ul>
+  );
   function showToggleUsers() {}
   return (
     <section>
