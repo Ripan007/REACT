a73f1da1810cb2abac8053202f9749006014a0df
ripan <reponmondal007@gmail.com>
2023-12-11 05:30:04 +0000

    Dec 11, 2023, 11:00 AM

--- Forms/components/Login.jsx
+++ Forms/components/Login.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <form>
+      <h2>login</h2>
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className="control no-margin">
+          <label htmlFor="password">password</label>
+          <input type="password" id="password" name="password" />
+        </div>
+      </div>
+    </form>
+  );
 }
