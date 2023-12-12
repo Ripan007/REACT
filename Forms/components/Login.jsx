62b902af5c4d0236cf03195e1078d06ee2731c21
ripan <reponmondal007@gmail.com>
2023-12-12 01:39:08 +0000

    Dec 12, 2023, 7:09 AM

--- Forms/components/Login.jsx
+++ Forms/components/Login.jsx
@@ -6,13 +6,23 @@ export default function Login() {
     password: '',
   });
 
+  function submitHandler(e) {
+    e.preventDefault();
+  }
+
+  function onChangeHandler() {}
   return (
     <form onSubmit={submitHandler}>
       <h2>login</h2>
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={e => onChangeHandler('', e.target.value)}
+          />
         </div>
         <div className="control-error"></div>
         <div className="control no-margin">
