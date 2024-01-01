5f54b857f616637beb007dbf712f6136ecf15ebd
ripan <reponmondal007@gmail.com>
2024-01-01 13:03:15 +0000

    Jan 1, 2024, 6:33 PM

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -11,13 +11,17 @@ export default function SignUp() {
     }
   return (
     <form onSubmit={submitHandler}>
-        <input type="text" placeholder='first name' name='name' />
-        <input type="email" placeholder='enter email' name='email' />
-        <input type="text" placeholder='last  name' name='last name' />
-       <fieldset>
-        <legend></legend>
-       </fieldset>
-        <button>submit</button>
+      <input type="text" placeholder="first name" name="name" />
+      <input type="email" placeholder="enter email" name="email" />
+      <input type="text" placeholder="last  name" name="last name" />
+      <fieldset>
+        <legend>how do you find us</legend>
+        <input type="checkbox" name="acquisition" />
+        <label htmlFor="">google</label>
+        <input type="checkbox" name="acquisition" />
+        <label htmlFor="">friends</label>
+      </fieldset>
+      <button>submit</button>
     </form>
-  )
+  );
 }
