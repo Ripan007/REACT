627c76f23e6893e313637097fa9fd5c610916599
ripan <reponmondal007@gmail.com>
2024-01-01 17:19:27 +0000

    Jan 1, 2024, 10:49 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [shoppingCart,setShopppingCart] = useState({
-    items:[]
-  })
+  const [shoppingCart, setShopppingCart] = useState({
+    items: [],
+  });
 
-   function handleAddItemToCart(id){
-    setShopppingCart((prevShoppingCart)=>{
-      const updatedItems =  [...prevShoppingCart.items]
-    })
-
-   }
-  return (
-    <>
-
-    </>
-  );
+  function handleAddItemToCart(id) {
+    setShopppingCart(prevShoppingCart => {
+      const updatedItems = [...prevShoppingCart.items];
+      const existingCartItemIndex = updatedItems.findIndex(
+        cartItem => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find(product => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+      return{
+        items:updatedItems
+      }
+    });
+  }
+  return <></>;
 }
-
