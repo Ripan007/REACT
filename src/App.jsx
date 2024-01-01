2be86c1a7fb47c7fb6b7e7a90f17185d3962dc97
ripan <reponmondal007@gmail.com>
2024-01-01 12:54:37 +0000

    Jan 1, 2024, 6:24 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Login from './Login';
+import SignUp from './SignUp';
 
 export default function App() {
   return (
     <>
-      <Login />
+      <SignUp />
     </>
   );
 }
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default function SignUp() {
+
+    function submitHandler(event){
+        event.preventDefault()
+
+        const fd = new FormData();
+        const data = fd.fromEnteries(fd.entries);
+        lconsole.log(data);
+
+    }
   return (
-    <div>SignUp</div>
+    <form onSubmit={submitHandler}>
+        <input type="text" placeholder='enter name' />
+        <input type="email" placeholder='enter email' />
+        <button>submit</button>
+    </form>
   )
 }
