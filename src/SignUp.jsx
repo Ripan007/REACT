5596b749282f5cad3e4f466474c8aa3613ac5e28
ripan <reponmondal007@gmail.com>
2024-01-01 13:06:45 +0000

    Jan 1, 2024, 6:36 PM

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -5,7 +5,9 @@ export default function SignUp() {
     function submitHandler(event){
         event.preventDefault()
         const fd = new FormData(event.target);
+        const acquisitionChannel = fd.getAll('acquisition');
         const data = Object.fromEntries(fd.entries());
+        data.acquisition = acquisitionChannel;
         console.log(data);
 
     }
@@ -16,9 +18,9 @@ export default function SignUp() {
       <input type="text" placeholder="last  name" name="last name" />
       <fieldset>
         <legend>how do you find us</legend>
-        <input type="checkbox" name="acquisition" />
+        <input type="checkbox" name="acquisition" value='google' />
         <label htmlFor="">google</label>
-        <input type="checkbox" name="acquisition" />
+        <input type="checkbox" name="acquisition" value='friend'/>
         <label htmlFor="">friends</label>
       </fieldset>
       <button>submit</button>
