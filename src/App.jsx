a7b19180f81f91611767d651846ef01d36c09c4c
ripan <reponmondal007@gmail.com>
2024-01-01 11:49:30 +0000

    Jan 1, 2024, 5:19 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [enteredValues, setEnteredValues] = useState({
+    name: '',
+    password: '',
+  });
 
-  function submitHandler(event){
-    event.preventDefault()
+  function inputHandleChange(identifier, value) {
+    setEnteredValues(prevValues => ({ ...prevValues, [identifier]: value }));
+  }
 
+  function submitHandler(event) {
+    event.preventDefault();
+    console.log(enteredValues);
   }
   return (
     <>
       <form onSubmit={submitHandler}>
         <label htmlFor="email">email</label>
-        <input type="text" placeholder="enter email" name="email" />
+        <input
+          type="text"
+          placeholder="enter email"
+          name="email"
+          value={enteredValues.email}
+          onChange={event => inputHandleChange('email', event.target.value)}
+        />
         <label htmlFor="password">password</label>
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          value={enteredValues.password}
+          onChange={event => inputHandleChange('password', event.target.value)}
+        />
         <button>submit</button>
       </form>
     </>
   );
 }
-
-
