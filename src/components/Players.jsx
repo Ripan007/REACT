0ecf73f50235169bd57d3e48d3c07654709afedc
ripan <reponmondal007@gmail.com>
2024-01-02 09:04:15 +0000

    Jan 2, 2024, 2:34 PM

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,34 +1,26 @@
 import React, { useState } from 'react';
 
 export default function Players() {
-const [enteredPlayerName,setEnteredplayerName] = useState(null)
-const [submitted,setSubmitted] = useState(false)
+  const [enteredPlayerName, setEnteredplayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
 
-function handleChange(){
+  function handleChange(event) {
+    setEnteredplayerName(event.target.value);
+  }
 
-}
+  function handleClick() {
+    setSubmitted(true)
+  }
 
-
-function handleClick(){
-    
-}
-
-
-   return (
+  return (
     <>
       <section id="players">
-        <h2>welcome unkown entity{show}</h2>
+        <h2>welcome {submitted ? enteredPlayerName : 'unkown entity'}</h2>
         <p>
-          <input type="text" />
-          <button>set name</button>
+          <input type="text" onChange={handleChange} />
+          <button onClick={handleClick}>set name</button>
         </p>
       </section>
     </>
   );
 }
-
-
-
-
-
-
