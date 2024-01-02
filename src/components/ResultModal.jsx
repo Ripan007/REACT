e21f813eee10eadf322a2a6d3f6a24d0c8664487
ripan <reponmondal007@gmail.com>
2024-01-02 10:33:47 +0000

    Jan 2, 2024, 4:03 PM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ResultModal({ result, targetTime }) {
   return (
-    <dialog open>
+    <dialog  className='result-modal'open>
       <h2>you{result}</h2>
       <p>
         the target time was <strong>{targetTime}seconds</strong>
