be6bcee46d6ae64543c828a7b2f4e37cfb049c30
ripan <reponmondal007@gmail.com>
2024-01-02 10:27:24 +0000

    Jan 2, 2024, 3:57 PM

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog>
+      <h2>you{result}</h2>
+      <p>
+        the target time was <strong>{targetTime}seconds</strong>
+      </p>
+      <p>you stopped the timer with <strong>x seconds left</strong></p>
+      <form method='dialog'>
+        <button>close</button>
+      </form>
+    </dialog>
+  );
+}
