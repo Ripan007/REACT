55120febc2907fa928d1e3661e0463c319b1bf87
ripan <reponmondal007@gmail.com>
2024-01-02 09:33:27 +0000

    Jan 2, 2024, 3:03 PM

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TimeChallenge from './components/TimeChallenge';
 
 export default function App() {
   return (
-    <>
+    <div>
       <Players />
       <div id="challenges">
         <TimeChallenge title="easy" TimeChallenge={5}/>
@@ -12,6 +12,6 @@ export default function App() {
         <TimeChallenge title="getting tough" TimeChallenge={5}/>
         <TimeChallenge title="pros only" TimeChallenge={5}/>
       </div>
-    </>
+    </div>
   );
 }
