6a99ec4736c6aea00395dbc08cc1331656fb5234
ripan <reponmondal007@gmail.com>
2024-01-02 09:39:32 +0000

    Jan 2, 2024, 3:09 PM

--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TimeChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
   return (
-    <section className='challenge'>
+    <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>you lost !</p>}
       <p className="challenge-time">
         {targetTime} seconds {targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>start challenge</button>
+        <button onClick={handleStart}>start challenge</button>
       </p>
-      <p>
-        time is  running / time is inactive
-      </p>
+      <p>time is running / time is inactive</p>
     </section>
   );
 }
