3531f3ccdff32f84bcef737ac9a50f698d84faa4
ripan <reponmondal007@gmail.com>
2024-01-02 08:48:32 +0000

    Jan 2, 2024, 2:18 PM

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Players() {
-  return (
+    const [getValues,setGetValues] = useState('')
+    const [show,setShow]= useState('')
+
+    function getUserInput(event){
+        setGetValues(event.target.value)
+
+    }
+
+    function displayData(){
+        setShow(getValues)
+    }
+   return (
     <>
       <section id="players">
-        <h2>welcome unkown entity</h2>
+        <h2>welcome unkown entity{show}</h2>
         <p>
-          <input type="text" />
-          <button>set name</button>
+          <input type="text" onChange={getUserInput}value={getValues} />
+          <button onClick={displayData}>set name</button>
         </p>
       </section>
     </>
