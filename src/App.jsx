e309359b5a8a3994b91859707381d7d12cc88003
ripan <reponmondal007@gmail.com>
2024-01-02 09:31:55 +0000

    Jan 2, 2024, 3:01 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Players from './components/Players';
+import TimeChallenge from './components/TimeChallenge';
 
 export default function App() {
   return (
     <>
       <Players />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimeChallenge title="easy" TimeChallenge={5}/>
+        <TimeChallenge title="not easy" TimeChallenge={10}/>
+        <TimeChallenge title="getting tough" TimeChallenge={5}/>
+        <TimeChallenge title="pros only" TimeChallenge={5}/>
+      </div>
     </>
   );
 }
