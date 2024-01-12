40565b0d4881907346ab4ddf30324f2e0530d232
ripan <reponmondal007@gmail.com>
2024-01-12 16:01:19 +0000

    Jan 12, 2024, 9:31 PM

--- src/React-Essentials/Main.jsx
+++ src/React-Essentials/Main.jsx
@@ -3,10 +3,12 @@ function Header() {
   return <header>
     <img src="" alt="" />
     <h1>react essentials</h1>
-    <p></p>
+    <p>react essentials concept are required for  creating any react app</p>
   </header>;
 }
 
 export default function Main() {
-  return <div>Main</div>;
+  return <div>
+    <Header/>
+  </div>;
 }
