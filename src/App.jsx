960968d605530e9f9b7ff83c419bdd36181147e7
ripan <reponmondal007@gmail.com>
2024-01-12 03:36:13 +0000

    Jan 12, 2024, 9:06 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import React from 'react';
-import Players from './components/Players';
-import TimeChallenge from './components/TimeChallenge';
+import React from 'react'
 
 export default function App() {
   return (
-    <div>
-      <Players />
-      <div id="challenges">
-        <TimeChallenge title="easy" targetTime={1}/>
-        <TimeChallenge title="not easy" targetTime={2}/>
-        <TimeChallenge title="getting tough" targetTime={3}/>
-        <TimeChallenge title="pros only" targetTime={4}/>
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-
-
