1b2056cb9060f5e1dd36392963e45ff3f10d79d3
ripan <reponmondal007@gmail.com>
2024-01-12 17:03:27 +0000

    Jan 12, 2024, 10:33 PM

--- src/React-Essentials/components/CoreConcept.jsx
+++ src/React-Essentials/components/CoreConcept.jsx
@@ -2,8 +2,10 @@
 export  default function CoreConcept({ title, description }) {
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
