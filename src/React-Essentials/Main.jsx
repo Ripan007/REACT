ed8d76698917c1a02c9e848ec5ab81af8fdbdf55
ripan <reponmondal007@gmail.com>
2024-01-12 16:00:15 +0000

    Jan 12, 2024, 9:30 PM

--- src/React-Essentials/Main.jsx
+++ src/React-Essentials/Main.jsx
@@ -1,22 +1,12 @@
-import React from 'react'
-
-const items = ['pant','tshart','kurta']
-
-function  getRandomInt(max){
-    return Math.floor(Math.random()  * max)
-
-
-
+import React from 'react';
+function Header() {
+  return <header>
+    <img src="" alt="" />
+    <h1>react essentials</h1>
+    <p></p>
+  </header>;
 }
 
-    console.log(Math.floor(Math.random() * max));
-
-
-
-
 export default function Main() {
-    const result = items[getRandomInt(2)];
-  return (
-    <div>{result}</div>
-  )
+  return <div>Main</div>;
 }
