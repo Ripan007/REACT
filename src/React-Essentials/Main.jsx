c3dfd49c61f19c2c6b636b7268ca559419a7420d
ripan <reponmondal007@gmail.com>
2024-01-12 16:11:19 +0000

    Jan 12, 2024, 9:41 PM

--- src/React-Essentials/Main.jsx
+++ src/React-Essentials/Main.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 
-const reactDescription =  ['fundamental','crucial','core'];
-function getRandomIndex(max){
-  return Math.floor(Math.random() *  (max + 1))
+const reactDescription = ['fundamental', 'crucial', 'core'];
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
-  const description = reactDescription[getRandomIndex(2)]
+  const description = reactDescription[getRandomIndex(2)];
   return (
-    <header>
-      <img style={{width:'100px',width:'100px'}}
+    <header
+      style={{
+        backgroundColor: 'lightblue',
+        textAlign: 'center',
+        fontFamily: 'monospace',
+        fontSize: 23,
+      }}
+    >
+      <img
+        style={{ width: '100px', width: '100px' }}
         src="https://cdn.pixabay.com/photo/2014/09/19/05/29/person-451713_1280.jpg"
         alt="change"
       />
       <h1>react essentials</h1>
-      <p>{description} react essentials concept are required for creating any react app</p>
+      <p>
+        {description} react essentials concept are required for creating any
+        react app
+      </p>
     </header>
   );
 }
 
 export default function Main() {
-  return <div>
-    <Header/>
-  </div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
