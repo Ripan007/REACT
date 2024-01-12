9a17be1096bb48af40b089870d9056772d5be1d0
ripan <reponmondal007@gmail.com>
2024-01-12 12:46:52 +0000

    Jan 12, 2024, 6:16 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import React, { useState } from 'react';
-import PosList from './components/PosList';
-import NewPost from './components/NewPost';
-import MainHeader from './components/MainHeader';
-
+import React from 'react'
 
 export default function App() {
-    const [modalIsVisible, setModalIsVisible] = useState(false);
-      function hideModalHandler() {
-        setModalIsVisible(false);
-      }
-      function showModalHandler(){
-        setModalIsVisible(true)
-      }
   return (
-    <main>
-      <MainHeader createPost={showModalHandler} />
-
-      <PosList isPosting ={modalIsVisible} hidePost ={hideModalHandler} />
-    </main>
-  );
+    <div>App</div>
+  )
 }
