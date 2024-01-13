4c57acc459441215079701c58eda4c688dcd545d
ripan <reponmondal007@gmail.com>
2024-01-13 05:20:54 +0000

    Jan 13, 2024, 10:50 AM

--- src/React-Essentials/Main.jsx
+++ src/React-Essentials/Main.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-
+import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import '../index.css';
 import TabButton from './components/TabButton';
+import { EXAMPLES } from './data';
 export default function Main() {
   const[selectedTopic,setSelectedTopic] = useState('please select a topic')
 
@@ -34,10 +35,11 @@ export default function Main() {
         <TabButton onSelect={() => handleSelect('prop')}>Prop</TabButton>
         <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
       </menu>
-      <div id='tab-content'></div>
-      {selectedTopic}
+      {}
     </div>
   );
 }
 
 
+
+//   in javascript  the  topic which i need to go through is function , array  , object , rest operator , array desttructing and  many more things , ultimately i want to say that javascriprt is reuiresf fpr react
