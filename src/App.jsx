baf4974b71aa009cc81f82ce3c0812da7106a1de
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 17:27:28 +0000

    May 20, 2024, 10:57 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <h1>welcome to react</h1>
+      <h1>welcome to react ,lets have all the fun</h1>
     </>
   );
 }
