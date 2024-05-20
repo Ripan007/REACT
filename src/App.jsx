8e7af8aa47886b0d57fd50a876e6ec53f4fc10a0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 17:31:11 +0000

    May 20, 2024, 11:01 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const content = [
+  [
+    'React is extremely popular',
+    'It makes building complex, interactive UIs a breeze',
+    "It's powerful & flexible",
+    'It has a very active and versatile ecosystem',
+  ],
+  [
+    'Components, JSX & Props',
+    'State',
+    'Hooks (e.g., useEffect())',
+    'Dynamic rendering',
+  ],
+  [
+    'Official web page (react.dev)',
+    'Next.js (Fullstack framework)',
+    'React Native (build native mobile apps with React)',
+  ],
+];
 
 export default function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
   return (
     <>
       <h1>welcome to react ,lets have all the fun</h1>
