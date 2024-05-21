06de41cc81c3217d2c0be65c738e25764c4294ea
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 05:54:28 +0000

    May 21, 2024, 11:24 AM

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './index.css';
 
 import { CORE_CONCEPTS } from './data';
 import CoreConcepts from './CoreConcepts';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <CoreConcepts {...CORE_CONCEPTS[0]} />
       <CoreConcepts {...CORE_CONCEPTS[1]} />
       <CoreConcepts {...CORE_CONCEPTS[2]} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactImage from './assets/couple.jpg';
+import ReactImage from '../assets/couple.jpg';
 
 const reactDesc = ['xx', 'yy', 'zz'];
 
