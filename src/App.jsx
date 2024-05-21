3870a3332344fd81a64f24d358d46783010c23ad
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 04:56:05 +0000

    May 21, 2024, 10:26 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import React, { useState } from 'react';
-
-const content = [
-  [
-    'React is extremely popular',
-    'It makes building complex, interactive UIs a breeze',
-    "It's powerful & flexible",
-    'It has a very active and versatile ecosystem',
-  ],
-  [
-    'Components, JSX & Props',
-    'State',
-    'Hooks (e.g., useEffect())',
-    'Dynamic rendering',
-  ],
-  [
-    'Official web page (react.dev)',
-    'Next.js (Fullstack framework)',
-    'React Native (build native mobile apps with React)',
-  ],
-];
+import React from 'react';
 
 export default function App() {
-  const [activeContentIndex, setActiveContentIndex] = useState(0);
   return (
     <>
-      <div id="tabs">
-        <menu>
-          <button onClick={() => setActiveContentIndex(0)}>why react ?</button>
-          <button onClick={() => setActiveContentIndex(1)}>
-            core features
-          </button>
-          <button onClick={() => setActiveContentIndex(2)}>
-            related resources
-          </button>
-        </menu>
-        <div id="tab-content">
-          <ul>
-            {content[activeContentIndex].map(item => (
-              <li key={item}>{item}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <h1>welcome to react</h1>
     </>
   );
 }
