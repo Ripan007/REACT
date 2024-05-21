c237b4ebbbbce0c5420d52d50b64148f5bc54d67
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 05:39:59 +0000

    May 21, 2024, 11:09 AM

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,14 @@ function getRanInde(max) {
 
 function CoreConcepts(props) {
   return (
-    <div>
+    <div
+      style={{
+        border: '1px solid black',
+        maxWidth: '100px',
+        padding: '10px',
+        borderRadius: '10px',
+      }}
+    >
       <h1>{props.title}</h1>
       <p>{props.desc}</p>
     </div>
