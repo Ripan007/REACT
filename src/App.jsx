eba83d325aa26e06335883101105c330af9c67ec
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 05:07:08 +0000

    May 21, 2024, 10:37 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+const reactDesc = ['xx', 'yy', 'zz'];
+
+function getRanInde(max) {
+  console.log(Math.floor(Math.random() * (max + 1)));
+  return Math.floor(Math.random() * (max + 1));
+}
 
 export default function App() {
   return (
     <>
-      <h1>welcome to react</h1>
+      <p> {reactDesc[getRanInde(2)]} show the output</p>
     </>
   );
 }
