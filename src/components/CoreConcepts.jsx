942554d161015967ec5aac01f4f3ccb58c4a4e26
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 07:30:08 +0000

    May 21, 2024, 1:00 PM

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,14 +1,16 @@
 export default function CoreConcepts({ title, describe }) {
   return (
-    <div style={{ display: 'flex', gap: '10px' }}>
-      <div
-        style={{
-          border: '1px solid black',
-          // maxWidth: '100px',
-          padding: '10px',
-          borderRadius: '10px',
-        }}
-      >
+    <div
+      style={{
+        border: '1px solid black',
+        // maxWidth: '100px',
+        padding: '10px',
+        borderRadius: '10px',
+        display: 'flex',
+        gap: '20px',
+      }}
+    >
+      <div>
         <h1>{title}</h1>
         <p>{describe}</p>
       </div>
