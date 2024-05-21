47f95e63d9d42a70b4deb28ac414006a06b9826a
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 05:34:19 +0000

    May 21, 2024, 11:04 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './index.css';
 import ReactImage from './assets/couple.jpg';
+import { CORE_CONCEPTS } from './data';
 const reactDesc = ['xx', 'yy', 'zz'];
+
+console.log(CORE_CONCEPTS[0].title);
 
 function getRanInde(max) {
   console.log(Math.floor(Math.random() * (max + 1)));
