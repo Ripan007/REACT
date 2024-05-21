81237865e27db468fc638ca04b9d2620f9f0303b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-21 05:28:17 +0000

    May 21, 2024, 10:58 AM

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,21 @@ function getRanInde(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreConcepts(props) {
+  return (
+    <>
+      <h1>title</h1>
+      <p>description</p>
+    </>
+  );
+}
 export default function App() {
   const descriptiion = reactDesc[getRanInde(2)];
   return (
     <>
       <img src={ReactImage} alt="couple-image" />
       <p> {descriptiion} show the output</p>
+      <CoreConcepts />
     </>
   );
 }
