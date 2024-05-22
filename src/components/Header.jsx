5226114fa3f59d504aedf448cf95ee94a4c2f501
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 04:52:24 +0000

    May 22, 2024, 10:22 AM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 export default function Header() {
+  const reactDescription = ['fundamental', 'core', 'essentials'];
+  function getRandomIndex(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
   return (
     <header>
       <h1>react essentials</h1>
+      <p>
+        {reactDescription[getRandomIndex(3)]} core react concepts you will need
+        for almost any app you are going to build
+      </p>
     </header>
   );
 }
