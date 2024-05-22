1d3cdd887bc1d00c2406a6f7e6c5f90adb1c95e6
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 05:49:14 +0000

    May 22, 2024, 11:19 AM

--- src/data.js
+++ src/data.js
@@ -20,6 +20,6 @@ export const CORE_CONCEPTS = [
   },
 ];
 
-export const EXAMPLES = [
-  { components: { tit: 'jjjjjjjjjjjjjj', desc: 'hhhhhhhhhhhhhhhhhhhhh' } },
-];
+export const EXAMPLES = {
+  components: { tit: 'jjjjjjjjjjjjjj', desc: 'hhhhhhhhhhhhhhhhhhhhh' },
+};
