5bfcd99cf10be3da26993d768fce46f585134e8f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 18:49:42 +0000

    May 23, 2024, 12:19 AM

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
       ];
+      return updatedTurns;
     });
   }
   return (
