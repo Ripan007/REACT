561c0e6bdbccb7e35928f639f07760de17f3e219
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 18:24:12 +0000

    May 22, 2024, 11:54 PM

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,16 @@ export default function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="player1" symbol="x" />
-          <Player initialName="player2" symbol="o" />
+          <Player
+            initialName="player1"
+            symbol="x"
+            isActive={activePlayer === 'x'}
+          />
+          <Player
+            initialName="player2"
+            symbol="o"
+            isActive={activePlayer === 'o'}
+          />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} />
       </div>
