a4d432f2057313a0dc9c072c61c0544c2a296fb1
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 05:11:06 +0000

    May 22, 2024, 10:41 AM

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default function CoreConcept({ title, description }) {
   return (
-    <div style={{ border: '1px solid black', display: 'flex' }}>
+    <div
+      style={{
+        border: '1px solid black',
+        padding: '20px',
+        margin: '10px',
+        borderRadius: '10px',
+      }}
+    >
       <h2>{title}</h2>
       <pre>
         <code>{description}</code>
