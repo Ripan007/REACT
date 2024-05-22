5f95092193c2fa0de129ef8f42d9a8a1efa70e6c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 13:56:27 +0000

    May 22, 2024, 7:26 PM

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let show = <span className="player-name">{name}</span>;
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
         <button>edit</button>
       </span>
