6b11dc08031675dc7643a094fcf1a15d543aea7f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 17:36:08 +0000

    May 22, 2024, 11:06 PM

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing(editing => !editing);
   }
-  let playerName = <span className="player-name">{name}</span>;
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = <input type="text" required value={playerName} />;
   }
   return (
     <li>
