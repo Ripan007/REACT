b1a9384bf4fdbee07197f447da19e00cb1730f13
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 19:16:14 +0000

    May 23, 2024, 12:46 AM

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -9,6 +9,8 @@ export default function GameBoard(onSelectSquare, turns) {
   let gameBoard = initialGameBoard;
   for (const turn of turns) {
     const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
   }
   //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
