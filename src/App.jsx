ca85168b755089c037c1008634d6e1670f3d58d3
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 05:18:23 +0000

    May 22, 2024, 10:48 AM

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './index.css';
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header';
 import { CORE_CONCEPTS } from './data';
+import CoreExample from './components/CoreExample';
 export default function App() {
   return (
     <>
@@ -14,6 +15,15 @@ export default function App() {
               <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
+        </menu>
+      </section>
+      <section id="core-examples">
+        <menu>
+          <h2>examples</h2>
+          <CoreExample>components</CoreExample>
+          <CoreExample></CoreExample>
+          <CoreExample>llllll</CoreExample>
+          <CoreExample>llllll</CoreExample>
         </menu>
       </section>
     </>
