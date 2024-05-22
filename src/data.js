69385046a06f90865cd6d2420ea748326cc838da
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 05:15:11 +0000

    May 22, 2024, 10:45 AM

--- src/data.js
+++ src/data.js
@@ -19,3 +19,9 @@ export const CORE_CONCEPTS = [
       'rambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu',
   },
 ];
+
+export const EXAMPLES = [
+  {
+    components: {},
+  },
+];
