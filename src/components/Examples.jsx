f40408c36dbbb46856866f20fd68e8539f458745
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 10:19:48 +0000

    May 22, 2024, 3:49 PM

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -29,33 +29,30 @@ export default function Examples() {
   }
   return (
     <Section id="examples">
-      <menu>
-        <TabButton
-          isSelected={selectedTopic === 'components'}
-          onClick={() => handleSelect('components')}
-        >
-          Components
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'jsx'}
-          onClick={() => handleSelect('jsx')}
-        >
-          JSX
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'props'}
-          onClick={() => handleSelect('props')}
-        >
-          Props
-        </TabButton>
-        <TabButton
-          isSelected={selectedTopic === 'state'}
-          onClick={() => handleSelect('state')}
-        >
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <TabButton
+        isSelected={selectedTopic === 'components'}
+        onClick={() => handleSelect('components')}
+      >
+        Components
+      </TabButton>
+      <TabButton
+        isSelected={selectedTopic === 'jsx'}
+        onClick={() => handleSelect('jsx')}
+      >
+        JSX
+      </TabButton>
+      <TabButton
+        isSelected={selectedTopic === 'props'}
+        onClick={() => handleSelect('props')}
+      >
+        Props
+      </TabButton>
+      <TabButton
+        isSelected={selectedTopic === 'state'}
+        onClick={() => handleSelect('state')}
+      >
+        State
+      </TabButton>
     </Section>
   );
 }
