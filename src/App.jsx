710a2035f3b09d9769b632c4ef1dcdfc0ee34858
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-22 13:46:46 +0000

    May 22, 2024, 7:16 PM

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ export default function App() {
       <div id="game-container">
         <ol id="players">
           <li>
-            <span className="player-name">player1</span>
-            <span className="player-symbol">x</span>
+            <span className="player">
+              <span className="player-name">player1</span>
+              <span className="player-symbol">x</span>
+              <button>edit</button>
+            </span>
           </li>
           <li>
-            <span className="player-name">player2</span>
-            <span className="player-symbol">o</span>
+            <span className="player">
+              <span className="player-name">player2</span>
+              <span className="player-symbol">o</span>
+              <button>edit</button>
+            </span>
           </li>
         </ol>
         game boards
