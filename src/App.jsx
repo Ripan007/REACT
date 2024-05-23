f13cda014c84c6b2b60c12cc9841c527897499a4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 04:43:28 +0000

    May 23, 2024, 10:13 AM

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,24 @@ import Header from './components/Header';
 import UserInput from './components/UserInput';
 
 export default function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} />
     </>
   );
 }
