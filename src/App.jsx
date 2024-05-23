a890a31a75a524f018923ed4444b08009546119d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 11:52:15 +0000

    May 23, 2024, 5:22 PM

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,19 @@ export default function App() {
 }
 
 /*
- * * STYLING REACT APPS
+ * ******** STYLING REACT APPS ***********
  * static and dynamic styling for pretty apps
     advantage :
     {1.css code is decoupled from jsx code
     2.you write css code as you (maybe) know and (maybe) love it
-    3.css code can be written by another developer who needs only a minimal amount of access to your jsx code
+    3.css code can be written by another developer who needs only
+    a minimal amount of access to your jsx code
     }
     disadvantage:
     {1.you need to know css
-    2.css code is not  scoped to components => css rules may  clash across components(eg.same css class name used in different components)
+    2.css code is not  scoped to components => css rules may  clash
+    across components(eg.same css class name used in different components
+      for different purposes)
     }
  * styling with vanilla css
  * scoping styles with  css modules
