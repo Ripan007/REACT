8887d6ee0fd1679373f162aacd737facdd309fc0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 04:42:46 +0000

    May 23, 2024, 10:12 AM

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,20 +1,6 @@
 import React, { useState } from 'react';
 
 export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput(prevUserInput => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-  }
   return (
     <section id="user-input">
       <div className="input-group">
