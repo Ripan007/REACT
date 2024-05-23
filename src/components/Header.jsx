0fbdecb7864d0587b34303c42ee2100765b523f4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 11:25:09 +0000

    May 23, 2024, 4:55 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <header>
-      <img src="" alt="A canvas" />
+      <img
+        src="https://cdn.pixabay.com/photo/2024/01/06/10/34/ai-generated-8491093_1280.jpg"
+        alt="A canvas"
+      />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
