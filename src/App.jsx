b50a0e1712b0d389e3ae9501daa2d5d5f845f9e4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 03:11:00 +0000

    May 23, 2024, 8:41 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,5 @@
-import React, { useState } from 'react';
-import Player from './components/Player';
-import GameBoard from './components/GameBoard';
-import Log from './Log';
+import React from 'react';
 
 export default function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('x');
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer(currActivePlayer => (currActivePlayer === 'x' ? 'o' : 'x'));
-    setGameTurns(prevTurns => {
-      let currentPlayer = 'x';
-      if (prevTurns.length > 0 && prevTurns[0].player === 'x') {
-        currentPlayer = 'o';
-      }
-      const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-        ...prevTurns,
-      ];
-      return updatedTurns;
-    });
-  }
-  return (
-    <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player
-            initialName="player1"
-            symbol="x"
-            isActive={activePlayer === 'x'}
-          />
-          <Player
-            initialName="player2"
-            symbol="o"
-            isActive={activePlayer === 'o'}
-          />
-        </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-          turns={gameTurns}
-        />
-      </div>
-      <Log />
-    </main>
-  );
+  return <div>App</div>;
 }
--- src/Log.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Log() {
-  return <ol id="log"></ol>;
-}
