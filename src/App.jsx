5d12785fa9ec0835c2580d6bd98e04d94b488981
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 11:46:59 +0000

    May 23, 2024, 5:16 PM

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,11 @@ export default function App() {
 /*
  * * STYLING REACT APPS
  * static and dynamic styling for pretty apps
+    advantage :
+    {1.css code is decoupled from jsx code
+    2.you write css code as you (maybe) know and (maybe) love it
+    3.
+    }
  * styling with vanilla css
  * scoping styles with  css modules
  * css-in-js styling with 'styled components'
