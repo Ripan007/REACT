2570193a9d8a23c7dbe59ed1f0fe09f69b586787
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 05:23:46 +0000

    May 23, 2024, 10:53 AM

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
     setUserInput(prevUserInput => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,5 +4,13 @@ import { calculateInvestmentResults } from '../util/investment';
 export default function Results({ input }) {
   const results = calculateInvestmentResults(input);
   console.log(results);
-  return <div>Results</div>;
+  return (
+    <table id="" result>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+  );
 }
