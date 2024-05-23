3a37ec6fd4658d7c567fa63164e5a2fe678451b1
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 05:38:28 +0000

    May 23, 2024, 11:08 AM

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -21,13 +21,14 @@ export default function Results({ input }) {
             yearData.valueEndOfYear -
             yearData.annualInvestment * yearData.year -
             initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
           return (
             <tr key={yearData.year}>
               <td>{yearData.year}</td>
               <td>{formatter.format(yearData.valueEndOfYear)}</td>
               <td>{formatter.format(yearData.interest)}</td>
-              <td>{yearData.year}</td>
-              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.totalInterest)}</td>
+              <td>{formatter.format(yearData.totalAmountInvested)}</td>
             </tr>
           );
         })}
