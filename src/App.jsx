78fe3ae9c4b665c55f364a837818d999f48530bd
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-23 10:58:02 +0000

    May 23, 2024, 4:28 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import UserInput from './components/UserInput';
-
-import Results from './components/Results';
+import React from 'react';
 
 export default function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput(prevUserInput => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: +newValue,
-      };
-    });
-  }
-  return (
-    <>
-      <Header />
-      <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
-    </>
-  );
+  return <div>App</div>;
 }
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return (
-    <header id="header">
-      <h1>investment calculator</h1>
-    </header>
-  );
-}
