18b10b05ff8ad606c53e91fd1877ce9beeed13e9
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-24 04:46:08 +0000

    May 24, 2024, 10:16 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Player from './components/Player';
+import TimerChallenge from './components/TimerChallenge';
 
 export default function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge />
+      </div>
     </>
   );
 }
