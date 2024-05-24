57f499df05c0649728629086604753e4163d5862
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-24 04:45:28 +0000

    May 24, 2024, 10:15 AM

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button> start challenge</button>
+      </p>
+      <p>time is running /...time is inactive</p>
+    </section>
+  );
+}
