645f157aaf1c74000499f20e2f28dafb76273b10
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-24 04:35:13 +0000

    May 24, 2024, 10:05 AM

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,15 @@ export default function App() {
 advanced dom access and value management
 * accessing dom elements and refs
 * managing values with refs
+{state:{
+  1.causes component re-evaluation(re-execution) when changed
+  2.should be used for values that are reflected in the ui
+  3.should not be used for 'behind the scenes' values that have no direct ui impact
+}}
+{refs:{
+1.do not cause component re-evaluation when changed
+2.can be used to gain direct DOM element access(great for reading values  or accessing certain  browser APIs)
+}}
 * exposing api functions from components
 * detaching dom rendering from jsx structure with portals
 */
