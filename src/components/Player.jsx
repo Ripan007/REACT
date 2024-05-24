5bfe879b6a253f8f1f7b2a5a1910bf3617260ed6
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-24 04:08:10 +0000

    May 24, 2024, 9:38 AM

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,11 @@
+
+
 export default function Player() {
+    const [enteredPlayerName,setEnteredPlayerName]
+
   return (
     <div id="player">
-      <h2>welcome unknown entity</h2>
+      <h2>welcome  'unknown entity' </h2>
       <p>
         <input type="text" />
         <button>set name</button>
