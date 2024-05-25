c9ecc7e12afd193b06752ad65b340db19038345b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 03:44:02 +0000

    May 25, 2024, 9:14 AM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,12 @@
 import React, { forwardRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }) {
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
   return (
     <>
-      <dialog className="result-modal" open>
+      <dialog ref={ref} className="result-modal" open>
         <h2>you {result}</h2>
         <p>
           the target time was <strong>{targetTime} seconds.</strong>
