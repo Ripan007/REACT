3a262cf20a549402959e193f14a5b092c2b36b4e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 02:51:51 +0000

    May 25, 2024, 8:21 AM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 export default function ResultModal({ result, targetTime }) {
   return (
-    <dialog>
-      <h2>you {result}</h2>
-      <p>
-        the target time was <strong>{targetTime} seconds.</strong>
-      </p>
-      <p>
-        you stopped the timer with <strong>x seconds left</strong>
-        <form method="dialog">
-          <button>close</button>
-        </form>
-      </p>
-    </dialog>
+    <>
+      {timerExpired && <ResultModal targetTime={targetTime} />}
+      <dialog>
+        <h2>you {result}</h2>
+        <p>
+          the target time was <strong>{targetTime} seconds.</strong>
+        </p>
+        <p>
+          you stopped the timer with <strong>x seconds left</strong>
+          <form method="dialog">
+            <button>close</button>
+          </form>
+        </p>
+      </dialog>
+    </>
   );
 }
