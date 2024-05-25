1efb49251eaf8c66352ba257966a8562cec42429
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 02:37:29 +0000

    May 25, 2024, 8:07 AM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function ResultModal() {
-  return <div>ResultModal</div>;
+export default function ResultModal({ result, targetTime }) {
+  return (
+    <dialog>
+      <h2>you {result}</h2>
+    </dialog>
+  );
 }
