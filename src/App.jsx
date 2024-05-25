0b087802e6fe7290d29edebb82af660223b180ed
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 14:41:06 +0000

    May 25, 2024, 8:11 PM

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,33 @@ export default function App() {
           quantity: 1,
         });
       }
+      return { items: updatedItems };
     });
-    return { items: updatedItems };
   }
-  return <div>App</div>;
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart(prevShoppingCart => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        item => item.id === productId
+      );
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+      updatedItem.quantity += amount;
+      if (updatedItem.quantity <= 0) {
+        updatedItems.spice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <Shop />
+    </>
+  );
 }
