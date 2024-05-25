ba45012b55b73d463caf206d782e2e11d6e039df
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 14:30:48 +0000

    May 25, 2024, 8:00 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { DUMMY_PRODUCTS } from './dummy-products';
 
 export default function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -7,8 +8,26 @@ export default function App() {
   function handleAddItemToCart() {
     setShoppingCart(prevShoppingCart => {
       const updatedItems = [...prevShoppingCart.items];
-      const existingCartItemIndex = updatedItems.findIndex(item.id);
-      cartItem => cartItem.id === item;
+      const existingCartItemIndex = updatedItems.findIndex(
+        cartItem => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItem[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find(product => product.id === id);
+
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
     });
   }
   return <div>App</div>;
