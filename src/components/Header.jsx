d2e683a9ae1c24976d9794317e79a997bb0d280e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-25 15:29:38 +0000

    May 25, 2024, 8:59 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-export default function Header({ cart }) {
+export default function Header({ cart, onUpdateCartItemQuantity }) {
+  const modal = useRef();
+  const cartQuantity = cart.items.length;
+function handle
   return <div>Header</div>;
 }
