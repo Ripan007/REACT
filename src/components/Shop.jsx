eca49ce4c9b3bb540beb9b1f8bbbda5cd52ab240
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-26 04:00:17 +0000

    May 26, 2024, 9:30 AM

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -6,13 +6,7 @@ export default function Shop({ onAddItemToCart }) {
     <section id="shop">
       <h2>Elegant Clothing For Everyone</h2>
 
-      <ul id="products">
-        {DUMMY_PRODUCTS.map(product => (
-          <li key={product.id}>
-            <Product {...product} onAddToCart={onAddItemToCart} />
-          </li>
-        ))}
-      </ul>
+      <ul id="products"></ul>
     </section>
   );
 }
