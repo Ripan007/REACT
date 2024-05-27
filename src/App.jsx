fae4a0305a7b363d06e41e82ec757753cd8e954f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-27 02:31:56 +0000

    May 27, 2024, 8:01 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Users from './components/Users';
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Users />
+    </div>
+  );
 }
