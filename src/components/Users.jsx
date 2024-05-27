f8425d6e181c7be4dfb79d1fc7db3a9947e8b166
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-27 02:40:12 +0000

    May 27, 2024, 8:10 AM

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 export default function Users() {
+  const [users, setUsers] = useState(true);
+  const toggleUsersHandler = () => {};
   return <div>User</div>;
 }
