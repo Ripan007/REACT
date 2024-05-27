6a333507309c845e79bddb1abe04efeb5d50adad
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-27 23:51:41 +0000

    May 28, 2024, 5:21 AM

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,3 @@ export default function App() {
     </div>
   );
 }
-
-/*
-✈️✈️✈️✈️ Error Boundaries ✈️✈️✈️✈️
-1.A JavaScript error in a part of the UI shouldn’t break the whole app.
- To solve this problem for React users,
- React 16 introduces a new concept of an “error boundary”.
-2.Error boundaries are React components that catch JavaScript
-errors anywhere in their child component tree,
-log those errors, and display a fallback UI instead
-of the component tree that crashed. Error boundaries
-catch errors during rendering, in lifecycle methods,
- and in constructors of the whole tree below them.
- 3.Note that error boundaries only catch errors in the components below
-  them in the tree. An error boundary can’t catch an error within itself.
-  If an error boundary fails trying to render the error message, the error will
-  propagate to the closest error boundary above it. This, too, is similar to how
-  the catch {} block works in JavaScript.
-
-
-
-*/
