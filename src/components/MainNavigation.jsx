2d28d48194a5578f27db5c353d8fad794770471f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 04:30:34 +0000

    May 30, 2024, 10:00 AM

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
 
 export default function MainNavigation() {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              to="/"
-            >
-              home{' '}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              to="/products"
-            >
-              products
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                isActive ? classes.active : undefined
+                            }
+                            to="/"
+                        >
+                            home{' '}
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                isActive ? classes.active : undefined
+                            }
+                            to="/products"
+                        >
+                            products
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
