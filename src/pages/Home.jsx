f713f1c526b24f6d33718dc21db8f5f53f2d9a92
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 03:16:31 +0000

    May 30, 2024, 8:46 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
     <>
       <h1> Home page</h1>
-      <p>Go to <></p>
+      <p>
+        Go to <Link to="/products">the list of products</Link>{' '}
+      </p>
     </>
   );
 }
