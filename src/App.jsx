93ee97adbed00745c160c21868d7c9be6175e045
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 02:49:11 +0000

    May 30, 2024, 8:19 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
+import ProductsPage from './ProductsPage';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 export default function App() {
   return <RouterProvider router={router}></RouterProvider>;
