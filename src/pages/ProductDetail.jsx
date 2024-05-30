451f667a236c2d3f1306febd4b8063026c001182
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 04:55:40 +0000

    May 30, 2024, 10:25 AM

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function ProductDetailsPage() {
+    const params = useParams()
     return (
         <div>
             <h1>product detail page</h1>
+            <p>{params.productId}</p>
         </div>
     )
 }
