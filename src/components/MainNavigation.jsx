8638f41e877ebdfaee6d7dbcf194a19290a96873
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 03:57:44 +0000

    May 30, 2024, 9:27 AM

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MainNavigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            {' '}
+            <Link to="/">home </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/products">products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
