c9c42dd4ccda68a506e3cf7318a928dc47335b37
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 04:41:44 +0000

    May 30, 2024, 10:11 AM

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Home() {
-  return (
-    <>
-      <h1> Home page</h1>
-      <p>
-        Go to <Link to="/products">the list of products</Link>{' '}
-      </p>
-    </>
-  );
+    const navigate = useNavigate()
+    function navigateHandler() {
+        navigate('/products')
+    }
+    return (
+        <>
+            <h1> Home page</h1>
+            <p>
+                Go to <Link to="/products">the list of products</Link>{' '}
+            </p>
+            <p>
+                <button onClick={navigateHandler}>navigate</button>
+            </p>
+        </>
+    )
 }
