30b9690a014dfb5ab5c1457d394799306b8fa260
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 04:54:34 +0000

    May 30, 2024, 10:24 AM

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 export default function ProductsPage() {
-  return <h1>Products page</h1>;
+    return (
+        <>
+            <h1>Products page</h1>;
+            <ul>
+                <li>products 1</li>
+                <li>products 2</li>
+                <li>products 3</li>
+            </ul>
+        </>
+    )
 }
