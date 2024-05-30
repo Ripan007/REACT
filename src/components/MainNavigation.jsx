9bd65afad0e282212a7b223e4697be31cdd39518
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-30 04:31:31 +0000

    May 30, 2024, 10:01 AM

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import classes from './MainNavigation.module.css'
 
 export default function MainNavigation() {
@@ -9,7 +9,7 @@ export default function MainNavigation() {
                 <ul>
                     <li>
                         <NavLink
-                            className={({ isActive }) =>
+                            className={({isActive}) =>
                                 isActive ? classes.active : undefined
                             }
                             to="/"
@@ -19,7 +19,7 @@ export default function MainNavigation() {
                     </li>
                     <li>
                         <NavLink
-                            className={({ isActive }) =>
+                            className={({isActive}) =>
                                 isActive ? classes.active : undefined
                             }
                             to="/products"
