e527f51ef5104209983528a1dc6137274d1683dd
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 13:16:03 +0000

    May 31, 2024, 6:46 PM

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 
 export default function App() {
-
     return (
         <div>
-            {/* <Login /> */}
-            <Signup />
+            <Login />
+            {/* <Signup /> */}
         </div>
     )
 }
