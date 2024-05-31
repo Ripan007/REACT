a55f01108be95d71e3d7ab7db5656be590ccf4c0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 05:31:39 +0000

    May 31, 2024, 11:01 AM

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -9,6 +9,15 @@ export default function Signup() {
                 <label htmlFor="email">email</label>
                 <input type="email" name="email" id="email" />
             </div>
+            <div className="control-row">
+                <div className="control">
+                    <label htmlFor="password">password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div className="control">
+                    <label htmlFor="" className="control-password"></label>
+                </div>
+            </div>
         </form>
     )
 }
