3ca48d5177e725d4a8e6cead8907b56cbbb74fdc
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 02:33:41 +0000

    May 31, 2024, 8:03 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-// pages routes
-import Home from './pages/Home'
-import ProductsPage from './pages/Product'
-import RootLayout from './pages/root'
-import ErrorPage from './pages/Error'
-import ProductDetailsPage from './pages/ProductDetail'
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            { index: true, path: '', element: <Home /> },
-            { path: '/products', element: <ProductsPage /> },
-            { path: '/products/:productId', element: <ProductDetailsPage /> },
-        ],
-    },
-])
 
 export default function App() {
-    return <RouterProvider router={router}></RouterProvider>
+    return <div>App</div>
 }
