e291e10efca62a26040ba0cd44c96722bf39d5bb
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 05:28:27 +0000

    May 31, 2024, 10:58 AM

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function Signup() {
-    return <div>Signup</div>
+    return (
+        <form>
+            <h2>welcome on board</h2>
+            <p>we just need little bit of data for you to get started</p>
+        </form>
+    )
 }
