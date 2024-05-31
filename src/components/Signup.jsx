af990ece556906ffe24b2fe3a815436484bd8356
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 05:29:27 +0000

    May 31, 2024, 10:59 AM

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,6 +5,10 @@ export default function Signup() {
         <form>
             <h2>welcome on board</h2>
             <p>we just need little bit of data for you to get started</p>
+            <div className="control">
+                <label htmlFor="email">email</label>
+                <input type="email" name="email" id="email" />
+            </div>
         </form>
     )
 }
