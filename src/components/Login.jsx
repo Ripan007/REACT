df5317e55f9eadd1d9caf48581559814cbb74c8b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 04:09:00 +0000

    May 31, 2024, 9:39 AM

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,8 +7,16 @@ export default function Login() {
             <div className="control-row">
                 <div className="control no-margin">
                     <label htmlFor="email">email</label>
-                    <input type="email" id="email" />
+                    <input type="email" id="email" name="email" />
                 </div>
+                <div className="control no-margin">
+                    <label htmlFor="password">password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <p className="form-actions">
+                    <button className="button button-flat">reset</button>
+                    <button className="button">login</button>
+                </p>
             </div>
         </form>
     )
