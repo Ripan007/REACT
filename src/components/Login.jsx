06afa10f2df7feef807768d8aa190aa44fed3423
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 04:57:48 +0000

    May 31, 2024, 10:27 AM

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <>
-            <form>
-                <h1>login</h1>
-                Login
-            </form>
-        </>
+        <form>
+            <h1>login</h1>
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div className="control no-margin">
+                    <label htmlFor="password">password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <p className="form-actions">
+                    <button>resest</button>
+                    <button>submit</button>
+                </p>
+            </div>
+        </form>
     )
 }
