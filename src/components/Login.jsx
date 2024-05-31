df3c345e8efde4d6e053b78666140a72ba5ff851
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-31 05:18:57 +0000

    May 31, 2024, 10:48 AM

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import { useRef } from 'react'
 
 export default function Login() {
+    const email = useRef()
+    const password = useRef()
+
     function handleSubmit(event) {
         event.preventDefault()
-        console.log(enteredValues)
+        const enteredEmail = email.current.value
+        const eneteredPassword = password.current.value
+        console.log(enteredEmail, eneteredPassword)
     }
 
     return (
@@ -12,11 +17,16 @@ export default function Login() {
             <div className="control-row">
                 <div className="control no-margin">
                     <label htmlFor="email">email</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" ref={email} />
                 </div>
                 <div className="control no-margin">
                     <label htmlFor="password">password</label>
-                    <input type="password" name="password" id="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        ref={password}
+                    />
                 </div>
                 <p className="form-actions">
                     <button className="button button-flat">resest</button>
