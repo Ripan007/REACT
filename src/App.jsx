b7007faad7d62a505e264b74b720a4da3fc7aa45
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 03:43:24 +0000

    Jun 2, 2024, 9:13 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accordian from './components/Accordian/Accordian'
+import AccordianItem from './components/Accordian/AccordianItem'
 
 export default function App() {
     return (
@@ -7,7 +8,20 @@ export default function App() {
             <main>
                 <section>
                     <h2>why work with us ?</h2>
-                    <Accordian className="accordian"></Accordian>
+                    <Accordian className="accordian">
+                        <AccordianItem tille="first item">
+                            <article>
+                                <p>shows the first item</p>
+                                <p>show some duplicate item</p>
+                            </article>
+                        </AccordianItem>
+                        <AccordianItem tille="first item">
+                            <article>
+                                <p>shows the first item</p>
+                                <p>show some duplicate item</p>
+                            </article>
+                        </AccordianItem>
+                    </Accordian>
                 </section>
             </main>
         </>
