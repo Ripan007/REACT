5c41ce23f6dea09205d28d1223e39e6f5dc0b014
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 02:30:59 +0000

    Jun 2, 2024, 8:00 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
-import Login from './components/Login'
-import Signup from './components/Signup'
-import StateLogin from './components/StateLogin'
 
 export default function App() {
-    return (
-        <div>
-            <Login />
-            {/* <Signup /> */}
-            {/* <StateLogin /> */}
-        </div>
-    )
+    return <div>App</div>
 }
