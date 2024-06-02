ec8d9a8bc5420ada0e41706685c86a7ac5194bc5
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 03:41:02 +0000

    Jun 2, 2024, 9:11 AM

--- src/components/Accordian/AccordianItem.jsx
+++ src/components/Accordian/AccordianItem.jsx
@@ -1,1 +1,8 @@
-export default function AccordianItem() {}
+export default function AccordianItem({ className, title, children }) {
+    return (
+        <li className={className}>
+            <h3>{title}</h3>
+            {children}
+        </li>
+    )
+}
