ae3ddf41e3bca6a4951e9ea3cca126903e96ed69
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 03:58:33 +0000

    Jun 2, 2024, 9:28 AM

--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
 export default function Accordian({ className, children }) {
-    return (
-        <ul className={className}>
-            <section>{children}</section>
-        </ul>
-    )
+    return <ul className={className}>{children}</ul>
 }
