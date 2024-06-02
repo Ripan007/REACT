35447975e025e0d7ed926e7b8a51c1721a89c5c7
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 03:57:36 +0000

    Jun 2, 2024, 9:27 AM

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,34 @@ import AccordianItem from './components/Accordian/AccordianItem'
 export default function App() {
     return (
         <>
-            <Accordian className="accordion">
-                <AccordianItem title="first" className="accordion-item">
-                    <article>
-                        <p>show first some randon data</p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Voluptas, eveniet.
-                        </p>
-                    </article>
-                </AccordianItem>
-                <AccordianItem title="second" className="accordion-item">
-                    <article>
-                        <p>show first some randon data</p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Voluptas, eveniet.
-                        </p>
-                    </article>
-                </AccordianItem>
-            </Accordian>
+            <main>
+                <section>
+                    <Accordian className="accordion">
+                        <AccordianItem title="first" className="accordion-item">
+                            <article>
+                                <p>show first some randon data</p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit. Voluptas, eveniet.
+                                </p>
+                            </article>
+                        </AccordianItem>
+                        <AccordianItem
+                            title="second"
+                            className="accordion-item">
+                            <article>
+                                <p>show first some randon data</p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipisicing elit. Voluptas, eveniet.
+                                </p>
+                            </article>
+                        </AccordianItem>
+                    </Accordian>
+                </section>
+            </main>
         </>
     )
 }
+
+//
--- src/components/Accordian/AccordianItem.jsx
+++ src/components/Accordian/AccordianItem.jsx
@@ -4,7 +4,7 @@ export default function AccordianItem({ className, title, children }) {
     return (
         <li className={className}>
             <h3>{title}</h3>
-            {children}
+            <div>{children}</div>
         </li>
     )
 }
