b70b7615a024f30ff19c0addc8aa3bfccf9952ef
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-02 03:52:34 +0000

    Jun 2, 2024, 9:22 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import Accordian from './components/Accordian/Accordian'
+import AccordianItem from './components/Accordian/AccordianItem'
+
 export default function App() {
-    return <></>
+    return (
+        <>
+            <Accordian className="accordion">
+                <AccordianItem></AccordianItem>
+            </Accordian>
+        </>
+    )
 }
