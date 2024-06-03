94d5029aa4b0a07fcb7314bb87750223ef85a063
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 13:06:38 +0000

    Jun 3, 2024, 6:36 PM

--- src/components/Accordian/AccordianContent.jsx
+++ src/components/Accordian/AccordianContent.jsx
@@ -1,14 +1,14 @@
 import { useAccordianContext } from './Accordion'
 
-export default function AccordionContent({ id, className, children }) {
-    const { openItemId } = useAccordianContext()
-    const isOpen = openItemId === id
-    return (
-        <div
-            className={
-                isOpen ? `${className ?? ''}open ` : `${className ?? ''} close`
-            }>
-            {children}
-        </div>
-    )
+export default function AccordionContent({ className, children }) {
+   const { openItemId } = useAccordianContext()
+   const isOpen = openItemId === id
+   return (
+      <div
+         className={
+            isOpen ? `${className ?? ''}open ` : `${className ?? ''} close`
+         }>
+         {children}
+      </div>
+   )
 }
