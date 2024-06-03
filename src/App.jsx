c766fef1f2e07f16792c90b52cb01d5a5713239b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 05:54:00 +0000

    Jun 3, 2024, 11:24 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
 import Accordion from './components/Accordian/Accordion'
 
 export default function App() {
-    return (
-        <>
-            <main>
-                <section>
-                    <Accordion className="accordion">
-                        <Accordion.Item className="accordion-item">
-                            <Accordion.Title className="accordion-item-title">
-                                we got 20 years of experience
-                            </Accordion.Title>
-                            <Accordion.Content className="accordion-item-content">
-                                <article>
-                                    <p>show experience data of the user</p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Voluptas, eveniet.
-                                    </p>
-                                </article>
-                            </Accordion.Content>
-                        </Accordion.Item>
-                        <Accordion.Item
-                            id={'local-guides'}
-                            className="accordion-item">
-                            <Accordion.Title className="accordion-item-title">
-                                what you got from it ?
-                            </Accordion.Title>
-                            <Accordion.Content className="accordion-item-content">
-                                <article>
-                                    <p>show accordion random data</p>
-                                    <p>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Voluptas, eveniet.
-                                    </p>
-                                </article>
-                            </Accordion.Content>
-                        </Accordion.Item>
-                    </Accordion>
-                </section>
-            </main>
-        </>
-    )
+   return (
+      <>
+         <main>
+            <section>
+               <Accordion className="accordion">
+                  <Accordion.Item className="accordion-item">
+                     <Accordion.Title className="accordion-item-title">
+                        we got 20 years of experience
+                     </Accordion.Title>
+                     <Accordion.Content className="accordion-item-content">
+                        <article>
+                           <p>show experience data of the user</p>
+                           <p>
+                              Lorem ipsum dolor sit amet, consectetur
+                              adipisicing elit. Voluptas, eveniet.
+                           </p>
+                        </article>
+                     </Accordion.Content>
+                  </Accordion.Item>
+                  <Accordion.Item
+                     id={'local-guides'}
+                     className="accordion-item">
+                     <Accordion.Title className="accordion-item-title">
+                        what you got from it ?
+                     </Accordion.Title>
+                     <Accordion.Content className="accordion-item-content">
+                        <article>
+                           <p>show accordion random data</p>
+                           <p>
+                              Lorem ipsum dolor sit amet, consectetur
+                              adipisicing elit. Voluptas, eveniet.
+                           </p>
+                        </article>
+                     </Accordion.Content>
+                  </Accordion.Item>
+               </Accordion>
+            </section>
+         </main>
+      </>
+   )
 }
