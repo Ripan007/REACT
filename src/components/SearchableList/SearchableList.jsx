9387f985446bb921b41cc5164da009701ed74d79
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 14:06:26 +0000

    Jun 3, 2024, 7:36 PM

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 export default function SearchableList({ items }) {
     const [searchTerm, setSearchTerm] = useState()
+    function handleChange() {}
     return (
         <div className="searchable-list">
-            <input type="search" placeholder="search" />
+            <input type="search" placeholder="search" onChange={handleChange} />
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item.toString()}</li>
