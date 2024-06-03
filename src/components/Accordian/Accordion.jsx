6b2b5b91ed3b4aa6e4279b9628874202277cc376
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 12:52:22 +0000

    Jun 3, 2024, 6:22 PM

--- src/components/Accordian/Accordion.jsx
+++ src/components/Accordian/Accordion.jsx
@@ -5,31 +5,31 @@ import AccordionContent from './AccordianContent'
 
 const AccordionContext = createContext()
 export function useAccordianContext() {
-    const ctx = useContext(AccordionContext)
-    if (!ctx) {
-        throw new Error(
-            'Accordian related component must be wrapped  by <Accordian>.'
-        )
-    }
-    return ctx
+   const ctx = useContext(AccordionContext)
+   if (!ctx) {
+      throw new Error(
+         'Accordian related component must be wrapped  by <Accordian>.'
+      )
+   }
+   return ctx
 }
 
 export default function Accordion({ className, children }) {
-    const [openItemId, setOpenItemId] = useState('')
+   const [openItemId, setOpenItemId] = useState('')
 
-    function toggleItem(id) {
-        setOpenItemId(prevId => (prevId === id ? null : id))
-    }
+   function toggleItem(id) {
+      setOpenItemId(prevId => (prevId === id ? null : id))
+   }
 
-    const contextValue = {
-        openItemId: openItemId,
-        toggleItem,
-    }
-    return (
-        <AccordionContext.Provider value={contextValue}>
-            <ul className={className}>{children}</ul>
-        </AccordionContext.Provider>
-    )
+   const contextValue = {
+      openItemId: openItemId,
+      toggleItem,
+   }
+   return (
+      <AccordionContext.Provider value={contextValue}>
+         <ul className={className}>{children}</ul>
+      </AccordionContext.Provider>
+   )
 }
 
 Accordion.Item = AccordionItem
