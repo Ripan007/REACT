95cb97fa0bc9f5dda2e43bf0b71924446d82eaab
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 02:42:10 +0000

    Jun 3, 2024, 8:12 AM

--- src/components/Accordian/AccordianItem.jsx
+++ src/components/Accordian/AccordianItem.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useAccordianContext } from './Accordian'
 
-export default function AccordianItem({ className, title, children }) {
+export default function AccordianItem({ id, className, title, children }) {
     const { openItemId, openItem, closeItem } = useAccordianContext()
+
     return (
         <li className={className}>
             <h3>{title}</h3>
