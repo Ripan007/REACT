dadcdcd1f596824a20ed5942a7d3af90897b0947
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 13:15:56 +0000

    Jun 3, 2024, 6:45 PM

--- src/components/Accordian/AccordianContent.jsx
+++ src/components/Accordian/AccordianContent.jsx
@@ -1,7 +1,9 @@
 import { useAccordianContext } from './Accordion'
+import { useAccordionItemContext } from './AccordionItem'
 
 export default function AccordionContent({ className, children }) {
    const { openItemId } = useAccordianContext()
+   const id = useAccordionItemContext()
    const isOpen = openItemId === id
    return (
       <div
