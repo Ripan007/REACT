f65daf704ed4d63a3562ec6cda033f2243a1c03d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 02:47:58 +0000

    Jun 3, 2024, 8:17 AM

--- src/components/Accordian/AccordianItem.jsx
+++ src/components/Accordian/AccordianItem.jsx
@@ -4,9 +4,16 @@ import { useAccordianContext } from './Accordian'
 export default function AccordianItem({ id, className, title, children }) {
     const { openItemId, openItem, closeItem } = useAccordianContext()
     const isOpen = openItemId === id
+    function handleClick() {
+        if (isOpen) {
+            closeItem()
+        } else {
+            openItem(id)
+        }
+    }
     return (
         <li className={className}>
-            <h3>{title}</h3>
+            <h3 onClick={handleClick}>{title}</h3>
             <div className={isOpen ? 'open' : undefined}>{children}</div>
         </li>
     )
