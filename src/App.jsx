092fb816c96aed23e80ada4aa1123141d87c59fb
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 13:51:32 +0000

    Jun 3, 2024, 7:21 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
 import Accordion from './components/Accordian/Accordion'
+import SearchableList from './components/SearchableList/SearchableList'
 
 export default function App() {
-   return (
-      <>
-         <main>
-            <section>
-               <Accordion className="accordion">
-                  <Accordion.Item className="accordion-item" id={'experience'}>
-                     <Accordion.Title className="accordion-item-title">
-                        we got 20 years of experience
-                     </Accordion.Title>
-                     <Accordion.Content className="accordion-item-content">
-                        <article>
-                           <p>show experience data of the user</p>
-                           <p>
-                              Lorem ipsum dolor sit amet, consectetur
-                              adipisicing elit. Voluptas, eveniet.
-                           </p>
-                        </article>
-                     </Accordion.Content>
-                  </Accordion.Item>
-                  <Accordion.Item
-                     id={'local-guides'}
-                     className="accordion-item">
-                     <Accordion.Title className="accordion-item-title">
-                        what you got from it ?
-                     </Accordion.Title>
-                     <Accordion.Content className="accordion-item-content">
-                        <article>
-                           <p>show accordion random data</p>
-                           <p>
-                              Lorem ipsum dolor sit amet, consectetur
-                              adipisicing elit. Voluptas, eveniet.
-                           </p>
-                        </article>
-                     </Accordion.Content>
-                  </Accordion.Item>
-               </Accordion>
-            </section>
-         </main>
-      </>
-   )
+    return (
+        <>
+            <main>
+                <section>
+                    <Accordion className="accordion">
+                        <Accordion.Item
+                            className="accordion-item"
+                            id={'experience'}>
+                            <Accordion.Title className="accordion-item-title">
+                                we got 20 years of experience
+                            </Accordion.Title>
+                            <Accordion.Content className="accordion-item-content">
+                                <article>
+                                    <p>show experience data of the user</p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Voluptas, eveniet.
+                                    </p>
+                                </article>
+                            </Accordion.Content>
+                        </Accordion.Item>
+                        <Accordion.Item
+                            id={'local-guides'}
+                            className="accordion-item">
+                            <Accordion.Title className="accordion-item-title">
+                                what you got from it ?
+                            </Accordion.Title>
+                            <Accordion.Content className="accordion-item-content">
+                                <article>
+                                    <p>show accordion random data</p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Voluptas, eveniet.
+                                    </p>
+                                </article>
+                            </Accordion.Content>
+                        </Accordion.Item>
+                    </Accordion>
+                </section>
+                <section>
+                    <SearchableList />
+                </section>
+            </main>
+        </>
+    )
 }
