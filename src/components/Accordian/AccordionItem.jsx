462fcd0e4d9e2d5a5e086185a9632c470b1e1b52
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 06:30:56 +0000

    Jun 3, 2024, 12:00 PM

--- src/components/Accordian/AccordionItem.jsx
+++ src/components/Accordian/AccordionItem.jsx
@@ -1,6 +1,15 @@
+import { createContext, useContext } from 'react'
+
+const AccordionItemContext = createContext()
+export function useAccordionItemContext() {
+   const ctx = useContext(AccordionItemContext)
+   if (!ctx) {
+      throw new Error('')
+   }
+}
 export default function AccordionItem({ id, className, children }) {
    return (
-      <AccordionItemContext.Provider>
+      <AccordionItemContext.Provider value={id}>
          <li className={className}>{children}</li>
       </AccordionItemContext.Provider>
    )
