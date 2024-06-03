a8973df02d0b4cb79490146d02c668eccbf99030
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-03 15:02:03 +0000

    Jun 3, 2024, 8:32 PM

--- src/Place.jsx
+++ src/Place.jsx
@@ -1,1 +1,10 @@
-e
+export default function Place({ item }) {
+    return (
+        <article className="place">
+            <div>
+                <h2>{item.title}</h2>
+                <p>{item.description}</p>
+            </div>
+        </article>
+    )
+}
