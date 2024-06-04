763f627bd9446da14354856980832a6d106eb13d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 01:29:41 +0000

    Jun 4, 2024, 6:59 AM

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ export default function App() {
             <header>
                 <img src="" alt="" />
                 <h1>react essentials</h1>
-                <p></p>
+                <p>
+                    fundamemtal react concept you will need for building any
+                    react app
+                </p>
             </header>
             <main>
                 <h2>time to get started</h2>
