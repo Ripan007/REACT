3ccadb7b86fa2dfb25c36169d2d4c3d88238f568
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 02:07:35 +0000

    Jun 4, 2024, 7:37 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+function Header() {
+    return (
+        <header>
+            <img
+                src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt="coffe-image"
+            />
+            <h1>react essentials</h1>
+            <p>
+                fundamemtal react concept you will need for building any react
+                app
+            </p>
+        </header>
+    )
+}
 export default function App() {
     return (
         <div>
-            <header>
-                <img
-                    src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                    alt="coffe-image"
-                />
-                <h1>react essentials</h1>
-                <p>
-                    fundamemtal react concept you will need for building any
-                    react app
-                </p>
-            </header>
+            <Header />
             <main>
                 <h2>time to get started</h2>
             </main>
