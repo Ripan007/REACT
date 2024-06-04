78b98c83254b3097bba624cb39082e2be689d642
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 01:30:25 +0000

    Jun 4, 2024, 7:00 AM

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ export default function App() {
     return (
         <div>
             <header>
-                <img src="" alt="" />
+                <img
+                    src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                    alt="coffe-image"
+                />
                 <h1>react essentials</h1>
                 <p>
                     fundamemtal react concept you will need for building any
