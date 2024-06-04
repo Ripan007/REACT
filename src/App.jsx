92537d7bbf9a41532922fbc4ebf2ea6ae23be11a
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 03:00:42 +0000

    Jun 4, 2024, 8:30 AM

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * (max + 1))
 }
 
-function Header() {
-    return (
-        <header>
-            <img
-                src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                alt="coffe-image"
-            />
-            <h1>react essentials</h1>
-            <p>
-                {description} react concept you will need for building any react
-                app
-            </p>
-        </header>
-    )
-}
 export default function App() {
+    function Header() {
+        return (
+            <header>
+                <img
+                    src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                    alt="coffe-image"
+                />
+                <h1>react essentials</h1>
+                <p>
+                    {description} react concept you will need for building any
+                    react app
+                </p>
+            </header>
+        )
+    }
     const description = reactDescritption[getRandomIndex(3)]
     return (
         <div>
@@ -29,3 +29,5 @@ export default function App() {
         </div>
     )
 }
+
+//
