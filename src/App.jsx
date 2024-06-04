b855830dbbb8d897c0d771d4eeb56d9701095ee9
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 03:03:13 +0000

    Jun 4, 2024, 8:33 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-const reactDescritption = ['fundamental', 'crucialc', 'core']
-function getRandomIndex(max) {
+const reactDescritption = ['fundamental', 'crucial', 'core']
+function genRandomIndex(max) {
     return Math.floor(Math.random() * (max + 1))
 }
 
+function Header() {
+    return (
+        <header>
+            <img
+                src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt="coffe-image"
+            />
+            <h1>react essentials</h1>
+            <p>
+                {description} react concept you will need for building any react
+                app
+            </p>
+        </header>
+    )
+}
+const description = reactDescritption[genRandomIndex(3)]
 export default function App() {
-    function Header() {
-        return (
-            <header>
-                <img
-                    src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-                    alt="coffe-image"
-                />
-                <h1>react essentials</h1>
-                <p>
-                    {description} react concept you will need for building any
-                    react app
-                </p>
-            </header>
-        )
-    }
-    const description = reactDescritption[getRandomIndex(3)]
     return (
         <div>
             <Header />
