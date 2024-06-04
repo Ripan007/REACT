b16b8be1802a925f9d3de6910078a42b8d0f5f06
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-04 05:21:13 +0000

    Jun 4, 2024, 10:51 AM

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+const reactDescritption = ['fundamental', 'crucial', 'core']
+function genRandomIndex(max) {
+    return Math.floor(Math.random() * (max + 1))
+}
+export default function Header() {
+    return function Header() {
+        const description = reactDescritption[genRandomIndex(3)]
+        return (
+            <header>
+                <img
+                    src="https://images.pexels.com/photos/21625359/pexels-photo-21625359/free-photo-of-bowl-of-soup.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                    alt="coffe-image"
+                />
+                <h1>react essentials</h1>
+                <p>
+                    {description} react concept you will need for building any
+                    react app
+                </p>
+            </header>
+        )
+    }
+}
