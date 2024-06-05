77b00b5015daead5b6ced8928fc22fcda157d547
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 04:01:08 +0000

    Jun 5, 2024, 9:31 AM

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -24,6 +24,7 @@ export default function Examples() {
         <>
             <Section title="Examples" id="examples">
                 <Tabs
+                    ButtonsContainer="menu"
                     buttons={
                         <>
                             <TabButton
