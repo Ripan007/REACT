65399b07590c84b0979a681dfcfb4dbc45738e72
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 03:10:48 +0000

    Jun 5, 2024, 8:40 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
-import { useState } from 'react'
-import CoreConcept from './components/CoreConcept'
 import Header from './components/Header/Header'
-import TabButton from './components/TabButton'
-import { CORE_CONCEPTS, EXAMPLES } from './data'
+
+import CoreConcepts from './components/CoreConcepts'
+import Examples from './components/Examples'
 
 export default function App() {
-    const [selectedTopic, setSelectedTopic] = useState('')
-    function handleSelect(selectedButton) {
-        setSelectedTopic(selectedButton)
-        console.log(selectedTopic)
-    }
-
-    let tabContent = <p>please select a topic</p>
-    if (selectedTopic) {
-        tabContent = (
-            <div id="tab-content">
-                <h3>{EXAMPLES[selectedTopic].title}</h3>
-                <p>{EXAMPLES[selectedTopic].description}</p>
-            </div>
-        )
-    }
-
     return (
         <>
             <Header />
-            <main></main>
+            <main>
+                <CoreConcepts />
+                <Examples />
+            </main>
         </>
     )
 }
--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react'
+import { EXAMPLES } from '../data'
+
 export default function Examples() {
+    const [selectedTopic, setSelectedTopic] = useState('')
+    function handleSelect(selectedButton) {
+        setSelectedTopic(selectedButton)
+        console.log(selectedTopic)
+    }
+
+    let tabContent = <p>please select a topic</p>
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+            </div>
+        )
+    }
     return (
         <>
             <section id="examples">
