a10f049e7086cf3ca46d013f287a722eb6fee489
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 15:13:18 +0000

    Jun 5, 2024, 8:43 PM

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -10,7 +10,13 @@ export default function GameBoard() {
         <ol id="game-board">
             {initialGameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
-                    <ol></ol>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
                 </li>
             ))}
         </ol>
