afa8ad9bef1f13868c0fca2d24b2e7252f27f3b3
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 04:02:46 +0000

    Jun 5, 2024, 9:32 AM

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Tabs({ buttons, children }) {
+export default function Tabs({ buttons, children, ButtonsContainer }) {
     return (
         <>
-            <menu>{buttons}</menu>
+            <ButtonsContainer>{buttons}</ButtonsContainer>
             {children}
         </>
     )
