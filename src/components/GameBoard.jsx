da2f43f8340ffa354dbbb6853e7f4b52e3f51060
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 15:11:44 +0000

    Jun 5, 2024, 8:41 PM

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
+
+export default function GameBoard() {
+    return (
+        <ol id="game-board">
+            {initialGameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol></ol>
+                </li>
+            ))}
+        </ol>
+    )
+}
