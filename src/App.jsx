d93ce15ebd24d76eb0bf50323c860f5f762b92b5
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 04:27:49 +0000

    Jun 5, 2024, 9:57 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
 export default function App() {
-    return <></>
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="player-name">player 1</span>
+                        <span className="player-symbol">x</span>
+                    </li>
+                    <li>
+                        <span className="player-name">player 2</span>
+                        <span className="player-symbol">0</span>
+                    </li>
+                </ol>
+            </div>
+        </main>
+    )
 }
