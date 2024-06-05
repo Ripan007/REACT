7e62b558677ce1dae8add1ce4597cb3603081c36
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-05 03:40:36 +0000

    Jun 5, 2024, 9:10 AM

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,10 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
     function handleClick() {
         console.log('button got clicked')
     }
     return (
         <li>
-            <button
-                className={isSelected ? 'active' : undefined}
-                onClick={onSelect}>
+            <button className={isSelected ? 'active' : undefined} {...props}>
                 {children}
             </button>
         </li>
