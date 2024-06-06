5f0741e3b5486d8ff48d7fb64e75df549bd67da9
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-06 13:04:30 +0000

    Jun 6, 2024, 6:34 PM

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function deriveActivePlayer(gameTurns) {
 export default function App() {
     const [gameTurns, setGameTurns] = useState([])
     const activePlayer = deriveActivePlayer(gameTurns)
-    let gameBoard = initialGameBoard
+    let gameBoard = [...initialGameBoard.map(array => [...array])]
     for (const turn of gameTurns) {
         const { square, player } = turn
         const { row, col } = square
