a0a09a34df594e213fba2a6c3a3dbd6e47eb070f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-06 13:41:19 +0000

    Jun 6, 2024, 7:11 PM

--- src/App.jsx
+++ src/App.jsx
@@ -31,22 +31,25 @@ export default function App() {
         const { row, col } = square
         gameBoard[row][col] = player
     }
-    let winner
 
-    for (const combination of WINNING_COMBINATIONS) {
-        const firstSquareSymbol =
-            gameBoard[combination[0].row][combination[0].column]
-        const secondSquareSymbol =
-            gameBoard[combination[1].row][combination[1].column]
-        const thirdSquareSymbol =
-            gameBoard[combination[2].row][combination[2].column]
-        if (
-            firstSquareSymbol &&
-            firstSquareSymbol === secondSquareSymbol &&
-            firstSquareSymbol === thirdSquareSymbol
-        ) {
-            winner = players[firstSquareSymbol]
+    function deriveWinner() {
+        let winner
+        for (const combination of WINNING_COMBINATIONS) {
+            const firstSquareSymbol =
+                gameBoard[combination[0].row][combination[0].column]
+            const secondSquareSymbol =
+                gameBoard[combination[1].row][combination[1].column]
+            const thirdSquareSymbol =
+                gameBoard[combination[2].row][combination[2].column]
+            if (
+                firstSquareSymbol &&
+                firstSquareSymbol === secondSquareSymbol &&
+                firstSquareSymbol === thirdSquareSymbol
+            ) {
+                winner = players[firstSquareSymbol]
+            }
         }
+        return winner
     }
     const hasDraw = gameTurns.length === 9 && !winner
     function handleSelectSquare(rowIndex, colIndex) {
