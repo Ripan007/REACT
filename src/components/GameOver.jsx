20aa194d2626010529f5210106bdfb87c6fa98f2
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-06 13:01:55 +0000

    Jun 6, 2024, 6:31 PM

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, onRestart }) {
     return (
         <div id="game-over">
             <h2>game over</h2>
             {winner && <p>{winner} won</p>}
             {!winner && <p>It&apos; a draw</p>}
             <p>
-                <button>rematch</button>
+                <button onClick={onRestart}>rematch</button>
             </p>
         </div>
     )
