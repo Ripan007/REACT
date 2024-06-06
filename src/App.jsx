c78a11dd146506207096bfdc097dc50369d9ad60
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-06 13:01:15 +0000

    Jun 6, 2024, 6:31 PM

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,9 @@ export default function App() {
             return updatedTurns
         })
     }
+    function handleRestart() {
+        setGameTurns([])
+    }
     return (
         <main>
             <div id="game-container">
