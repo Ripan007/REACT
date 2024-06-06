207e1e1159edbcb10bb875bc4d5b723adf0ac874
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-06 05:36:03 +0000

    Jun 6, 2024, 11:06 AM

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -4,7 +4,10 @@ export default function Log({ turns }) {
     return (
         <ol id="log">
             {turns.map(turn => (
-                <li key={``}></li>
+                <li key={`${turn.square.row}${turn.square.col}`}>
+                    {turn.player} selected {turn.square.row}
+                    {turn.square.col}
+                </li>
             ))}
         </ol>
     )
