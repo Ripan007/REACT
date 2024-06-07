865c021bfac1074df15ff8c5c358268d72e15311
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 02:45:00 +0000

    Jun 7, 2024, 8:15 AM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Header() {
     return (
         <header id="header">
-            <img src="" alt="inavestment logo" />
+            <img
+                src="https://wallpaperaccess.com/full/2860145.jpg"
+                alt="inavestment logo"
+            />
             <h1>invest calculator</h1>
         </header>
     )
