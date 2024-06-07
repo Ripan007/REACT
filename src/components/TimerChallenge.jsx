f2f9586eb5c11ca3ddd9af3c22c7204ecbbccdc4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 14:45:38 +0000

    Jun 7, 2024, 8:15 PM

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export default function TimerChallenge() {
-    return <section className="challenge"></section>
+export default function TimerChallenge({ title, targetTime }) {
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} second {targetTime > 1 ? 's' : ''}
+            </p>
+            <p>
+                <button>start challenge</button>
+            </p>
+            <p>time id running / .. timer is inactive</p>
+        </section>
+    )
 }
