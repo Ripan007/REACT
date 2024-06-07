97b50a05d6df60a7c39a807d6906398e0cf0f170
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 04:20:57 +0000

    Jun 7, 2024, 9:50 AM

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { calculateInvestmentResults } from '../util/investment'
 
 export default function Results({ input }) {
+    const resultsData = calculateInvestmentResults(input)
+    console.log('show data', resultsData)
     console.log(input)
     return <p>Results...</p>
 }
