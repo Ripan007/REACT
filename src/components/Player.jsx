2ebf91f621d6ca0c65033c0b8ed1c6f8ce3e8cd4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 13:37:41 +0000

    Jun 7, 2024, 7:07 PM

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Player() {
+    const [enteredPlayerName, setEnteredPlayerName] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+    function handleChange(event) {
+        setEnteredPlayerName(event.target.value)
+    }
+    function handleSubmitted() {
+        setSubmitted(true)
+    }
     return (
         <section id="player">
-            <h2>welcome unknown entity</h2>
+            <h2>welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
             <p>
-                <input type="text" />
-                <button>set name</button>
+                <input
+                    type="text"
+                    value={enteredPlayerName}
+                    onChange={handleChange}
+                />
+                <button onClick={handleSubmitted}>set name</button>
             </p>
         </section>
     )
