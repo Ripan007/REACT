64da7bb54532332eb33a4d2db08ce509b5f6944c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 02:49:57 +0000

    Jun 7, 2024, 8:19 AM

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function UserInput() {
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label htmlFor="">initial investment</label>
+                    <input type="text" />
+                </p>
+            </div>
+        </section>
+    )
+}
