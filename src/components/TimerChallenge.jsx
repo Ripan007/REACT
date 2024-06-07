d13e83cddafeeee6bd49f1f1cf8e5026c7711c85
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 15:33:20 +0000

    Jun 7, 2024, 9:03 PM

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TimerChallenge({ title, targetTime }) {
+    const [timerExpired, setTimerExpired] = useState(false)
     function handleStart() {
-        setTimeout(() => {}, targetTime * 1000)
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000)
     }
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>you lost</p>}
             <p className="challenge-time">
                 {targetTime} second {targetTime > 1 ? 's' : ''}
             </p>
