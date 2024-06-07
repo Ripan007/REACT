ff80cf262771c7cc66c6b3831c9eb4ac5c9b275a
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 07:01:58 +0000

    Jun 7, 2024, 12:31 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import Player from './components/Player'
 
 export default function App() {
-    return <div>App</div>
+    return (
+        <>
+            <Player />
+            <div id="challenges"></div>
+        </>
+    )
 }
