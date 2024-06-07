a747547a690bd040e4314e3e2d6141a4153058c0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 06:59:54 +0000

    Jun 7, 2024, 12:29 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
-import Header from './components/Header'
-import AuthInputs from './components/AuthInputs'
 
 export default function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <AuthInputs />
-            </main>
-        </>
-    )
+    return <div>App</div>
 }
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <img src="" alt="A canvas" />
-            <h1>ReactArt</h1>
-            <p>A community of artists and art-lovers.</p>
-        </header>
-    )
-}
