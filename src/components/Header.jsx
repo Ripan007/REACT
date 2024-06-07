309e4df0b11f361deb7def2d85a3f713cc35f881
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-07 02:43:48 +0000

    Jun 7, 2024, 8:13 AM

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header id="header">
+            <img src="" alt="inavestment logo" />
+            <h1>invest calculator</h1>
+        </header>
+    )
+}
