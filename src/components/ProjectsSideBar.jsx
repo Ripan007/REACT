7488f4b5c62154fcbc8a10e5d80fb62bc6c63f6d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 01:55:23 +0000

    Jun 8, 2024, 7:25 AM

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function ProjectsSideBar() {
-    return <div>ProjectsSideBar</div>
+    return (
+        <aside>
+            <h2>your projects</h2>
+            <div>
+                <button>+ add projects</button>
+            </div>
+            <ul></ul>
+        </aside>
+    )
 }
