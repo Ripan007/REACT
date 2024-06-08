59c1605d698d9cf822d2aa11f607b305d3aa571e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 03:37:25 +0000

    Jun 8, 2024, 9:07 AM

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function NoProjectSelected() {
     return (
         <div className="mt-24  text-center  w-2/3">
-            <img src="" alt="" />
+            <img
+                src="https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-empty-notebook-with-a-pencil-3d-rendering-png-image_9954855.png"
+                alt="an empty task list"
+            />
             <h2>no project selected</h2>
             <p>select a project or get started with new one</p>
             <p>
