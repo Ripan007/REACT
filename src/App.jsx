a3cb5a006c0663b96069903015b631d5492f4d54
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 03:13:08 +0000

    Jun 8, 2024, 8:43 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProjectsSideBar from './components/ProjectsSideBar'
+import NewProject from './components/NewProject'
 
 export default function App() {
     return (
-        <main className="h-screen my-8">
+        <main className="h-screen my-8 flex gap-8">
             <ProjectsSideBar />
+            <NewProject />
         </main>
     )
 }
