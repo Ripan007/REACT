01470b5bb0cdfc1a7f4b48f27f1628a6ef69f72c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 03:34:44 +0000

    Jun 8, 2024, 9:04 AM

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function NoProjectSelected() {
-    return <div>NoProjectSelected</div>
+    return (
+        <div>
+            <img src="" alt="" />
+            <h2>no project selected</h2>
+            <p>select a project or get started with new one</p>
+        </div>
+    )
 }
