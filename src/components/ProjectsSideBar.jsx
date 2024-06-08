92ad4ed814561821065f7225fd6f8fbb40ec720d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 02:03:32 +0000

    Jun 8, 2024, 7:33 AM

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function ProjectsSideBar() {
     return (
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-            <h2>your projects</h2>
+            <h2 className="mb-8  font-bold uppercase md:text-xl  text-stone-200 ">
+                your projects
+            </h2>
             <div>
                 <button>+ add projects</button>
             </div>
