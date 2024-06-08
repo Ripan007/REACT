ae49d50b5c1b231f244d4bb3e4cba38e0a57b73f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 03:02:13 +0000

    Jun 8, 2024, 8:32 AM

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NewProject() {
+    return (
+        <div>
+            <menu>
+                <li>
+                    <button>cancel</button>
+                </li>
+                <li>
+                    <button>save</button>
+                </li>
+            </menu>
+        </div>
+    )
+}
