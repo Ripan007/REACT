db58c56a7c1b453dfefaeb171ad8a80cf8a4737e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 01:52:09 +0000

    Jun 8, 2024, 7:22 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
 import React from 'react'
-import Player from './components/Player'
-import PlayerRef from './components/PlayerRef'
-import TimerChallenge from './components/TimerChallenge'
 
 export default function App() {
-    return (
-        <>
-            <PlayerRef />
-            <div id="challenges">
-                <TimerChallenge title="easy" targetTime={1} />
-                <TimerChallenge title="not easy" targetTime={5} />
-                <TimerChallenge title="getting tough" targetTime={10} />
-                <TimerChallenge title="pros only" targetTime={15} />
-            </div>
-        </>
-    )
+    return <div>App</div>
 }
