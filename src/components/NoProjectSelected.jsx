ecfb2779175efe789b6b02348700eefbff08a88b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 03:40:47 +0000

    Jun 8, 2024, 9:10 AM

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -6,10 +6,15 @@ export default function NoProjectSelected() {
             <img
                 src="https://png.pngtree.com/png-vector/20230903/ourmid/pngtree-empty-notebook-with-a-pencil-3d-rendering-png-image_9954855.png"
                 alt="an empty task list"
+                className="w-16 h-16 object-contain mx-auto"
             />
-            <h2>no project selected</h2>
-            <p>select a project or get started with new one</p>
-            <p>
+            <h2 className="text-xl font-bold  text-stone-500 my-4 ">
+                no project selected
+            </h2>
+            <p className="text-stone-400 mb-4">
+                select a project or get started with new one
+            </p>
+            <p className="mt-8">
                 <button>create new project</button>
             </p>
         </div>
