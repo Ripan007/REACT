da3be376151a0c956b1976516ce1e63314df0628
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 13:58:27 +0000

    Jun 8, 2024, 7:28 PM

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useImperativeHandle, forwardRef, useRef } from 'react'
 import { createPortal } from 'react-dom'
-export default function Modal({ children }) {
+const Modal = forwardRef(function Modal({ children }, ref) {
+    const dialog = useRef()
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal()
+            },
+        }
+    })
     return createPortal(
-        <dialog>{children}</dialog>,
+        <dialog ref={dialog}>{children}</dialog>,
         document.getElementById('modal-root')
     )
-}
+})
+export default Modal
