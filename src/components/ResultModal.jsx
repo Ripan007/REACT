11868dc1a85216f77c7d81f58bd8aaf97893444f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 01:34:20 +0000

    Jun 8, 2024, 7:04 AM

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -7,7 +7,7 @@ const ResultModal = forwardRef(function ResultModal(
     const dialog = useRef()
     const userLost = remainingTime <= 0
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
-    const score = Math.round((1 - remainingTime / targetTime) * 1000)
+    const score = Math.round((1 - (remainingTime / targetTime) * 1000) * 100)
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -18,6 +18,7 @@ const ResultModal = forwardRef(function ResultModal(
     return (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>you lost</h2>}
+            {!userLost && <h2>your score : {score}</h2>}
 
             <p>
                 the target time was <strong>{targetTime}</strong>seconds
