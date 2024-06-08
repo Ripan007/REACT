98978c958b6695d4e544d103b4d08fc0cb28c005
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 04:14:49 +0000

    Jun 8, 2024, 9:44 AM

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectsSideBar from './components/ProjectsSideBar'
 import NewProject from './components/NewProject'
 import NoProjectSelected from './components/NoProjectSelected'
 
 export default function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
+    })
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            }
+        })
+    }
     return (
         <main className="h-screen my-8 flex gap-8">
             <ProjectsSideBar />
