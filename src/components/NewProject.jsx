40b57e4cd1c1ce2dc4bdf2177381e20ae90f9b69
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 13:59:49 +0000

    Jun 8, 2024, 7:29 PM

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react'
 import Input from './Input'
+import Modal from './Modal'
 
 export default function NewProject({ onAdd }) {
+    const modal = useRef()
     const title = useRef()
     const description = useRef()
     const dueDate = useRef()
@@ -24,26 +26,29 @@ export default function NewProject({ onAdd }) {
         })
     }
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">
-                        cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                        onClick={handleSave}>
-                        save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type="text" ref={title} label="title" />
-                <Input ref={description} label="description" textarea />
-                <Input type="date" ref={dueDate} label="due date" />
+        <>
+            <Modal ref={modal} />
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-950">
+                            cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                            onClick={handleSave}>
+                            save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="title" />
+                    <Input ref={description} label="description" textarea />
+                    <Input type="date" ref={dueDate} label="due date" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
