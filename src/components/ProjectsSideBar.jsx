6280840841deb22f7f1a2c27fa25a263618828f9
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-08 02:06:13 +0000

    Jun 8, 2024, 7:36 AM

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -7,7 +7,9 @@ export default function ProjectsSideBar() {
                 your projects
             </h2>
             <div>
-                <button>+ add projects</button>
+                <button className="px-4 py-2 text-xs md:text-base  rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+                    + add projects
+                </button>
             </div>
             <ul></ul>
         </aside>
