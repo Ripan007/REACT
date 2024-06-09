668703eb026117130e12d8ba505f413734b23e5b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 13:18:29 +0000

    Jun 9, 2024, 6:48 PM

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     const modal = useRef()
     const { items } = useContext(CartContext)
 
-    const cartQuantity = cart.items.length
+    const cartQuantity = items.length
 
     function handleOpenCartClick() {
         modal.current.open()
@@ -26,13 +26,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
 
     return (
         <>
-            <CartModal
-                ref={modal}
-                cartItems={cart.items}
-                onUpdateCartItemQuantity={onUpdateCartItemQuantity}
-                title="Your Cart"
-                actions={modalActions}
-            />
+            <CartModal ref={modal} title="Your Cart" actions={modalActions} />
             <header id="main-header">
                 <div id="main-title">
                     <img src="" alt="Elegant model" />
