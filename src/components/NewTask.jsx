e0556eeccf86fff09b9a3906e9528c9b206fc5f7
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 02:15:45 +0000

    Jun 9, 2024, 7:45 AM

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function NewTask() {
     return (
-        <div className="">
-            <input type="text" />
-            <button>add task</button>
+        <div className="flex items-center gap-4">
+            <input
+                className="w-64 px-2  py-1 rounded-sm  bg-stone-200"
+                type="text"
+            />
+            <button className="text-stone-700 hover:text-stone-950">
+                add task
+            </button>
         </div>
     )
 }
