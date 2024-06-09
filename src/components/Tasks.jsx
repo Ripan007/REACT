f9373ca6c9c682fec6386a3d677ff5d5f3e677ae
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 01:57:15 +0000

    Jun 9, 2024, 7:27 AM

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function Tasks() {
-    return <div>Tasks</div>
+    return (
+        <section>
+            <h2 className="text-2xl  font-bold text-stone-700 mb-4">task</h2>
+            new tasks
+            <p className="text-stone-800 mb-4">
+                this projects does not have any tasks yet
+            </p>
+            <ul></ul>
+        </section>
+    )
 }
