7229d3aed7718a66e5bc1cd0da8cc6ea5095c0e9
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 13:24:47 +0000

    Jun 9, 2024, 6:54 PM

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { CartContext } from '../store/Shopping-cart-context'
 import { useContext } from 'react'
 export default function Cart({ onUpdateItemQuantity }) {
-    const { items } = useContext(CartContext)
+    const { items, updateItemQuantity } = useContext(CartContext)
 
     const totalPrice = items.reduce(
         (acc, item) => acc + item.price * item.quantity,
