22cdb4ca3740461f53b8f0cf6367deed6683cc4d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 04:18:51 +0000

    Jun 9, 2024, 9:48 AM

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Tasks from './Tasks'
 
-export default function SelectedProject({ project, onDelete }) {
+export default function SelectedProject({
+    project,
+    onDelete,
+    onAddTask,
+    onDeleteTask,
+}) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString(
         'en-US',
         { year: 'numeric', month: 'short', day: 'numeric' }
@@ -24,7 +29,7 @@ export default function SelectedProject({ project, onDelete }) {
                     {project.description}
                 </p>
             </header>
-            <Tasks />
+            <Tasks onAdd={onAddTask} />
         </div>
     )
 }
