ba8a6975ce56430db233e05ca70632b93aabcbc1
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 03:59:55 +0000

    Jun 9, 2024, 9:29 AM

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 
 export default function NewTask() {
     const [enteredTask, setEnteredTask] = useState()
+    function handleChange(event) {
+        setEnteredTask(event.target.value)
+    }
     return (
         <div className="flex items-center gap-4">
             <input
                 className="w-64 px-2  py-1 rounded-sm  bg-stone-200"
                 type="text"
+                onChange={handleChange}
             />
             <button className="text-stone-700 hover:text-stone-950">
                 add task
