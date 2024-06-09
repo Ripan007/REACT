21df557046d36b59cf595692e773b306b0ab9f7e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 06:00:00 +0000

    Jun 9, 2024, 11:30 AM

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
                 cart={shoppingCart}
                 onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
             />
-            <Shop onAddItemToCart={handleAddItemToCart}>
+            <Shop>
                 {DUMMY_PRODUCTS.map(product => (
                     <li key={product.id}>
                         <Product
