232a9929616424ef4037dc0679a5be5ceeb1e62d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 01:35:57 +0000

    Jun 9, 2024, 7:05 AM

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -18,7 +18,12 @@ export default function ProjectsSideBar({
             <ul className="mt-8">
                 {projects.map(project => {
                     let cssClasses =
-                        'w-full text-left px-2 py-1  rounded-sm my-1 text-stone-400 hover:text-stone-200  hover:bg-stone-800'
+                        'w-full text-left px-2 py-1  rounded-sm my-1 hover:text-stone-200  hover:bg-stone-800'
+                    if (project.id === selectedProjectId) {
+                        cssClasses += 'bg-stone-800 text-stone-200'
+                    } else {
+                        cssClasses += 'text-stone-400 '
+                    }
                     return (
                         <li key={project.id}>
                             <button onClick={onSelectProject} className="">
