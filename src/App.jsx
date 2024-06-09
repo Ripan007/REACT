d42e08810d50ebafcbdfe49a409731325618d596
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 04:14:26 +0000

    Jun 9, 2024, 9:44 AM

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ export default function App() {
         <SelectedProject
             project={selectedProject}
             onDelete={handleDeleteProject}
-            onAddTask={}
+            onAddTask={handleAddTask}
         />
     )
     if (projectsState.selectedProjectId === null) {
