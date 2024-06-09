8489613c94e356ee04e661a46f96f814b8af4243
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 01:33:37 +0000

    Jun 9, 2024, 7:03 AM

--- src/components/ProjectsSideBar.jsx
+++ src/components/ProjectsSideBar.jsx
@@ -5,6 +5,7 @@ export default function ProjectsSideBar({
     onStartAddProject,
     projects,
     onSelectProject,
+    selectedProjectId,
 }) {
     return (
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -15,15 +16,17 @@ export default function ProjectsSideBar({
                 <Button onClick={onStartAddProject}>+ add projects</Button>
             </div>
             <ul className="mt-8">
-                {projects.map(project => (
-                    <li key={project.id}>
-                        <button
-                            onClick={onSelectProject}
-                            className="w-full text-left px-2 py-1  rounded-sm my-1 text-stone-400 hover:text-stone-200  hover:bg-stone-800">
-                            {project.title}
-                        </button>
-                    </li>
-                ))}
+                {projects.map(project => {
+                    let cssClasses =
+                        'w-full text-left px-2 py-1  rounded-sm my-1 text-stone-400 hover:text-stone-200  hover:bg-stone-800'
+                    return (
+                        <li key={project.id}>
+                            <button onClick={onSelectProject} className="">
+                                {project.title}
+                            </button>
+                        </li>
+                    )
+                })}
             </ul>
         </aside>
     )
