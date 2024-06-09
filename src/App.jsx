a7fd4dda3997408ece1b4b54b083f14fec4a92bd
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 13:05:34 +0000

    Jun 9, 2024, 6:35 PM

--- src/App.jsx
+++ src/App.jsx
@@ -68,8 +68,13 @@ function App() {
         })
     }
 
+    const ctxValue = {
+        items: shoppingCart.items,
+        handleAddItemToCart: handleAddItemToCart,
+    }
+
     return (
-        <CartContext.Provider value={{ items: [] }}>
+        <CartContext.Provider value={ctxValue}>
             <Header
                 cart={shoppingCart}
                 onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
