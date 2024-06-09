5d30d70161df0b486b90b390c033dc033977f155
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 03:57:48 +0000

    Jun 9, 2024, 9:27 AM

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function NewTask() {
+    const [enteredTask, setEnteredTask] = useState()
     return (
         <div className="flex items-center gap-4">
             <input
