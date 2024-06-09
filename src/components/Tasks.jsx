18f94becc073183fd98f1d98cbd3743672cdf73c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 04:39:13 +0000

    Jun 9, 2024, 10:09 AM

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import NewTask from './NewTask'
 
-export default function Tasks({ onAdd, onDelete }) {
+export default function Tasks({ tasks, onAdd, onDelete }) {
     return (
         <section>
             <h2 className="text-2xl  font-bold text-stone-700 mb-4">task</h2>
             <NewTask onAdd={onAdd} />
-            <p className="text-stone-800 my-4">
-                this projects does not have any tasks yet
-            </p>
-            <ul></ul>
+            {tasks.length === 0 && (
+                <p className="text-stone-800 my-4">
+                    this projects does not have any tasks yet
+                </p>
+            )}
+            {tasks.length > 0 && <ul></ul>}
         </section>
     )
 }
