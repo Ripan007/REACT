f1dc1ef569e39ab49aff492d786c525748a6801f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-09 04:33:08 +0000

    Jun 9, 2024, 10:03 AM

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 
-export default function NewTask() {
+export default function NewTask({ onAdd }) {
     const [enteredTask, setEnteredTask] = useState()
     function handleChange(event) {
         setEnteredTask(event.target.value)
     }
     function handleClick() {
+        onAdd(enteredTask)
         setEnteredTask('')
     }
     return (
