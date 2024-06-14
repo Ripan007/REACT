dd0ef28c09fce7869a2ef2c1d1a60f691f4630c7
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-14 05:10:25 +0000

    Jun 14, 2024, 10:40 AM

--- src/css.js
+++ src/css.js
@@ -4,3 +4,9 @@ const countriesContainer = document.querySelector('.countries')
 const request = new XMLHttpRequest()
 request.open('GET', 'https://restcountries.com/v2/name/portugal')
 request.send()
+
+console.log(request.responseText)
+
+request.addEventListener('load', function () {
+    console.log(this.responseText)
+})
