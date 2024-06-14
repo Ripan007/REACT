df38c14b924dfd8734e827d21ec8d12342b3eec1
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-14 05:09:30 +0000

    Jun 14, 2024, 10:39 AM

--- src/css.js
+++ src/css.js
@@ -1,3 +1,6 @@
 'use strict'
 const btn = document.querySelector('.btn-country')
 const countriesContainer = document.querySelector('.countries')
+const request = new XMLHttpRequest()
+request.open('GET', 'https://restcountries.com/v2/name/portugal')
+request.send()
