6336865f390ea334ab750068cbc53bc76f3d8767
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-14 05:20:35 +0000

    Jun 14, 2024, 10:50 AM

--- src/css.js
+++ src/css.js
@@ -7,5 +7,20 @@ request.open('GET', 'https://restcountries.com/v2/name/portugal')
 request.send()
 
 request.addEventListener('load', function () {
-    console.log(this.responseText)
+    // console.log(this.responseText)
+    const [data] = JSON.parse(this.responseText)
+    console.log(data)
+    const html = `
+          <article class="country">
+                    <img src=${data} alt="" class="country__img" />
+                    <div class="country__data">
+                        <h3 class="country__name">country</h3>
+                        <h4 class="country__region">region</h4>
+                        <p class="country__row">😖<span>pop people</span></p>
+                        <p class="country__row">🦩<span>language</span></p>
+                        <p class="country__row">🪐<span>region</span></p>
+                    </div>
+                </article>
+
+    `
 })
