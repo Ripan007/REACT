f63dd1e116f9182b6a2ac0f876fb7e6f33d8245d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-14 05:38:55 +0000

    Jun 14, 2024, 11:08 AM

--- src/css.js
+++ src/css.js
@@ -16,15 +16,13 @@ request.addEventListener('load', function () {
                     <div class="country__data">
                         <h3 class="country__name">${data.name}</h3>
                         <h4 class="country__region">${data.region}</h4>
-                        <p class="country__row"> 😖 <span>${(
+                        <p class="country__row"> <span>😖 </span>${(
                             +data.population / 1000000
-                        ).toFixed(1)}</span></p>
-                        <p class="country__row"> 🦩 <span>${
-                            data.languages[0].name
-                        }</span></p>
-                        <p class="country__row"> 🪐 <span>${
-                            data.currencies[0].name
-                        }</span></p>
+                        ).toFixed(1)}</p>
+                        <p class="country__row">  <span>
+                        🦩</span> ${data.languages[0].name}
+                        <p class="country__row"> <span>
+                        🪐 </span> ${data.currencies[0].name}
                     </div>
                 </article>
 
