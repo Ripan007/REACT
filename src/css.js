b59ead5ab5ef620afa95aac312eacf687d4cbd4d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 03:51:24 +0000

    Jun 17, 2024, 9:21 AM

--- src/css.js
+++ src/css.js
@@ -1,15 +1,3 @@
-new Promise(function (resolve, reject) {
-    if (Math.random() < 0.5) {
-        reject('we dont know');
-    }
-    resolve(42);
-})
-    .then(function (answer) {
-        console.log('🚀 ~ answer:', answer);
-    })
-    .then(function (error) {
-        console.warn('🚀 ~ error:', error);
-    })
-    .finally(function () {
-        console.log('no matter what , we will run');
-    });
+fetch('https://jsonplaceholder.typicode.com/posts').then(function (response) {
+    console.log('🚀 ~ response:', response);
+});
