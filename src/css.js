47572075bb8513bdf6ca92d0e4061a674b86cca5
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 22:55:41 +0000

    Jun 18, 2024, 4:25 AM

--- src/css.js
+++ src/css.js
@@ -3,4 +3,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].textContent;
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', function () {
+        console.log('button got clicked');
+    });
