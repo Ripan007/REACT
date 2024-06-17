b7adda0b4e1b740cd848848f3e05ae450e0ce86f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 06:36:33 +0000

    Jun 17, 2024, 12:06 PM

--- src/css.js
+++ src/css.js
@@ -2,4 +2,9 @@
 
 const show = document.querySelector('.message').textContent;
 
-document.querySelector('.check');
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector();
+    }
+});
