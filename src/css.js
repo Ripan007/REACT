e2e42e8289b45328d0d6f9de8ec53b456372dcc0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 03:52:02 +0000

    Jun 17, 2024, 9:22 AM

--- src/css.js
+++ src/css.js
@@ -1,3 +1,7 @@
-fetch('https://jsonplaceholder.typicode.com/posts').then(function (response) {
-    console.log('🚀 ~ response:', response);
-});
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function (response) {
+        console.log('🚀 ~ response:', response);
+    })
+    .catch(function (err) {
+        console.warn(err);
+    });
