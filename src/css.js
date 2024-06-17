fb19a20893780dbd023b296dab919dd4ca52472d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 03:28:39 +0000

    Jun 17, 2024, 8:58 AM

--- src/css.js
+++ src/css.js
@@ -2,8 +2,13 @@ let sayHello = new Promise(function (resolve, reject) {
     setTimeout(function () {
         resolve('hello world');
     }, 2000);
+    reject('unable to say hi');
 });
 
-sayHello.then(function (msg) {
-    console.log('🚀 ~ msg:', msg);
-});
+sayHello
+    .then(function (msg) {
+        console.log('🚀 ~ msg:', msg);
+    })
+    .catch(function (err) {
+        console.warn('🚀 ~ err:', err);
+    });
