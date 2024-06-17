f6c74f655722c8f9b8b3d8139ca8053ad63bb368
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 22:55:02 +0000

    Jun 18, 2024, 4:25 AM

--- src/css.js
+++ src/css.js
@@ -3,5 +3,4 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+for (let i = 0; i < btnsOpenModal.length; i++) btnsOpenModal[i].textContent;
