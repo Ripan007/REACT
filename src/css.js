73c51b7ddac7279e1cf989c7c793622feef297f5
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 22:13:00 +0000

    Jun 18, 2024, 3:43 AM

--- src/css.js
+++ src/css.js
@@ -7,6 +7,8 @@ let highScore = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log('🚀 ~ secretNumber:', secretNumber);
 
+const displayMessage = function (message) {};
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log('🚀 ~ guess:', guess, typeof guess);
