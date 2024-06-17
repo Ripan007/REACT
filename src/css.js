c4d150def35514d4bfd7f4f94fa5da530f3a737b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 06:53:14 +0000

    Jun 17, 2024, 12:23 PM

--- src/css.js
+++ src/css.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const show = document.querySelector('.message').textContent;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log('🚀 ~ guess:', guess, typeof guess);
     if (!guess) {
         document.querySelector('.message').textContent = '🚫 no number!';
+    } else if (guess === secretNumber) {
+        document.querySelector;
     }
 });
