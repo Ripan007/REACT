152c792847d231dbb2de79a8459092d3be0523ab
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 03:59:10 +0000

    Jun 17, 2024, 9:29 AM

--- src/css.js
+++ src/css.js
@@ -1,1 +1,10 @@
-Promise.all([fetch('')]);
+Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts'),
+    fetch('https://jsonplaceholder.typicode.com/users'),
+]).then(function (responses) {
+    return Promises.all(
+        responses.map(function (response) {
+            return response.json();
+        })
+    );
+});
