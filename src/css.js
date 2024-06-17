e7623dedd47b8bef9cea057d1515bfdd1a8961f3
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 03:32:55 +0000

    Jun 17, 2024, 9:02 AM

--- src/css.js
+++ src/css.js
@@ -2,7 +2,16 @@ let count = new Promise(function (resolve, reject) {
     resolve(1);
 });
 
-count.then(function (num) {
-    console.log('🚀 ~ num:', num);
-    return count + 1;
-});
+count
+    .then(function (num) {
+        console.log('🚀 ~ num:', num);
+        return num + 1;
+    })
+    .then(function (num) {
+        console.log('🚀 ~ num:', num);
+        return num + 1;
+    })
+    .then(function (num) {
+        console.log('🚀 ~ num:', num);
+        return num + 1;
+    });
