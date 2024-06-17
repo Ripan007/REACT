afd924a75bb9a1f854d9d40ed1e43a4dc6f46ad2
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-17 21:19:19 +0000

    Jun 18, 2024, 2:49 AM

--- src/css.js
+++ src/css.js
@@ -3,7 +3,7 @@
 const show = document.querySelector('.message').textContent;
 let score = 20;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+console.log('🚀 ~ secretNumber:', secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +14,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //  when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '😀 correct number';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.background = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     // when guess is to high
