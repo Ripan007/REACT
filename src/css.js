baaa79da07914db41e950e52d7b73787c2f0a7ac
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-18 02:43:34 +0000

    Jun 18, 2024, 8:13 AM

--- src/css.js
+++ src/css.js
@@ -20,9 +20,7 @@ overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
     console.log(e);
-    if (e.key === 'Escape') {
-        if (!modal.classList.contains('hidden')) {
-            closeModal();
-        }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
     }
 });
