e95c59c5c0c84bd81738be67835f6cfe4a3e0f79
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-18 02:14:23 +0000

    Jun 18, 2024, 7:44 AM

--- src/css.js
+++ src/css.js
@@ -4,18 +4,16 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', function () {
-        // console.log('button got clicked');
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+    btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', function () {
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-});
+};
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+overlay.addEventListener('click', closeModal);
