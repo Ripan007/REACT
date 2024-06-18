c7b419c9e2f5e3149337bea842f313ec2e1c2369
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-06-18 02:41:18 +0000

    Jun 18, 2024, 8:11 AM

--- src/css.js
+++ src/css.js
@@ -3,9 +3,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
-
 const openModal = function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -14,11 +11,15 @@ const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
+    console.log(e);
     if (e.key === 'Escape') {
         if (!modal.classList.contains('hidden')) {
             closeModal();
